6c8ca3f2dea8ce4554a0a05eacac7b48b638f7eb
cychiang <cychiang@chromium.org>
2012-07-12 20:21:05 +0800

    Fix the error in probing for battery
    Catch the exception when probing for battery information failed
    
    BUG=chrome-os-partner:11303
    TEST=run gooftool verify_hwid on snow dvt board
    
    Change-Id: If5e59bd6e1c8df6eeb65f08255f0e092c52b083e
    Reviewed-on: https://gerrit.chromium.org/gerrit/27276
    Reviewed-by: Hsinyu Chao <hychao@chromium.org>
    Tested-by: Cheng-Yi Chiang <cychiang@chromium.org>

--- py/probe.py
+++ py/probe.py
@@ -313,6 +313,7 @@ def _ProbeAudioCodec():
 @_ComponentProbe('battery')
 def _ProbeBattery():
   """Compose data from sysfs."""
+  results = []
   node_path_list = glob('/sys/class/power_supply/*')
   type_data_list = [_ReadSysfsFields(node_path, ['type'])[0]
                     for node_path in node_path_list]
@@ -322,7 +323,10 @@ def _ProbeBattery():
                        for node_path, type_data
                        in zip(node_path_list, type_data_list)
                        if type_data == 'Battery']
-  results = [CompactStr(x) for x in battery_data_list]
+  try:
+    results = [CompactStr(x) for x in battery_data_list]
+  except:
+    pass
   return ' ; '.join(results) if results else None
 
 
