3ccaac1cd3ddc63937a9508e61441f4d05a8ea7f
Dave Parker <dparker@chromium.org>
2013-01-30 14:15:40 -0800

    CHERRY-PICK: Factory: Add manual override option to HWID test.
    This allows an operator to change the HWID returned by the shopfloor
    server. Turned off by default. The main use of this option is for
    RMA. The shopfloor server returns the original HWID of the
    machine which an operator may adjust if they have changed components.
    
    BUG=chrome-os-partner:17302
    TEST=Set 'manual_override: True' in a test_list and use a
    shopfloor server to provide a HWID. Verify you get prompted to select
    a HWID with the default option being the HWID from the shopfloor
    server.
    
    Original-Change-Id: Ie7655af3001655fa21200c63610fabd0f66c15d3
    Reviewed-on: https://gerrit.chromium.org/gerrit/42343
    Reviewed-by: Ricky Liang <jcliang@chromium.org>
    Commit-Queue: Dave Parker <dparker@chromium.org>
    Tested-by: Dave Parker <dparker@chromium.org>
    
    Conflicts:
    
    	py/test/pytests/hwid.py
    
    Change-Id: Ifc45e34bc00fc0f298a47be4de02bb1e42b58387
    Reviewed-on: https://gerrit.chromium.org/gerrit/42518
    Commit-Queue: Dave Parker <dparker@chromium.org>
    Reviewed-by: Dave Parker <dparker@chromium.org>
    Tested-by: Dave Parker <dparker@chromium.org>

--- py/test/pytests/hwid.py
+++ py/test/pytests/hwid.py
@@ -153,7 +153,7 @@ class SelectHWIDTask(FactoryTask):
     self.hwid_list = None
 
   def BuildHWIDList(self):
-    current_hwid = Spawn(['crossystem', 'hwid'],
+    current_hwid = self.test.hwid or Spawn(['crossystem', 'hwid'],
                          check_output=True).stdout_data.strip()
 
     if self.test.hwid_list:
@@ -211,6 +211,9 @@ class HWIDTest(unittest.TestCase):
     Arg('override_hwid', (str, unicode),
         'An override HWID which is used during development.', default=None,
         optional=True),
+    Arg('manual_override', bool, 'Whether to allow manual HWID selection even '
+        'when the shopfloor server is enabled.', default=False,
+        optional=True),
     Arg('auto_probe', bool, 'Whether to enable HWID auto probe.', default=False,
         optional=True),
     Arg('auto_select', bool,
@@ -250,8 +253,10 @@ class HWIDTest(unittest.TestCase):
         self.task_list.append(AutoProbeHWIDTask(self))
         self.task_list.append(SelectHWIDTask(self))
       else:
-        self.task_list.append(ShopFloorHWIDTask(self) if shopfloor.is_enabled()
-                              else SelectHWIDTask(self))
+        if shopfloor.is_enabled():
+          self.task_list.append(ShopFloorHWIDTask(self))
+        if not shopfloor.is_enabled() or self.args.manual_override:
+          self.task_list.append(SelectHWIDTask(self))
     self.task_list.append(WriteHWIDTask(self))
 
     FactoryTaskManager(self.ui, self.task_list).Run()
