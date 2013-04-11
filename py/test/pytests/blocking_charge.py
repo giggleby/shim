e773994412687bb6584f72c87279334f9f33a700
Shawn Nematbakhsh <shawnn@google.com>
2013-04-11 16:02:52 -0700

    STOUT-FACTORY: blocking_charge: Allow delta charge amount.
    Rather than taking an absolute charge value, allow blocking_charge to
    take a delta value: It will pass when charge becomes (current_charge +
    delta)% or 100%, whichever is smaller.
    
    BUG=chrome-os-partner:17786
    TEST=Manual. Run test image with delta charge amount.
    
    Change-Id: I0de820b2e1d05cc9523a562275817ffbb70e434c
    Reviewed-on: https://gerrit.chromium.org/gerrit/47926
    Commit-Queue: Shawn Nematbakhsh <shawnn@chromium.org>
    Tested-by: Shawn Nematbakhsh <shawnn@chromium.org>
    Reviewed-by: Bowgo Tsai <bowgotsai@chromium.org>

--- py/test/pytests/blocking_charge.py
+++ py/test/pytests/blocking_charge.py
@@ -44,6 +44,9 @@ class ChargerTest(unittest.TestCase):
   ARGS = [
       Arg('target_charge_pct', int, 'Target charge level',
           default=78),
+      Arg('target_charge_pct_is_delta', bool,
+          'Specify target_charge_pct is a delta of current charge',
+          default=False),
       Arg('timeout_secs', int, 'Maximum allowed time to charge battery',
           default=3600),
       ]
@@ -63,7 +66,10 @@ class ChargerTest(unittest.TestCase):
   def Charge(self):
     start_charge = self._power.GetChargePct()
     self.assertTrue(start_charge, 'Error getting battery state.')
-    if start_charge >= self.args.target_charge_pct:
+    target_charge = self.args.target_charge_pct
+    if self.args.target_charge_pct_is_delta is True:
+      target_charge = min(target_charge + start_charge, 100)
+    if start_charge >= target_charge:
       return
     self._ec.SetChargeState(EC.ChargeState.CHARGE)
     self._template.SetState(MakeSpriteHTMLTag('charging_sprite.png', 256, 256))
@@ -71,14 +77,14 @@ class ChargerTest(unittest.TestCase):
 
     for elapsed in xrange(self.args.timeout_secs):
       charge = self._power.GetChargePct()
-      if charge >= self.args.target_charge_pct:
+      if charge >= target_charge:
         return
       self._ui.RunJS('$("batteryIcon").style.backgroundPosition = "-%dpx 0px"' %
                      ((elapsed % 4) * 256))
       self._template.SetInstruction(MakeChargeTextLabel(
                                       start_charge,
                                       charge,
-                                      self.args.target_charge_pct,
+                                      target_charge,
                                       elapsed,
                                       self.args.timeout_secs - elapsed))
       if elapsed % 300 == 0:
@@ -88,7 +94,7 @@ class ChargerTest(unittest.TestCase):
       time.sleep(1)
 
     self.fail('Cannot charge battery to %d%% in %d seconds.' %
-              (self.args.target_charge_pct, self.args.timeout_secs))
+              (target_charge, self.args.timeout_secs))
 
   def runTest(self):
     self._thread.start()
