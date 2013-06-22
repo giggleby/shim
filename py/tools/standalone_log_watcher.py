23055f35d3ef220dbcfbd707a5f1fe74cb48b06d
Vic Yang <victoryang@chromium.org>
2013-06-22 12:30:16 +0800

    Fix a bug that factory log is overwritten
    BUG=None
    TEST=None
    
    Change-Id: I196fb6530b80d64be5f3d5570086460045f36c40
    Signed-off-by: Vic Yang <victoryang@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/59672
    Reviewed-by: Jon Salz <jsalz@chromium.org>

--- py/tools/standalone_log_watcher.py
+++ py/tools/standalone_log_watcher.py
@@ -7,6 +7,7 @@
 import time
 
 import factory_common  # pylint: disable=W0611
+from cros.factory.test import factory
 from cros.factory.utils.process_utils import Spawn
 
 COLOR_RED = '\033[1;31m'
@@ -36,7 +37,7 @@ class StandaloneLogWatcher(object):
         f.writelines([mark_line, mark_line, msg + '\n\n',
                       action + '\n', mark_line, mark_line])
     else:
-      with open('/var/factory/log/factory.log', 'w') as f:
+      with open(factory.FACTORY_LOG_PATH, 'a') as f:
         f.write(msg + '\n')
 
   def WatchForever(self):
