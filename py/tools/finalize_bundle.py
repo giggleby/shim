0dc23a31bb44671d55be137c376cdf879db71140
bowgotsai <bowgotsai@chromium.org>
2013-06-24 12:48:51 +0800

    CHERRY-PICK: finalize_bundle: Add support for depthcharge netboot firmware.
    For netboot in depthcharge, it has below differences comapred to U-boot.
    
    1. Use 'update_firmware_settings.py instead of update_firmware_var.py
       to assign the IP address of omahaserver and tftpserver.
       ./update_firmware_settings.py --clobber \
       --input image.net.bin  --output image.net.bin.out \
       --omahaserver=http://x.x.x.x:8080/update \
       --tftpserverip=x.x.x.x
    
    2. Need to truncate the first 64 bytes of netboot image: vmlinux.uimg
    
    3. Default name of netboot image used in tftpserver is
       changed to vmlinux.bin
    
    BUG=chrome-os-partner:20121
    CQ-DEPEND=CL:59710,CL:*40079
    TEST=Run ./finalize_bundle.py ~/factory_bundle_falco_20130624_evt/.
         And verify that netboot works.
    
    Change-Id: I5bf2ce8dd40c738c8113fdd45e96f95dcb796e1c
    Reviewed-on: https://gerrit.chromium.org/gerrit/59904
    Commit-Queue: Bowgo Tsai <bowgotsai@chromium.org>
    Reviewed-by: Bowgo Tsai <bowgotsai@chromium.org>
    Tested-by: Bowgo Tsai <bowgotsai@chromium.org>

--- py/tools/finalize_bundle.py
+++ py/tools/finalize_bundle.py
@@ -645,6 +645,62 @@ class FinalizeBundle(object):
       self._WriteWithSudo(lsb_factory_path, lsb_factory)
       return True
 
+    def UpdateNetbootURL():
+      """Updates Omaha & TFTP servers' URL in netboot firmware.
+
+      It takes care of both uboot and depthcharge firmware, if presents.
+      """
+      UpdateUbootNetboot()
+      UpdateDepthchargeNetboot()
+
+    def UpdateUbootNetboot():
+      # Updates Omaha & TFTP servers' URL in uboot netboot firmware.
+      netboot_firmware_image = os.path.join(
+          self.bundle_dir, 'netboot_firmware',
+          'nv_image-%s.bin' % self.simple_board)
+      if os.path.exists(netboot_firmware_image):
+        update_firmware_vars = os.path.join(
+            self.bundle_dir, 'factory_setup', 'update_firmware_vars.py')
+        new_netboot_firmware_image = netboot_firmware_image + '.INPROGRESS'
+        Spawn([update_firmware_vars,
+               '--force',
+               '-i', netboot_firmware_image,
+               '-o', new_netboot_firmware_image,
+               '--omahaserver=%s' % mini_omaha_url,
+               '--tftpserverip=%s' %
+                 urlparse.urlparse(mini_omaha_url).hostname],
+              check_call=True, log=True)
+        shutil.move(new_netboot_firmware_image, netboot_firmware_image)
+
+    def UpdateDepthchargeNetboot():
+      # Updates Omaha & TFTP servers' URL in depthcharge netboot firmware.
+      netboot_firmware_image = os.path.join(
+          self.bundle_dir, 'netboot_firmware', 'image.net.bin')
+      if os.path.exists(netboot_firmware_image):
+        update_firmware_settings = os.path.join(
+            self.bundle_dir, 'factory_setup', 'update_firmware_settings.py')
+        new_netboot_firmware_image = netboot_firmware_image + '.INPROGRESS'
+        Spawn([update_firmware_settings,
+               '--clobber',
+               '--input', netboot_firmware_image,
+               '--output', new_netboot_firmware_image,
+               '--omahaserver=%s' % mini_omaha_url,
+               '--tftpserverip=%s' %
+                 urlparse.urlparse(mini_omaha_url).hostname],
+              check_call=True, log=True)
+        shutil.move(new_netboot_firmware_image, netboot_firmware_image)
+
+        # For netboot in depthcharge, we need to truncate the first 64 bytes.
+        # The default name is also changed to 'vmlinux.bin'.
+        # Keep both of the files so everyone can be aware of the difference.
+        netboot_image = os.path.join(self.bundle_dir, 'factory_shim',
+                                     'netboot', 'vmlinux.uimg')
+        new_netboot_image = os.path.join(self.bundle_dir, 'factory_shim',
+                                         'netboot', 'vmlinux.bin')
+        Spawn(['dd', 'if=' + netboot_image, 'of=' + new_netboot_image,
+               'skip=64', 'bs=1'],
+              sudo=True, check_call=True, log=True)
+
     # Patch in the install shim, if present.
     shims = glob.glob(os.path.join(self.bundle_dir, 'factory_shim',
                                    'chromeos_*_factory*.bin'))
@@ -687,21 +743,7 @@ class FinalizeBundle(object):
                   check_call=True, log=True)
             shutil.move(new_netboot_image, netboot_image)
 
-    # Take care of netboot firmware, if present.
-    netboot_firmware_image = os.path.join(self.bundle_dir, 'netboot_firmware',
-        'nv_image-%s.bin' % self.simple_board)
-    if os.path.exists(netboot_firmware_image):
-      update_firmware_vars = os.path.join(self.bundle_dir, 'factory_setup',
-                                         'update_firmware_vars.py')
-      new_netboot_firmware_image = netboot_firmware_image + '.INPROGRESS'
-      Spawn([update_firmware_vars,
-             '--force',
-             '-i', netboot_firmware_image,
-             '-o', new_netboot_firmware_image,
-             '--omahaserver=%s' % mini_omaha_url,
-             '--tftpserverip=%s' % urlparse.urlparse(mini_omaha_url).hostname],
-             check_call=True, log=True)
-      shutil.move(new_netboot_firmware_image, netboot_firmware_image)
+    UpdateNetbootURL()
 
   def MakeFactoryPackages(self):
     release_images = glob.glob(os.path.join(self.bundle_dir, 'release/*.bin'))
