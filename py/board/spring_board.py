21e46ce4e05fcdbee82ba718a120c6c71bec79b8
Cheng-Yi Chiang <cychiang@chromium.org>
2013-08-15 02:26:22 -0700

    Revert "Workaround charging test USB current ILIM issue"
    This reverts commit c326440f66c433faf7192e50954477d1e0996451
    
    Change-Id: Ifa859e9a3fdbda9476936c183457dc3775ff1be2
    Reviewed-on: https://gerrit.chromium.org/gerrit/65949
    Commit-Queue: Cheng-Yi Chiang <cychiang@chromium.org>
    Reviewed-by: Cheng-Yi Chiang <cychiang@chromium.org>
    Tested-by: Cheng-Yi Chiang <cychiang@chromium.org>

--- py/board/spring_board.py
+++ py/board/spring_board.py
@@ -4,7 +4,6 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import time
 import factory_common  # pylint: disable=W0611
 
 from cros.factory.board.chromeos_board import ChromeOSBoard
@@ -43,9 +42,6 @@ class SpringBoard(ChromeOSBoard):
       if state == Board.ChargeState.CHARGE:
         self._CallECTool(['extpwrcurrentlimit', '9999'])
         self._CallECTool(['gpioset', 'charger_en', '1'])
-        self._CallECTool(['gpioset', 'boost_en', '0'])
-        time.sleep(1)
-        self._CallECTool(['gpioset', 'boost_en', '1'])
       elif state == Board.ChargeState.IDLE:
         self._CallECTool(['extpwrcurrentlimit', '9999'])
         self._CallECTool(['gpioset', 'charger_en', '0'])
