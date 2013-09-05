ddfda59f812106eb487f95a338b35ee2af47e0cb
Cheng-Yi Chiang <cychiang@chromium.org>
2013-09-05 15:37:48 +0800

    write_device_data_to_vpd: support both rw_vpd and ro_vpd
    Change argument so the test can support both rw_vpd and ro_vpd
    
    BUG=chrome-os-partner:22328
    TEST=run test with argument ro_vpd and rw_vpd set to some device data,
      check they are written in ro_vpd and rw_vpd
    
    Reviewed-on: https://chromium-review.googlesource.com/168183
    Reviewed-by: Dean Liao <deanliao@chromium.org>
    Commit-Queue: Cheng-Yi Chiang <cychiang@chromium.org>
    Tested-by: Cheng-Yi Chiang <cychiang@chromium.org>
    
    (cherry picked from commit cdc0ba68129f14a1905e8199f334f5b39ac47869)
    
    Change-Id: I47df01cd8f87440dff0c6c27c89a9a2e986f1fd1
    Reviewed-on: https://chromium-review.googlesource.com/177192
    Reviewed-by: Ricky Liang <jcliang@chromium.org>
    Commit-Queue: Ricky Liang <jcliang@chromium.org>
    Tested-by: Ricky Liang <jcliang@chromium.org>

--- py/test/pytests/write_device_data_to_vpd.py
+++ py/test/pytests/write_device_data_to_vpd.py
@@ -4,8 +4,13 @@
 # found in the LICENSE file.
 
 
-"""Writes a subset device data to the RW VPD.
+"""Writes a subset device data to VPD.
 
+Arguments:
+  rw_keys: Keys to write to RW VPD.
+  rw_prefix: Prefix to use when writing keys to RW VPD.
+  ro_keys: Keys to write to RO VPD.
+  ro_prefix: Prefix to use when writing ro_keys to RO VPD.
 Data is all written as strings."""
 
 
@@ -22,25 +27,33 @@ from cros.factory.test.args import Arg
 
 class CallShopfloor(unittest.TestCase):
   ARGS = [
-    Arg('keys', (list, tuple), 'Keys to write to the VPD.'),
-    Arg('prefix', str, 'Prefix to use when writing keys to the VPD.',
+    Arg('rw_keys', (list, tuple), 'Keys to write to RW VPD.'),
+    Arg('rw_prefix', str, 'Prefix to use when writing keys to RW VPD.',
         default='factory.device_data.'),
-
+    Arg('ro_keys', (list, tuple), 'Keys to write to RO VPD', default=None,
+        optional=True),
+    Arg('ro_prefix', str, 'Prefix to use when writing keys to RO VPD.',
+        default='factory.device_data.')
   ]
 
   def runTest(self):
     ui = test_ui.UI()
     template = ui_templates.OneSection(ui)
-    template.SetState(test_ui.MakeLabel(
-        'Writing device data to RW VPD...',
-        '机器资料正在写入到 RW VPD...'))
-
     device_data = shopfloor.GetDeviceData()
-    data_to_write = dict((k, device_data.get(k))
-                         for k in self.args.keys)
-    missing_keys = [k for k, v in data_to_write.iteritems() if v is None]
-    if missing_keys:
-      self.fail('Missing device data keys: %r' % sorted(missing_keys))
+    for label, keys, prefix in [('rw', self.args.rw_keys, self.args.rw_prefix),
+                                ('ro', self.args.ro_keys, self.args.ro_prefix)]:
+      if not keys:
+        continue
+      template.SetState(test_ui.MakeLabel(
+          'Writing device data to %s VPD...' % label.upper(),
+          '机器资料正在写入到 %s VPD...' % label.upper()))
 
-    vpd.rw.Update(dict((self.args.prefix + k, str(v))
-                       for k, v in data_to_write.iteritems()))
+      data_to_write = dict((k, device_data.get(k))
+                           for k in keys)
+      missing_keys = [k for k, v in data_to_write.iteritems() if v is None]
+      if missing_keys:
+        self.fail('Missing device data keys for %s vpd: %r' % (
+            label.upper(), sorted(missing_keys)))
+
+      getattr(vpd, label).Update(dict((prefix + k, str(v))
+                                      for k, v in data_to_write.iteritems()))
