78059fcdfbd0b34a93d8e08d674c1697f47dd8a8
Vic Yang <victoryang@chromium.org>
2013-10-01 11:52:34 +0800

    Change display test to show only three gray colors
    (factory-spring-4262.B only)
    
    This is a temporary hack to change the display test to only show the
    three gray colors we want. On ToT, we should refactor this test so that
    the colors and the names of the colors can be passed in from dargs.
    
    BUG=chrome-os-partner:22978
    TEST=Run the test. See only three colors listed.
    
    Change-Id: I8250a9b433ac00388e96c55c41e028fb3fefffab
    Signed-off-by: Vic Yang <victoryang@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/171350
    Reviewed-by: Cheng-Yi Chiang <cychiang@chromium.org>

--- py/test/pytests/display_static/display.js
+++ py/test/pytests/display_static/display.js
@@ -15,7 +15,10 @@ DisplayTest = function(container) {
   this.styleDiv = null;
   this.fullScreenElement = null;
   this.styleList = [
-    "display-subtest-solid-red",
+    "display-subtest-solid-gray-170",
+    "display-subtest-solid-gray-127",
+    "display-subtest-solid-gray-63",
+    /*"display-subtest-solid-red",
     "display-subtest-solid-green",
     "display-subtest-solid-blue",
     "display-subtest-solid-white",
@@ -26,10 +29,13 @@ DisplayTest = function(container) {
     "display-subtest-gradient-red",
     "display-subtest-gradient-green",
     "display-subtest-gradient-blue",
-    "display-subtest-gradient-white",
+    "display-subtest-gradient-white",*/
   ];
   this.enItemList = [
-    "solid-red",
+    "solid-gray-170",
+    "solid-gray-127",
+    "solid-gray-63",
+    /*"solid-red",
     "solid-green",
     "solid-blue",
     "solid-white",
@@ -40,10 +46,13 @@ DisplayTest = function(container) {
     "gradient-red",
     "gradient-green",
     "gradient-blue",
-    "gradient-white",
+    "gradient-white",*/
   ];
   this.zhItemList = [
-    "红色",
+    "灰色170",
+    "灰色127",
+    "灰色63",
+    /*"红色",
     "绿色",
     "蓝色",
     "白色",
@@ -54,7 +63,7 @@ DisplayTest = function(container) {
     "渐红",
     "渐绿",
     "渐蓝",
-    "渐白",
+    "渐白",*/
   ];
   this.enPassed = "Passed";
   this.zhPassed = "通过";
@@ -95,6 +104,8 @@ function setupDisplayTest(container) {
  * There is a table with itemNumber rows and two columns.
  */
 DisplayTest.prototype.init = function() {
+  this.itemNumber = this.enItemList.length;
+
   var caption = document.createElement("div");
   caption.className = "display-caption";
   appendSpanEnZh(caption, this.enInstruct, this.zhInstruct);
