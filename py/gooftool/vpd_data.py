deb5407844cd41e34009cb3ddb39e8a88f2ddd18
Cheng-Yi Chiang <cychiang@chromium.org>
2013-10-08 17:58:08 +0800

    vpd_data: update INITIAL_LOCALE
    BUG=chrome-os-partner:23187
    TEST=run WriteHWID
    
    Change-Id: I558c50979ec9b234bdc66caf5649770bbea3f900
    Reviewed-on: https://chromium-review.googlesource.com/172231
    Reviewed-by: Ricky Liang <jcliang@chromium.org>
    Commit-Queue: Cheng-Yi Chiang <cychiang@chromium.org>
    Tested-by: Cheng-Yi Chiang <cychiang@chromium.org>

--- py/gooftool/vpd_data.py
+++ py/gooftool/vpd_data.py
@@ -68,141 +68,141 @@ KEYBOARD_LAYOUT = [
   'xkb:mn::mon',
   ]
 
-# initial_locale: http://git.chromium.org/gitweb/?p=chromium.git;a=blob;f=ui/base/l10n/l10n_util.cc
+# initial_locale: https://code.google.com/p/chromium/codesearch#chromium/src/ui/base/l10n/l10n_util.cc&q=l10n_util.cc&sq=package:chromium&type=cs
 INITIAL_LOCALE = [
-  "af",
-  "am",
-  "ar",
-  "az",
-  "be",
-  "bg",
-  "bh",
-  "bn",
-  "br",
-  "bs",
-  "ca",
-  "co",
-  "cs",
-  "cy",
-  "da",
-  "de",
-  "de-AT",
-  "de-CH",
-  "de-DE",
-  "el",
-  "en",
-  "en-AU",
-  "en-CA",
-  "en-GB",
-  "en-NZ",
-  "en-US",
-  "en-ZA",
-  "eo",
-  "es",
-  "es-419",
-  "et",
-  "eu",
-  "fa",
-  "fi",
-  "fil",
-  "fo",
-  "fr",
-  "fr-CA",
-  "fr-CH",
-  "fr-FR",
-  "fy",
-  "ga",
-  "gd",
-  "gl",
-  "gn",
-  "gu",
-  "ha",
-  "haw",
-  "he",
-  "hi",
-  "hr",
-  "hu",
-  "hy",
-  "ia",
-  "id",
-  "is",
-  "it",
-  "it-CH",
-  "it-IT",
-  "ja",
-  "jw",
-  "ka",
-  "kk",
-  "km",
-  "kn",
-  "ko",
-  "ku",
-  "ky",
-  "la",
-  "ln",
-  "lo",
-  "lt",
-  "lv",
-  "mk",
-  "ml",
-  "mn",
-  "mo",
-  "mr",
-  "ms",
-  "mt",
-  "nb",
-  "ne",
-  "nl",
-  "nn",
-  "no",
-  "oc",
-  "om",
-  "or",
-  "pa",
-  "pl",
-  "ps",
-  "pt",
-  "pt-BR",
-  "pt-PT",
-  "qu",
-  "rm",
-  "ro",
-  "ru",
-  "sd",
-  "sh",
-  "si",
-  "sk",
-  "sl",
-  "sn",
-  "so",
-  "sq",
-  "sr",
-  "st",
-  "su",
-  "sv",
-  "sw",
-  "ta",
-  "te",
-  "tg",
-  "th",
-  "ti",
-  "tk",
-  "to",
-  "tr",
-  "tt",
-  "tw",
-  "ug",
-  "uk",
-  "ur",
-  "uz",
-  "vi",
-  "xh",
-  "yi",
-  "yo",
-  "zh",
-  "zh-CN",
-  "zh-TW",
-  "zu",
+  "af",     # Afrikaans
+  "am",     # Amharic
+  "ar",     # Arabic
+  "az",     # Azerbaijani
+  "be",     # Belarusian
+  "bg",     # Bulgarian
+  "bh",     # Bihari
+  "bn",     # Bengali
+  "br",     # Breton
+  "bs",     # Bosnian
+  "ca",     # Catalan
+  "co",     # Corsican
+  "cs",     # Czech
+  "cy",     # Welsh
+  "da",     # Danish
+  "de",     # German
+  "de-AT",  # German (Austria)
+  "de-CH",  # German (Switzerland)
+  "de-DE",  # German (Germany)
+  "el",     # Greek
+  "en",     # English
+  "en-AU",  # English (Australia)
+  "en-CA",  # English (Canada)
+  "en-GB",  # English (UK)
+  "en-NZ",  # English (New Zealand)
+  "en-US",  # English (US)
+  "en-ZA",  # English (South Africa)
+  "eo",     # Esperanto
+  "es",     # Spanish
+  "es-419", # Spanish (Latin America)
+  "et",     # Estonian
+  "eu",     # Basque
+  "fa",     # Persian
+  "fi",     # Finnish
+  "fil",    # Filipino
+  "fo",     # Faroese
+  "fr",     # French
+  "fr-CA",  # French (Canada)
+  "fr-CH",  # French (Switzerland)
+  "fr-FR",  # French (France)
+  "fy",     # Frisian
+  "ga",     # Irish
+  "gd",     # Scots Gaelic
+  "gl",     # Galician
+  "gn",     # Guarani
+  "gu",     # Gujarati
+  "ha",     # Hausa
+  "haw",    # Hawaiian
+  "he",     # Hebrew
+  "hi",     # Hindi
+  "hr",     # Croatian
+  "hu",     # Hungarian
+  "hy",     # Armenian
+  "ia",     # Interlingua
+  "id",     # Indonesian
+  "is",     # Icelandic
+  "it",     # Italian
+  "it-CH",  # Italian (Switzerland)
+  "it-IT",  # Italian (Italy)
+  "ja",     # Japanese
+  "jw",     # Javanese
+  "ka",     # Georgian
+  "kk",     # Kazakh
+  "km",     # Cambodian
+  "kn",     # Kannada
+  "ko",     # Korean
+  "ku",     # Kurdish
+  "ky",     # Kyrgyz
+  "la",     # Latin
+  "ln",     # Lingala
+  "lo",     # Laothian
+  "lt",     # Lithuanian
+  "lv",     # Latvian
+  "mk",     # Macedonian
+  "ml",     # Malayalam
+  "mn",     # Mongolian
+  "mo",     # Moldavian
+  "mr",     # Marathi
+  "ms",     # Malay
+  "mt",     # Maltese
+  "nb",     # Norwegian (Bokmal)
+  "ne",     # Nepali
+  "nl",     # Dutch
+  "nn",     # Norwegian (Nynorsk)
+  "no",     # Norwegian
+  "oc",     # Occitan
+  "om",     # Oromo
+  "or",     # Oriya
+  "pa",     # Punjabi
+  "pl",     # Polish
+  "ps",     # Pashto
+  "pt",     # Portuguese
+  "pt-BR",  # Portuguese (Brazil)
+  "pt-PT",  # Portuguese (Portugal)
+  "qu",     # Quechua
+  "rm",     # Romansh
+  "ro",     # Romanian
+  "ru",     # Russian
+  "sd",     # Sindhi
+  "sh",     # Serbo-Croatian
+  "si",     # Sinhalese
+  "sk",     # Slovak
+  "sl",     # Slovenian
+  "sn",     # Shona
+  "so",     # Somali
+  "sq",     # Albanian
+  "sr",     # Serbian
+  "st",     # Sesotho
+  "su",     # Sundanese
+  "sv",     # Swedish
+  "sw",     # Swahili
+  "ta",     # Tamil
+  "te",     # Telugu
+  "tg",     # Tajik
+  "th",     # Thai
+  "ti",     # Tigrinya
+  "tk",     # Turkmen
+  "to",     # Tonga
+  "tr",     # Turkish
+  "tt",     # Tatar
+  "tw",     # Twi
+  "ug",     # Uighur
+  "uk",     # Ukrainian
+  "ur",     # Urdu
+  "uz",     # Uzbek
+  "vi",     # Vietnamese
+  "xh",     # Xhosa
+  "yi",     # Yiddish
+  "yo",     # Yoruba
+  "zh",     # Chinese
+  "zh-CN",  # Chinese (Simplified)
+  "zh-TW",  # Chinese (Traditional)
+  "zu",     # Zulu
   ]
 
 # initial_timezone: http://git.chromium.org/gitweb/?p=chromium.git;a=blob;f=chrome/browser/chromeos/system/timezone_settings.cc
