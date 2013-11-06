1630b2e0e1a8e09d4cfe2f6f34a0298b36291842
bowgotsai <bowgotsai@chromium.org>
2013-11-06 13:51:26 +0800

    CHERRY-PICK: Supporting read from RO_VPD or RW_VPD for read_devcie_data_to_vpd
    BUG=None
    TEST=Manual
    
    Change-Id: I26d7c30b2a2acf1764ab145343e8d8b5c4fc0248
    Original-Change-Id: I91803584a9f89ab304f459b78cc0261a023406a8
    Reviewed-on: https://chromium-review.googlesource.com/175892
    Tested-by: Bowgo Tsai <bowgotsai@chromium.org>
    Reviewed-by: Heng-ruey Hsu <henryhsu@chromium.org>
    Commit-Queue: Bowgo Tsai <bowgotsai@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/186089
    Reviewed-by: Bowgo Tsai <bowgotsai@chromium.org>
    Commit-Queue: Chia-Hsiu Chang <chia-hsiu.chang@quantatw.com>
    Tested-by: Chia-Hsiu Chang <chia-hsiu.chang@quantatw.com>

--- py/test/pytests/read_device_data_from_vpd.py
+++ py/test/pytests/read_device_data_from_vpd.py
@@ -21,6 +21,10 @@ from cros.factory.test import ui_templates
 from cros.factory.test.args import Arg
 
 
+_MSG_READING_VPD = lambda vpd_section: test_ui.MakeLabel(
+    'Reading device data from %s VPD...' % vpd_section.upper(),
+    '正在从 %s VPD 读机器资料...' % vpd_section.upper())
+
 class CallShopfloor(unittest.TestCase):
   ARGS = [
     Arg('prefix', str,
@@ -28,18 +32,24 @@ class CallShopfloor(unittest.TestCase):
          'stripped; e.g., if there is a VPD entry for '
          'factory.device_data.foo=bar, then foo=bar is added to device_data.'),
         default='factory.device_data.'),
+    Arg('vpd_section', str,
+        'It should be rw or ro which means RW_VPD or RO_VPD to read.',
+        default='rw', optional=True),
   ]
 
   def runTest(self):
+    if self.args.vpd_section not in ['ro', 'rw']:
+      self.fail('Invalid vpd_section:% r, should be %r or %r.' %
+          (self.args.vpd_section, 'ro', 'rw'))
+
     ui = test_ui.UI()
     template = ui_templates.OneSection(ui)
-    template.SetState(test_ui.MakeLabel(
-        'Reading device data from RW VPD...',
-        '正在从 RW VPD 读机器资料...'))
+    template.SetState(_MSG_READING_VPD(self.args.vpd_section))
 
-    vpd_data = vpd.rw.GetAll()
+    vpd_data = getattr(vpd, self.args.vpd_section).GetAll()
     shopfloor.UpdateDeviceData(
         dict((k[len(self.args.prefix):], v)
              for k, v in vpd_data.iteritems() if k.startswith(
                  self.args.prefix)))
+
     factory.get_state_instance().UpdateSkippedTests()
