cbe9fc33909386b8f7f834a4b86d7d877d82c8b8
Joel Kitching <kitching@google.com>
2014-09-11 10:22:27 +0800

    wireless: Modify throughput test to use iperf instead of downloading a test file from the AP.
    CQ-DEPEND=CL:*176100
    BUG=None
    TEST=Manually test
    
    Change-Id: I4b1784f935653cf375db88d211ac94a1cd51866c
    Reviewed-on: https://chromium-review.googlesource.com/217462
    Reviewed-by: Mao Huang <littlecvr@chromium.org>
    Tested-by: Joel Kitching <kitching@google.com>
    Commit-Queue: Mao Huang <littlecvr@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/218090
    Reviewed-by: Joel Kitching <kitching@google.com>
    Commit-Queue: Joel Kitching <kitching@google.com>

--- py/test/pytests/wireless.py
+++ py/test/pytests/wireless.py
@@ -4,21 +4,17 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-'''A factory test for basic Wifi and LTE connectivity.
+'''A factory test for basic WiFi and LTE connectivity.
 
 The test accepts a list of wireless services, checks for their signal strength,
-connects to them, and optionally tests data transmission by connecting to an
-URL.
+connects to them, and optionally tests data throughput rate using iperf.
 '''
 
 import dbus
 import logging
-import os
 import sys
-import tempfile
 import time
 import unittest
-import urllib2
 
 from cros.factory.event_log import Log
 from cros.factory.goofy.service_manager import GetServiceStatus
@@ -28,7 +24,7 @@ from cros.factory.test import factory
 from cros.factory.test.args import Arg
 from cros.factory.utils.net_utils import GetWLANInterface
 from cros.factory.utils.net_utils import GetEthernetIp
-from cros.factory.utils.process_utils import Spawn, CheckOutput
+from cros.factory.utils.process_utils import Spawn, SpawnOutput
 
 try:
   sys.path.append('/usr/local/lib/flimflam/test')
@@ -37,8 +33,7 @@ except:  # pylint: disable=W0702
   pass
 
 
-_SERVICE_LIST = ['shill', 'shill_respawn', 'wpasupplicant',
-                 'modemmanager']
+_SERVICE_LIST = ['shill', 'shill_respawn', 'wpasupplicant', 'modemmanager']
 
 
 def FlimGetService(flim, name):
@@ -77,87 +72,100 @@ def FlimConfigureService(flim, name, password):
 class WirelessTest(unittest.TestCase):
   ARGS = [
     Arg('services', (list, tuple),
-        'A list of Wifi or LTE service (name, password) tuple to test.'
+        'A list of WiFi or LTE services (name, password) tuple to test.'
         ' e.g. [("ssid1", "password1"), ("ssid2", "password2")].'
-        ' Set password to None or "" if it is open network.'
-        'If services are not specified, this test will check for any AP',
+        ' Set password to None or "" if it is an open network.'
+        'If services are not specified, this test will check for any AP.',
         optional=True),
     Arg('min_signal_quality', int,
-        'Minimun signal strength required. (range from 0 to 100)',
+        'Minimum signal strength required (range from 0 to 100).',
         optional=True),
-    Arg('test_url', str, 'URL for testing data transmission.',
-        optional=True),
-    Arg('throughput_threshold', (int, float),
-        'Required minimum throughput in bytes/sec.',
+    Arg('host', str,
+        'Host running iperf in server mode, used for testing data '
+        'transmission speed.',
         optional=True, default=None),
-    Arg('md5sum', str, 'md5sum of the test file in test_url',
-        optional=True),
-    Arg('msecs_before_retries', int,
-        'Milliseconds before next retry when downloading the file.',
-        optional=True, default=1000),
+    Arg('transmit_time', int,
+        'Time in seconds for which to transmit data.',
+        optional=True, default=10),
+    Arg('transmit_interval', (int, float),
+        'There will be an overall average of transmission speed.  But it may '
+        'also be useful to check bandwidth within subintervals of this time. '
+        'This argument can be used to check bandwidth for every interval of n '
+        'seconds.  There will be floor(transmit_time / n) intervals, and any '
+        'remaining time < transmit_time will not be independently reported.',
+        optional=True, default=1),
+    Arg('throughput_threshold', int,
+        'Required minimum throughput in bytes/sec.  If any intervals or the '
+        'overall throughput is lower than this, we will fail.',
+        optional=True, default=None),
   ]
 
-  def _DownloadFileAndMeasureThroughput(self):
-    '''Tries to download a file, checks its md5sum, and measures throughput.
+  def _RunIperfClientAndParseThroughput(self):
+    '''Invokes an iperf client and parses throughput.
 
     This function uses the following data members:
-      self.args.test_url: the URL to download.
-      self.args.md5sum: the expected md5sum of the file.
-      self.args.msecs_before_retries: delay before next retry.
+      self.args.host: the host running an iperf server.
+      self.args.transmit_time: total time (seconds) to transmit data.
+      self.args.transmit_interval: time (seconds) of each sub-interval.
 
     Returns:
       A dict containing:
-        result: True if the function successfully downloaded the file and
-            verified its md5sum, False otherwise.
-        file_size: file size in bytes.
-        time_spent: time spent in seconds.
-        throughput: throughput in bytes/seccond.
-      file_size, time_spent, and throughput are valid only if result is True.
+        result: True if the function successfully connected to the iperf server,
+            False otherwise.
+        raw_output: raw CSV data output from iperf.
+        throughput: a list of throughputs in bytes/second, where a[:-1] are for
+            intervals and a[-1] is overall.
+      raw_output and throughput are only valid if result is True.
     '''
-    logging.info('Try connecting to %s', self.args.test_url)
-    for i in range(5): # pylint: disable=W0612
-      try:
-        remote_file = urllib2.urlopen(self.args.test_url, timeout=2)
-      except urllib2.HTTPError as e:
-        factory.console.info(
-            'Connected to %s but got status code %d: %s.',
-            self.args.test_url, e.code, e.reason)
-      except urllib2.URLError as e:
-        factory.console.info(
-            'Failed to connect to %s: %s.', self.args.test_url, e.reason)
-      else:
-        with tempfile.NamedTemporaryFile() as local_file:
-          # Download file and measure time.
-          start = time.time()
-          file_content = remote_file.read()
-          time_spent = time.time() - start
-          local_file.write(file_content)
-          local_file.flush()
-          os.fdatasync(local_file)
-          # Calculate size and throughput.
-          file_size = os.path.getsize(local_file.name)
-          throughput = file_size / time_spent
-          # Calculate md5sum.
-          md5sum_output = CheckOutput(['md5sum', local_file.name],
-                                      log=True).strip().split()[0]
-        logging.info('Got local file md5sum %s', md5sum_output)
-        logging.info('Golden file md5sum %s', self.args.md5sum)
-        if md5sum_output == self.args.md5sum:
-          factory.console.info(
-              'Successfully connected to %s, file size: %d bytes, '
-              'time spent: %f sec, throughput: %d bytes/sec',
-              self.args.test_url, file_size, time_spent, throughput)
-          return {'result': True,
-                  'file_size': file_size,
-                  'time_spent': time_spent,
-                  'throughput': throughput}
-      # If failed, delay before retry.
-      time.sleep(self.args.msecs_before_retries / 1000.0)
+    logging.info('Running iperf connecting to host %s for %d seconds',
+        self.args.host, self.args.transmit_time)
+    iperf_cmd = ['iperf',
+        '--client', self.args.host,
+        '--time', str(self.args.transmit_time),
+        '--interval', str(self.args.transmit_interval),
+        '--reportstyle', 'c']
+    # We enclose the iperf call in timeout, since when given an unreachable
+    # host, it seems to hang indefinitely.
+    timeout_cmd = ['timeout',
+        '--signal', 'KILL',
+        # Add 5 seconds to allow for process overhead and connection time.
+        str(self.args.transmit_time + 5)] + iperf_cmd
+    iperf_output = SpawnOutput(timeout_cmd, log=True, ignore_stderr=True)
 
-    return {'result': False,
-            'file_size': 0,
-            'time_spent': 0,
-            'throughput': 0}
+    # iperf outputs CSV with the following as its columns, with rows[:-1] for
+    # intervals, and a[-1] for overall values:
+    # 0: timestamp
+    # 1: source_address
+    # 2: source_port
+    # 3: destination_address
+    # 4: destination_port
+    # 5: [unclear?]
+    # 6: interval
+    # 7: transferred_bytes
+    # 8: bits_per_second
+    # Check for properly-formatted output (CSV with 9 columns).  Also, ensure
+    # that transferred_bytes > 0.  If these conditions fail, we can assume iperf
+    # failed.
+    iperf_output_table = \
+        [x.split(',') for x in iperf_output.strip().split("\n")]
+    if len(iperf_output_table[0]) != 9 or int(iperf_output_table[0][8]) <= 0:
+      factory.console.info(
+          'Failed to make a connection to %s, or received bogus '
+          'output from iperf.', self.args.host)
+      return {'result': False,
+              'raw_output': iperf_output,
+              'throughput': []}
+
+    # We only need (bits_per_second / 8) as our return list of throughputs.
+    throughputs = [int(x[8]) / 8.0 for x in iperf_output_table]
+    factory.console.info(
+        'Successfully connected to %s, transferred: %d bytes, '
+        'time spent: %d sec, throughput: %d bytes/sec',
+        self.args.host, int(iperf_output_table[-1][7]) / 8.0,
+        self.args.transmit_time, throughputs[-1])
+    return {'result': True,
+            'raw_output': iperf_output,
+            'throughput': throughputs}
 
   def setUp(self):
     for service in _SERVICE_LIST:
@@ -221,21 +229,29 @@ class WirelessTest(unittest.TestCase):
 
         Spawn(['ifconfig'], check_call=True, log=True)
 
-        # Try to download file if needed.
-        if self.args.test_url is not None:
-          ret = self._DownloadFileAndMeasureThroughput()
-          Log('downloading_file',
+        # Try to test throughput with iperf if needed.
+        if self.args.host is not None:
+          ret = self._RunIperfClientAndParseThroughput()
+          Log('throughput_test',
               result=ret['result'],
-              file_size=ret['file_size'],
-              time_spent=ret['time_spent'],
+              raw_output=ret['raw_output'],
               throughput=ret['throughput'])
-          if not ret['result']:
-            self.fail('Failed to connect to url %s' % self.args.test_url)
-          if (self.args.throughput_threshold is not None and
-              ret['throughput'] < self.args.throughput_threshold):
-            self.fail(
-                "Throughput: %f < %f bytes/sec didn't meet the requirement." % (
-                    ret['throughput'], self.args.throughput_threshold))
 
-        logging.info('Disconnecting %s', name)
+          if not ret['result']:
+            self.fail(
+                'Failed to make a connection to %s, or received bogus '
+                'output from iperf.' % self.args.host)
+
+          if self.args.throughput_threshold is not None:
+            # We want to ensure that ALL measured throughputs are over the
+            # threshold.  Filter throughputs for failed cases.
+            failed_throughputs = [x for x in ret['throughput']
+                if x < self.args.throughput_threshold]
+            if failed_throughputs:
+              self.fail(
+                  'Throughputs: %s < %d bytes/sec didn\'t meet '
+                  'the threshold requirement.' % (
+                      str(["%d" % x for x in ret['throughput']]),
+                      self.args.throughput_threshold))
+
         flim.DisconnectService(service)
