a827bb0952c4c734dacbcb2089d62f4d2be49406
Cheng-Yi Chiang <cychiang@chromium.org>
2014-09-12 15:11:30 +0800

    logparser: Do not check serial number format
    BUG=chrome-os-partner:31950
    TEST=check logparser can work
    
    Change-Id: I3748dca25910a5d54360c0f6ff183e9c90289f2b
    Reviewed-on: https://chromium-review.googlesource.com/217734
    Reviewed-by: Heng-ruey Hsu <henryhsu@google.com>
    Commit-Queue: Cheng-Yi Chiang <cychiang@chromium.org>
    Tested-by: Cheng-Yi Chiang <cychiang@chromium.org>

--- py/tools/logparser.py
+++ py/tools/logparser.py
@@ -35,7 +35,9 @@ _CAMERA_MAPPING_FILE = '%s/tarfiles/camera_mapping' % _BASE_DIR
 
 # File pattern setting
 _SERIAL_DIGIT = 10
-_FILE_NAME_RE = re.compile(r'(\w+?)_(\w{%d})_(\d{17})$' % _SERIAL_DIGIT)
+# HACK: We do not specify serial digit because they are not following
+# the same format.
+_FILE_NAME_RE = re.compile(r'(\w+?)_(\w+?)_(\d{17})$')
 _FILE_EXT_RE = re.compile(r'.(tgz|zip|tar)$')
 
 # Event Log setting
