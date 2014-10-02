07329af287d885e371631b6d17fc68be8cada300
bowgotsai <bowgotsai@chromium.org>
2014-10-02 16:15:06 +0800

    CHERRY-PICK: gooftool: support probeing eMMC 5.0 firmware version
    BUG=chrome-os-partner:32573
    TEST=unittest
    TEST=Manual on my DUT
    
    Original-Change-Id: Iff1b855a9fce784f6a5594d028cf05480297cc42
    Reviewed-on: https://chromium-review.googlesource.com/221030
    Tested-by: Bowgo Tsai <bowgotsai@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>
    Commit-Queue: Bowgo Tsai <bowgotsai@chromium.org>
    (cherry picked from commit 79d5d032e5fa9971aca83e3c9865b5ff8f2e1355)
    
    Change-Id: Id7c01575303f339f15d7a1004de9956f417e7f54
    Reviewed-on: https://chromium-review.googlesource.com/221896
    Reviewed-by: Bowgo Tsai <bowgotsai@chromium.org>
    Commit-Queue: Bowgo Tsai <bowgotsai@chromium.org>
    Tested-by: Bowgo Tsai <bowgotsai@chromium.org>

---
+++ py/gooftool/probe_unittest.py
@@ -0,0 +1,77 @@
+#!/bin/env python
+# Copyright 2014 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import mox
+import unittest
+
+import factory_common # pylint: disable=W0611
+from cros.factory.common import Obj
+from cros.factory.gooftool import probe
+
+
+class GetEMMC5FirmwareVersionUnittest(unittest.TestCase):
+  def setUp(self):
+    self.mox = mox.Mox()
+    self.mox.StubOutWithMock(probe, 'Shell')
+
+  def tearDown(self):
+    self.mox.UnsetStubs()
+
+  def testFirmwareVersionASCII(self):
+    mock_stdout = """
+[FIRMWARE_VERSION[261]]: 0x48
+[FIRMWARE_VERSION[260]]: 0x47
+[FIRMWARE_VERSION[259]]: 0x46
+[FIRMWARE_VERSION[258]]: 0x45
+[FIRMWARE_VERSION[257]]: 0x44
+[FIRMWARE_VERSION[256]]: 0x43
+[FIRMWARE_VERSION[255]]: 0x42
+[FIRMWARE_VERSION[254]]: 0x41
+"""
+    probe.Shell('mmc extcsd read /dev/mmcblk0').AndReturn(
+        Obj(stdout=mock_stdout))
+    self.mox.ReplayAll()
+
+    result = probe._GetEMMC5FirmwareVersion(  # pylint: disable=W0212
+        '/sys/class/block/mmcblk0')
+    self.assertEquals('4142434445464748 (ABCDEFGH)', result)
+    self.mox.VerifyAll()
+
+  def testFirmwareVersionHex(self):
+    mock_stdout = """
+[FIRMWARE_VERSION[261]]: 0x00
+[FIRMWARE_VERSION[260]]: 0x00
+[FIRMWARE_VERSION[259]]: 0x00
+[FIRMWARE_VERSION[258]]: 0x00
+[FIRMWARE_VERSION[257]]: 0x00
+[FIRMWARE_VERSION[256]]: 0x00
+[FIRMWARE_VERSION[255]]: 0x00
+[FIRMWARE_VERSION[254]]: 0x03
+"""
+    probe.Shell('mmc extcsd read /dev/mmcblk0').AndReturn(
+        Obj(stdout=mock_stdout))
+    self.mox.ReplayAll()
+
+    result = probe._GetEMMC5FirmwareVersion(  # pylint: disable=W0212
+        '/sys/class/block/mmcblk0')
+    self.assertEquals('0300000000000000 (3)', result)
+    self.mox.VerifyAll()
+
+  def testFirmwareVersionNotFound(self):
+    mock_stdout = """
+No Firmware version.
+"""
+    probe.Shell('mmc extcsd read /dev/mmcblk0').AndReturn(
+        Obj(stdout=mock_stdout))
+    self.mox.ReplayAll()
+
+    result = probe._GetEMMC5FirmwareVersion(  # pylint: disable=W0212
+        '/sys/class/block/mmcblk0')
+    self.assertTrue(result is None)
+    self.mox.VerifyAll()
+
+
+if __name__ == '__main__':
+  unittest.main()
