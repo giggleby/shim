aa1009cbee9fdce66703cd2a759fd90c9ee3d860
Gediminas Ramanauskas <gedis@google.com>
2014-12-21 14:41:58 -0800

    gooftool probe: add 'dw-hdmi-audio' to KNOWN_INVALID_CODEC_NAMES
    dw-hdmi-audio appears during probe when hdmi is connected on veyron
    derivatives - but is itself not a real codec, thus blacklisting it.
    
    Originally reviewed at
    https://chromium-review.googlesource.com/#/c/237077
    
    Bringing into the factory branch as it's needed now while ToT
    is still under review.
    
    BUG=none
    TEST=manually at the factory
    
    Change-Id: Ia29f0e9f12fa6207fe4620e8e56f5358684daa1b
    Reviewed-on: https://chromium-review.googlesource.com/237730
    Reviewed-by: Katie Roberts-Hoffman <katierh@chromium.org>
    Commit-Queue: Katie Roberts-Hoffman <katierh@chromium.org>
    Tested-by: Shunqian Zheng <zhengsq@rock-chips.com>

--- py/gooftool/probe.py
+++ py/gooftool/probe.py
@@ -534,8 +534,11 @@ def _ProbeAudioCodecArm():
   There is a set of known invalid codec names that are not included in the
   return value.
   """
-  # ts3a227e.4-003b is autonomous autiojack switch, not an audio codec
-  KNOWN_INVALID_CODEC_NAMES = set(['snd-soc-dummy', 'ts3a227e.4-003b'])
+  KNOWN_INVALID_CODEC_NAMES = set([
+    'snd-soc-dummy',
+    'ts3a227e.4-003b',  # autonomous autiojack switch, not an audio codec
+    'dw-hdmi-audio'  # virtual driver that only exists when hdmi is connected
+    ])
   with open('/sys/kernel/debug/asoc/codecs') as f:
     return [DictCompactProbeStr(codec) for codec in
             filter(lambda value: value not in KNOWN_INVALID_CODEC_NAMES,
