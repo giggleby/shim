ba957bb97ed2ef0209ab8ac470ec6f3c9b5cd23e
Wei-Ning Huang <wnhuang@google.com>
2015-01-20 21:09:51 +0800

    CHERRY-PICK: ethernet: implement link_only check to see if link is up
    When link_only=True, the test only check if the iterface can be
    found on DUT. We can also specify use_swconfig to use swconfig
    command to check if link is up.
    
    BUG=chrome-os-partner:34902
    TEST=run ethernet test with link_only=True and iface set
    TEST=run ethernet test with link_only=True and use_swconfig=True
    
    Original-Change-Id: I6009e79c63e5da635e356f0509eb75a4c3767ae4
    Reviewed-on: https://chromium-review.googlesource.com/241933
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>
    Commit-Queue: Wei-Ning Huang <wnhuang@chromium.org>
    Tested-by: Wei-Ning Huang <wnhuang@chromium.org>
    
    Change-Id: I04a185d88977bda1667b97bfc529672ed6431c62
    Reviewed-on: https://chromium-review.googlesource.com/245945
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>
    Commit-Queue: Wei-Ning Huang <wnhuang@chromium.org>
    Tested-by: Wei-Ning Huang <wnhuang@chromium.org>

--- py/test/pytests/ethernet.py
+++ py/test/pytests/ethernet.py
@@ -12,6 +12,7 @@ import time
 import unittest
 import urllib2
 
+import factory_common  # pylint: disable=W0611
 from cros.factory.test import factory
 from cros.factory.test import test_ui
 from cros.factory.test.args import Arg
@@ -55,17 +56,26 @@ window.onkeydown = function(event) {
 
 _LOCAL_FILE_PATH = '/tmp/test'
 
+
 class EthernetTest(unittest.TestCase):
   """Test built-in ethernet port"""
   ARGS = [
-    Arg('auto_start', bool, 'Auto start option.', False),
-    Arg('test_url', str, 'URL for testing data transmission.',
-        optional=True),
-    Arg('md5sum', str, 'md5sum of the test file in test_url.',
-        optional=True),
-    Arg('retry_interval_msecs', int,
-        'Milliseconds before next retry.',
-        default=1000),
+      Arg('auto_start', bool, 'Auto start option.', False),
+      Arg('test_url', str, 'URL for testing data transmission.',
+          optional=True),
+      Arg('md5sum', str, 'md5sum of the test file in test_url.',
+          optional=True),
+      Arg('retry_interval_msecs', int,
+          'Milliseconds before next retry.',
+          default=1000),
+      Arg('iface', str, 'Interface name for testing.', default=None,
+          optional=True),
+      Arg('link_only', bool, 'Only test if link is up or not', default=False),
+      Arg('use_swconfig', bool, 'Use swconfig for polling link status.',
+          default=False),
+      Arg('swconfig_switch', str, 'swconfig switch name.', default='switch0'),
+      Arg('swconfig_ports', (int, list), 'swconfig port numbers. Either '
+          'a single int or a list of int.', default=None, optional=True)
   ]
 
   def setUp(self):
@@ -79,6 +89,21 @@ class EthernetTest(unittest.TestCase):
     self.ui.CallJSFunction('init', self.args.auto_start)
     if bool(self.args.test_url) != bool(self.args.md5sum):
       raise ValueError('Should both assign test_url and md5sum.')
+    if self.args.use_swconfig:
+      if not self.args.link_only:
+        raise ValueError('Should set link_only=True if use_swconfig is set.')
+      if self.args.swconfig_ports is None:
+        raise ValueError('Should assign swconfig_ports if use_swconfig is'
+                         'set.')
+    elif self.args.link_only and not self.args.iface:
+      raise ValueError('Should assign iface if link_only is set.')
+
+  def GetInterface(self):
+    devices = GetEthernetInterfaces()
+    if self.args.iface:
+      return self.args.iface if self.args.iface in devices else None
+    else:
+      return self.GetCandidateInterface()
 
   def GetCandidateInterface(self):
     devices = GetEthernetInterfaces()
@@ -106,7 +131,7 @@ class EthernetTest(unittest.TestCase):
       factory.console.info(
           'Failed to connect to %s: %s.', self.args.test_url, e.reason)
     else:
-      with open(_LOCAL_FILE_PATH, "w") as local_file:
+      with open(_LOCAL_FILE_PATH, 'w') as local_file:
         local_file.write(remote_file.read())
         local_file.flush()
         os.fdatasync(local_file)
@@ -122,25 +147,64 @@ class EthernetTest(unittest.TestCase):
         factory.console.info('md5 checksum error')
     return False
 
+  def CheckLinkSimple(self, dev):
+    if dev:
+      with open('/sys/class/net/%s/carrier' % dev, 'r') as f:
+        status = f.read().strip()
+      if int(status):
+        self.ui.Pass()
+        return True
+      else:
+        self.ui.Fail('Link is down on dev %s' % dev)
+        return False
+
+  def CheckLinkSWconfig(self):
+    if type(self.args.swconfig_ports) == int:
+      self.args.swconfig_ports = [self.args.swconfig_ports]
+
+    for i in self.args.swconfig_ports:
+      status = CheckOutput(['swconfig', 'dev', self.args.swconfig_switch,
+                            'port', str(i), 'get', 'link'])
+      if 'up' in status:
+        factory.console.info('Link is up on switch %s port %d',
+                             self.args.swconfig_switch, i)
+      else:
+        self.ui.Fail('Link is down on switch %s port %d' %
+                     (self.args.swconfig_switch, i))
+        return False
+
+    self.ui.Pass()
+    return True
+
   def StartTest(self, event):  # pylint: disable=W0613
     # Only retry 5 times
-    for i in xrange(5): # pylint: disable=W0612
-      eth = self.GetCandidateInterface()
-      if eth:
-        if self.args.test_url is None:
-          ethernet_ip = GetEthernetIp(eth)
-          if ethernet_ip:
-            factory.console.info('Get ethernet IP %s for %s',
-                ethernet_ip, eth)
-            self.ui.Pass()
+    for i in xrange(5):  # pylint: disable=W0612
+      eth = self.GetInterface()
+      if self.args.link_only:
+        if not self.args.use_swconfig:
+          if self.CheckLinkSimple(eth):
             break
         else:
-          if self.GetFile():
-            self.ui.Pass()
+          if self.CheckLinkSWconfig():
             break
-      time.sleep(self.args.retry_interval_msecs / 1000.0)
+      else:
+        if eth:
+          if self.args.test_url is None:
+            ethernet_ip = GetEthernetIp(eth)
+            if ethernet_ip:
+              factory.console.info('Get ethernet IP %s for %s',
+                                   ethernet_ip, eth)
+              self.ui.Pass()
+              break
+          else:
+            if self.GetFile():
+              self.ui.Pass()
+              break
+        time.sleep(self.args.retry_interval_msecs / 1000.0)
 
-    if self.args.test_url is None:
+    if self.args.link_only:
+      self.ui.Fail('Cannot find interface %s' % self.args.iface)
+    elif self.args.test_url is None:
       self.ui.Fail('Cannot get ethernet IP')
     else:
       self.ui.Fail('Failed to download url %s' % self.args.test_url)
