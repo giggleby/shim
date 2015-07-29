c819cb14d24bade33d0de81c1bc442eb72919a17
Chun-Ta Lin <itspeter@google.com>
2015-07-29 15:06:28 +0800

    More features to show_debug_statistic test.
    - Show all statistic: caller now can assign an empty list means to show
      all known statistic.
    - Clear all statistic: an option to clear all statistic
    - Shrink the font size so more statistic can be shown
    - Shopfloor bugs fixed.
    
    BUG=chrome-os-partner:43392
    TEST=make lint-overlay-whirlwind LINT_WHITELIST=" \
      py/test/pytests/show_debug_statistic.py"
    TEST=run_pytest show_debug_statistic --args "{'clear_all': True}"
    
    Change-Id: I57b2d6dd799dcf054d2102004a5ec2a2476eb552
    Reviewed-on: https://chromium-review.googlesource.com/289678
    Reviewed-by: Chun-ta Lin <itspeter@chromium.org>
    Commit-Queue: Chun-ta Lin <itspeter@chromium.org>
    Trybot-Ready: Chun-ta Lin <itspeter@chromium.org>
    Tested-by: Chun-ta Lin <itspeter@chromium.org>

--- py/test/pytests/show_debug_statistic.py
+++ py/test/pytests/show_debug_statistic.py
@@ -34,44 +34,62 @@ class ShowDebugStat(unittest.TestCase):
   ARGS = [
       Arg('prefix_of_debug_keys', list,
           ('A list of the prefix of the debug information. <prefix>_total '
-           'and <prefix>_success will be extracted to compute.'),
+           'and <prefix>_success will be extracted to compute. Empty list '
+           'will show all in the device-data.'),
           optional=False, default=[]),
       Arg('save_to_shopfloor', bool,
           'True to save the data to the shopfloor in a CSV form.',
           default=False, optional=True),
+      Arg('clear_all', bool,
+          'True to clear all statitic information and shows no UI at all.',
+          default=False, optional=True),
   ]
 
   def runTest(self):
-    ui = test_ui.UI(css=CSS)
     device_data = shopfloor.GetDeviceData()
+    if self.args.clear_all:
+      keys_to_del = [key for key in device_data.iterkeys() if (
+          key.endswith('_total') or key.endswith('_success'))]
+      shopfloor.DeleteDeviceData(keys_to_del)
+      return
+
+    ui = test_ui.UI(css=CSS)
     serial_number = device_data.get('mlb_serial_number', 'unknown')
 
     csv_header = ['serial_number']
     csv_row = [serial_number]
     table = []
-    for prefix in self.args.prefix_of_debug_keys:
-      total = device_data.get('%s_total' % prefix, 0)
-      success = device_data.get('%s_success' % prefix, 0)
-      csv_header.append('%s_total' % prefix)
-      csv_header.append('%s_success' % prefix)
+    for key in sorted(device_data.iterkeys()):
+      if not key.endswith('_total'):
+        continue
+
+      prefix = key[1:-6]
+      if (prefix not in self.args.prefix_of_debug_keys and
+          self.args.prefix_of_debug_keys):
+        continue
+      total = device_data.get('_%s_total' % prefix, 0)
+      success = device_data.get('_%s_success' % prefix, 0)
+      csv_header.append('_%s_total' % prefix)
+      csv_header.append('_%s_success' % prefix)
       csv_row.append(str(total))
       csv_row.append(str(success))
 
       status = 'passed' if success == total else 'failed-and-waived'
-      table.append(('<tr class="test-status-%s" style="font-size: 300%%">'
+      table.append(('<tr class="test-status-%s" style="font-size: 250%%">'
                     '<th>%s</th><td>%s</td></tr>') %
                    (status, prefix, '%s / %s' % (success, total)))
 
     # Save to the shopfloor if save_to_shopfloor
-    logging.info('Uploading debug statistic as CSV to shopfloor.')
-    csv_filename = 'debug_stat_%s_%s.csv' % (
-        datetime.datetime.now().strftime('%Y%m%d%H%M%S%f')[:-3],  # time
-        serial_number)
-    csv_content = '%s\n%s' % (','.join(csv_header), ','.join(csv_row))
-    shopfloor_server = shopfloor.GetShopfloorConnection()
-    shopfloor_server.SaveAuxLog(
-        posixpath.join('debug_stat', csv_filename),
-        xmlrpclib.Binary(csv_content))
+    if self.args.save_to_shopfloor:
+      logging.info('Uploading debug statistic as CSV to shopfloor.')
+      csv_filename = 'debug_stat_%s_%s.csv' % (
+          datetime.datetime.now().strftime('%Y%m%d%H%M%S%f')[:-3],  # time
+          serial_number)
+      csv_content = '%s\n%s' % (','.join(csv_header), ','.join(csv_row))
+      shopfloor_server = shopfloor.GetShopfloorConnection()
+      shopfloor_server.SaveAuxLog(
+          posixpath.join('debug_stat', csv_filename),
+          xmlrpclib.Binary(csv_content))
 
     html = [
         '<div class="test-vcenter-outer"><div class="test-vcenter-inner">',
