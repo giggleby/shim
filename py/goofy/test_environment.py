867cab11eee755f91b293e4eec3d9cb5a41450bc
Wei-Ning Huang <wnhuang@google.com>
2015-10-13 06:42:44 +0800

    HACK: increase websocket wait timeout
    Cyan devices take longer time to start to chrome, increase timeout to
    avoid problems.
    
    BUG=chrome-os-partner:44107
    TEST=manually
    
    Change-Id: Idc82a3e82f0b54d66860b88064a86832cb8e5841
    Reviewed-on: https://chromium-review.googlesource.com/307214
    Reviewed-by: Wei-Ning Huang <wnhuang@chromium.org>
    Commit-Queue: Wei-Ning Huang <wnhuang@chromium.org>
    Tested-by: Wei-Ning Huang <wnhuang@chromium.org>

--- py/goofy/test_environment.py
+++ py/goofy/test_environment.py
@@ -150,7 +150,7 @@ class DUTEnvironment(Environment):
 
   def launch_chrome(self):
     self.override_chrome_start_pages()
-    sync_utils.WaitFor(self.has_sockets, 30)
+    sync_utils.WaitFor(self.has_sockets, 90)
     subprocess.check_call(['initctl', 'emit', 'login-prompt-visible'])
 
   def create_connection_manager(self, wlans, scan_wifi_period_secs,
