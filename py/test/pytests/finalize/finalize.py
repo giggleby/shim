eeea4951e7c60fbc8d545c9c72cb0f484d42e416
Shun-Hsing Ou <shunhsingou@chromium.org>
2016-06-17 11:32:02 +0800

    CHERRY-PICK: finalize: allow informing shopfloor before wiping
    After we have in-place wiping, we allow factory to send finalize record to
    shopfloor server after device gets fully wiped. However, some ODMs need
    special settings and fail to connect to ethernet after wiping, e.g.
    using WiFi for network connection. Add an test list argument to disable
    this function so factory can choose to make record before wiping on their own
    decision.
    
    BUG=chrome-os-partner:544021
    TEST=Manually test on edgar device.
    
    Original Change-Id: I5122b8dfc81e724f272d828e248f7268cdf56e91
    Original Reviewed-on: https://chromium-review.googlesource.com/353397
    Original Commit-Ready: Shun-Hsing Ou <shunhsingou@chromium.org>
    Original Tested-by: Cindy Kuo <Cindy_Kuo@pegatroncorp.com>
    Original Reviewed-by: Hung-Te Lin <hungte@chromium.org>
    
    Conflicts:
    	py/test/pytests/finalize/finalize.py
    
    Change-Id: I458ba4a00dbc90420c2a3bc7b0c4e100fb5c2959
    Reviewed-on: https://chromium-review.googlesource.com/354310
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>
    Reviewed-by: Michael H Yang <michael.h.yang@intel.com>
    Reviewed-by: Cindy Kuo <Cindy_Kuo@pegatroncorp.com>
    Commit-Queue: Cindy Kuo <Cindy_Kuo@pegatroncorp.com>
    Tested-by: Cindy Kuo <Cindy_Kuo@pegatroncorp.com>

--- py/test/pytests/finalize/finalize.py
+++ py/test/pytests/finalize/finalize.py
@@ -122,6 +122,12 @@ class Finalize(unittest.TestCase):
           'False for legacy implementation to invoke wiping under '
           'release image after reboot.',
           default=False, optional=True),
+      Arg('inform_shopfloor_after_wipe', bool,
+          'Inform shopfloor server that the device is finalized after it gets'
+          'wiped. For in-place wipe, it is recommended to set to True so'
+          'a shopfloor call can be made AFTER device gets wiped successfully.'
+          'For legacy wipe, shopfloor call is always made before wiping.',
+          default=True),
       Arg('cutoff_options', dict,
           'Battery cutoff options after wiping. Only used when wipe_in_place'
           'is set to true. Should be a dict with following optional keys:\n'
@@ -457,7 +463,9 @@ class Finalize(unittest.TestCase):
         for key, value in self.args.cutoff_options.iteritems():
           cutoff_args += ' --%s %s' % (key.replace('_', '-'), str(value))
         command += ' --cutoff_args "%s"' % cutoff_args
-      if shopfloor.is_enabled():
+      if (self.args.inform_shopfloor_after_wipe and
+          shopfloor.is_enabled() and
+          shopfloor.get_shopfloor_handler_uri()):
         command += ' --shopfloor_url "%s"' % shopfloor.get_server_url()
 
     command += ' --upload_method "%s"' % upload_method
@@ -473,6 +481,10 @@ class Finalize(unittest.TestCase):
           ' '.join(self.args.enforced_release_channels))
       logging.info(
           'Enforced release channels: %s.', self.args.enforced_release_channels)
+
+    if self.args.wipe_in_place:
+      if not self.args.inform_shopfloor_after_wipe and shopfloor.is_enabled():
+        shopfloor.finalize()  # notify shopfloor
 
     gooftools.run(command)
 
