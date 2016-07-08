4e2bd69646798635b6c25fc3205543646511fcf3
Wei-Han Chen <stimim@google.com>
2016-07-08 14:49:12 +0800

    CHERRY-PICK ONLY: ghost file for station.
    MID will be "test-list name" + "wifi MAC".
    Property file will be "${test-list name}-properties.json", if this file
    doesn't exist, "default-properties.json" will be used.
    
    TEST=manual
    BUG=None
    
    Change-Id: I1f530ba2ca366e9d67baae5c9b939ed92e938b93
    Reviewed-on: https://chromium-review.googlesource.com/384402
    Reviewed-by: Wei-Han Chen <stimim@chromium.org>
    Commit-Queue: Wei-Han Chen <stimim@chromium.org>
    Tested-by: Wei-Han Chen <stimim@chromium.org>

--- init/goofy.d/device/ghost.sh
+++ init/goofy.d/device/ghost.sh
@@ -6,4 +6,16 @@
 FACTORY_BASE="/usr/local/factory"
 
 export PATH=${FACTORY_BASE}/bin:${FACTORY_BASE}/bin/overlord:$PATH
-${FACTORY_BASE}/bin/ghost --fork > /dev/null 2>&1
+
+TEST_LIST="$(factory test-list | tail -n1)"
+MAC="$(iw dev | grep addr | cut -d' ' -f2)"
+
+PROPERTIES_FILE="${FACTORY_BASE}/board/${TEST_LIST}-properties.json"
+if ! [ -e "${PROPERTIES_FILE}" ]; then
+  PROPERTIES_FILE="${FACTORY_BASE}/board/default-properties.json"
+fi
+
+${FACTORY_BASE}/bin/ghost \
+    --fork 172.23.104.2 \
+    --mid "${TEST_LIST}-${MAC}" \
+    --prop-file "${PROPERTIES_FILE}" > /dev/null 2>&1
