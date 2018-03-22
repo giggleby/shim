6fa30c05a07d0ac1c06325bc1ab36490c0421f4a
Daniel Johansson <dajo@google.com>
2018-03-22 20:02:59 +0100

    Fizz-factory: Add test case for HDMI CEC
    Add pytest for CEC
    
    This depends on EC firmware support
    https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/973226
    
    CQ-DEPEND=CL:973446
    BUG=None
    TEST=Local test on desk
    
    Change-Id: If17fc3c5127a259222833d3988e93def4d1e7791
    Reviewed-on: https://chromium-review.googlesource.com/976124
    Reviewed-by: Yong Hong <yhong@google.com>
    Commit-Queue: Dennis Wu <denniswu@google.com>
    Tested-by: Dennis Wu <denniswu@google.com>

---
+++ py/test/pytests/display_cec.py
@@ -0,0 +1,112 @@
+# Copyright 2018 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import time
+import subprocess
+
+import factory_common  # pylint: disable=unused-import
+from cros.factory.test import test_ui
+from cros.factory.utils import process_utils
+from cros.factory.utils import type_utils
+
+_MSG_CEC_SELF_TEST_INFO = ('Please disconnect HDMI, then press SPACE<br>' +
+                           'after one second reconnect HDMI')
+
+_MSG_CEC_MANUAL_INFO = ('The TV will turn off and then on again<br>' +
+                        'Press SPACE to start the test')
+
+_MSG_CEC_TEST = ('Did the TV turn off then on again?<br>' +
+                 'Press SPACE if yes, "F" if no.')
+
+_HTML_CEC = '<div id="cec-title"></div>'
+
+_CSS_CEC = """
+  #cec-title {
+    font-size: 2em;
+    width: 70%;
+  }
+"""
+
+class DisplayCecTest(test_ui.TestCaseWithUI):
+
+  def setUp(self):
+    self.ui.AppendCSS(_CSS_CEC)
+    self.template.SetState(_HTML_CEC)
+
+  def runTest(self):
+    self.ui.SetHTML(_MSG_CEC_SELF_TEST_INFO, id='cec-title')
+    self.ui.WaitKeysOnce(test_ui.SPACE_KEY)
+    # Self test
+    # Check that CEC_IN and CEC_PULL_UP is connected
+    self.InvokeGPIO('set', 'CEC_OUT', 1)
+    self.InvokeGPIO('set', 'CEC_PULL_UP', 1)
+    self.InvokeGPIO('get', 'CEC_IN', 1)
+    self.InvokeGPIO('set', 'CEC_PULL_UP', 0)
+    self.InvokeGPIO('get', 'CEC_IN', 0)
+
+    # Check that CEC_OUT is connected with the others
+    self.InvokeGPIO('set', 'CEC_OUT', 0)
+    self.InvokeGPIO('set', 'CEC_PULL_UP', 1)
+    self.InvokeGPIO('get', 'CEC_IN', 0)
+
+    # Restore to default settings
+    self.InvokeGPIO('set', 'CEC_OUT', 1)
+    self.InvokeGPIO('set', 'CEC_PULL_UP', 1)
+
+    #Manual test
+    self.ui.SetHTML(_MSG_CEC_MANUAL_INFO, id='cec-title')
+    self.ui.WaitKeysOnce(test_ui.SPACE_KEY)
+
+    self.Ectool(['cec', 'tv_off'])
+    time.sleep(1)
+    self.Ectool(['cec', 'status'])
+    time.sleep(10)
+    self.Ectool(['cec', 'tv_on'])
+    time.sleep(1)
+    self.Ectool(['cec', 'status'])
+
+    self.ui.SetHTML(_MSG_CEC_TEST, id='cec-title')
+    key = self.ui.WaitKeysOnce([test_ui.SPACE_KEY] + ['F'])
+    if key == 'F':
+      raise type_utils.TestFailure('Failed to send CEC commands')
+    return
+
+  def InvokeGPIO(self, direction, pin_name, value):
+    """Sets or checks GPIO value using 'ectool'.
+
+    Args:
+      direction: 'get' or 'set'
+      pin_name: Name of pin used by 'ectool', e.g. CEC_IN
+      value: If direction=='set' set pin to this value,
+       if direction=='get' assert that this value is read
+
+     Raises:
+       TestFailure if value does not match what is returned by ectool
+
+    """
+    if direction == 'get':
+      ectool_output = self.Ectool(['gpioget', pin_name]).rstrip()
+      desired_string = 'GPIO %s = %d' % (pin_name, value)
+      self.assertEqual(
+          desired_string, ectool_output,
+          'Wrong GPIO value: %s (%s)' % (ectool_output, desired_string))
+    elif direction == 'set':
+      ectool_output = self.Ectool(['gpioset', pin_name, str(value)])
+    else:
+      raise ValueError("direction must be either 'get' or 'set'")
+
+  def Ectool(self, args):
+    """Calls 'ectool' with the given args.
+
+    Args:
+      args: The args to pass along with 'ectool'.
+
+    Raises:
+      TestFailure if the ectool command returns non-zero exit code.
+    """
+    try:
+      return process_utils.CheckOutput(['ectool'] + args, log=True)
+    except subprocess.CalledProcessError as e:
+      raise type_utils.TestFailure(
+          'Non-zero exit code from ectool command: %d' % e.returncode)
