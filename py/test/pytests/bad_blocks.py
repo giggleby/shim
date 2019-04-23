80ca6b7b027ada38fc5d533c1bfe9eecded87d26
Wei-Han Chen <stimim@google.com>
2019-04-23 16:25:39 +0800

    bad_blocks: ignore FAILERR and FAILLOG
    bad_blocks test in factory should be aligned with autotest
    hardware_Smartctl.  The autotest ignores FAILERR and FAILLOG when
    determining if the test passed or not.
    
    In this change, we modify the bad_blocks test to align with the
    autotest.
    
    BUG=b:130521180
    TEST=None
    
    Change-Id: Ie8eda6fac1e9cfbc0234af709ba34e0accb85f8e
    (cherry picked from commit dcdfe12605f4c545ca5a0efc7cc578841ec27fc6)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/1609739
    Reviewed-by: Wei-Han Chen <stimim@chromium.org>
    Tested-by: Darren Chen <darren.chen@quanta.corp-partner.google.com>
    Commit-Queue: Zhuohao Lee <zhuohao@chromium.org>

--- py/test/pytests/bad_blocks.py
+++ py/test/pytests/bad_blocks.py
@@ -18,6 +18,7 @@ import logging
 import re
 from select import select
 import subprocess
+import tempfile
 import time
 
 import factory_common  # pylint: disable=unused-import
@@ -395,8 +396,13 @@ class BadBlocksTest(test_case.TestCase):
       event_log.Log('log_command', command=self.args.extra_log_cmd,
                     stdout=stdout_data, stderr=stderr_data)
 
-    smartctl_output = self.dut.CheckOutput(
-        ['smartctl', '-a', self._filesystem])
+    cmd = ['smartctl', '-a', self._filesystem]
+    with tempfile.TemporaryFile() as stdout:
+      exit_code = self.dut.Call(cmd, stdout=stdout)
+      stdout.flush()
+      stdout.seek(0)
+      smartctl_output = stdout.read()
+
     event_log.Log('smartctl', stdout=smartctl_output)
     logging.info('smartctl output: %s', smartctl_output)
 
@@ -404,6 +410,10 @@ class BadBlocksTest(test_case.TestCase):
         'SMART overall-health self-assessment test result: PASSED'
         in smartctl_output,
         'SMART says drive is not healthy')
+    # Ignore error other than first two bits, this should be aligned to
+    # hardware_Smartctl autotest.
+    self.assertTrue(
+        exit_code & 0x3 == 0, 'smartclt exit code = 0x%x' % exit_code)
 
   def _UpdateSATALinkSpeed(self):
     """Updates the current SATA link speed based on /var/log/messages."""
