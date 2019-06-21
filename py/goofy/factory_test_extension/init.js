ce3ca3467a868a03a4bfa2426a9ec661055a2a9e
Wei-Han Chen <stimim@google.com>
2019-06-21 21:51:47 +0800

    goofy: retry to start factory page every 3 seconds
    There are some race condition between goofy http server and front end
    javascript start time.  If javascript tries to connect to goofy http
    server too soon, it will fail and Chrome browswer would show a white
    screen (at this moment, manual refresh could reload the page).
    
    Add retry to the function so it will connect to goofy http server
    eventually.
    
    BUG=b:132565716
    TEST=reboot device for ~100 times and could not reproduce the issue
    
    Change-Id: I34bf8a751419374c99f1948ed01c9c021d9b786e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/1670841
    Reviewed-by: Wei-Han Chen <stimim@chromium.org>
    Commit-Queue: Wei-Han Chen <stimim@chromium.org>
    Tested-by: Wei-Han Chen <stimim@chromium.org>

--- py/goofy/factory_test_extension/init.js
+++ py/goofy/factory_test_extension/init.js
@@ -2,4 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-chrome.runtime.sendMessage(null, 'StartFactoryPage');
+function StartFactoryPage() {
+  chrome.runtime.sendMessage(null, 'StartFactoryPage');
+
+  setTimeout(StartFactoryPage, 3000);  // retry in 3 seconds.
+}
+
+StartFactoryPage();
