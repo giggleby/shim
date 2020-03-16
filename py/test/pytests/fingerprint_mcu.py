4a9d4a542d86b2dc587d6675f071f8132c76092b
Cheng Yueh <cyueh@google.com>
2020-03-16 13:40:50 +0800

    fingerprint_mcu: Accepts masked ID matching
    Previously we only accept exact match of sensor IDs. In practice we may
    only want to match certain bits in the ID. This CL accepts masked ID
    matching.
    
    BUG=b:150407388, b:168006986
    TEST=Manually test on devices.
    
    Change-Id: I47705201f72cad4fb7e79e27e38beab241ab991a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/2108467
    Reviewed-by: Pin-yen Lin <treapking@chromium.org>
    Tested-by: Cheng Yueh <cyueh@chromium.org>
    Commit-Queue: Cheng Yueh <cyueh@chromium.org>
    (cherry picked from commit dd7f79c72a343a9e63c9c427823a4a3dae28e6a7)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/2405834

--- py/test/pytests/fingerprint_mcu.py
+++ py/test/pytests/fingerprint_mcu.py
@@ -39,7 +39,10 @@ add this in test list::
     "pytest_name": "fingerprint_mcu",
     "args": {
       "dead_pixel_max": 10,
-      "sensor_hwid": 5132,
+      "sensor_hwid": [
+        1234,
+        [5120, 65520]
+      ],
       "pixel_median": {
         "cb_type1" : [180, 220],
         "cb_type2" : [80, 120],
@@ -81,7 +84,13 @@ class FingerprintTest(unittest.TestCase):
   """Tests the fingerprint sensor."""
   ARGS = [
       Arg('sensor_hwid', (int, list),
-          'The finger sensor Hardware ID exported in the model field.',
+          'The list of rules of accepted finger sensor Hardware IDs. A rule in '
+          'sensor_hwid should be an integer or a list of two integers. If the '
+          'rule is an integer then it is an exact match. Otherwise the ID '
+          'masked by the second integer must match the first integer. If the '
+          'list is empty, the test do not check ID. Otherwise, The test fail '
+          'if all of the rules are not matched. This value could also be an'
+          'integer for backward compability, and it is an exact match.',
           default=None),
       Arg('max_dead_pixels', int,
           'The maximum number of dead pixels on the fingerprint sensor.',
@@ -272,7 +281,11 @@ class FingerprintTest(unittest.TestCase):
     testlog.UpdateParam(
         name='sensor_hwid', description='Sensor Hardware ID register')
     testlog.LogParam('sensor_hwid', model)
-    if expected_hwid and model not in expected_hwid:
+    def Match(rule):
+      if isinstance(rule, int):
+        return model == rule
+      return (model & rule[1]) == rule[0]
+    if expected_hwid and not any(Match(rule) for rule in expected_hwid):
       raise type_utils.TestFailure('Invalid sensor HWID: %r' % model)
 
     # checkerboard test patterns
