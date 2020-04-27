ee5f6047342a9b1431b6d212bdb89cb06397af38
Fei Shao <fshao@chromium.org>
2020-04-27 15:32:06 +0800

    tpm_verify_ek: wait and check TPM is owned before verifying key
    When we tries to acquire TPM ownership via `tpm-manager initialize`, the
    return code implies if the TakeOwnership command is sent successfully
    but not if the initialization is done. To ensure TPM is properly owned
    and password is generated, we need to rely on the response of
    `cryptohome --action=tpm-status`.
    Adding a poll on TPM status after initialization command is sent.
    
    BUG=b:153767839
    TEST=manual on Krane
    
    Change-Id: Id5dd2ae66c84489e81ddc766e75fa009ec0d8b80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/2165693
    Reviewed-by: Andrey Pronin <apronin@chromium.org>
    Reviewed-by: Stimim Chen <stimim@chromium.org>
    Commit-Queue: Fei Shao <fshao@chromium.org>
    Tested-by: Fei Shao <fshao@chromium.org>

--- py/test/pytests/tpm_verify_ek.py
+++ py/test/pytests/tpm_verify_ek.py
@@ -20,6 +20,7 @@ import factory_common  # pylint: disable=unused-import
 from cros.factory.device import device_utils
 from cros.factory.utils.arg_utils import Arg
 from cros.factory.utils import string_utils
+from cros.factory.utils import sync_utils
 
 
 class TPMVerifyEK(unittest.TestCase):
@@ -32,26 +33,26 @@ class TPMVerifyEK(unittest.TestCase):
   def setUp(self):
     self.dut = device_utils.CreateDUTInterface()
 
+  def _TPMStatus(self):
+    """Returns TPM status as a dictionary.
+
+    e.g., {'TPM Being Owned': 'false',
+           'TPM Ready': 'true',
+           'TPM Password': 'd641b63ce6ff',
+           'TPM Enabled': 'true',
+           'TPM Owned': 'true'}
+    """
+    status_txt = self.dut.CheckOutput(['cryptohome', '--action=tpm_status'],
+                                      log=True)
+    status = string_utils.ParseDict(status_txt.splitlines())
+    logging.info('TPM status: %r', status)
+    return status
+
   def VerifyByCryptoHome(self):
     """Verifies TPM endorsement by CryptoHome service."""
 
-    def _TPMStatus():
-      """Returns TPM status as a dictionary.
-
-      e.g., {'TPM Being Owned': 'false',
-             'TPM Ready': 'true',
-             'TPM Password': 'd641b63ce6ff',
-             'TPM Enabled': 'true',
-             'TPM Owned': 'true'}
-      """
-      status_txt = self.dut.CheckOutput(['cryptohome', '--action=tpm_status'],
-                                        log=True)
-      status = string_utils.ParseDict(status_txt.splitlines())
-      logging.info('TPM status: %r', status)
-      return status
-
     # Make sure TPM is enabled.
-    status = _TPMStatus()
+    status = self._TPMStatus()
     self.assertEqual('true', status['TPM Enabled'])
 
     # Check explicitly for the case where TPM is owned but password is
@@ -72,7 +73,7 @@ class TPMVerifyEK(unittest.TestCase):
     # Sync, to make sure TPM password was written to disk.
     self.dut.CheckCall(['sync'], log=True)
 
-    self.assertEqual('true', _TPMStatus()['TPM Owned'])
+    self.assertEqual('true', self._TPMStatus()['TPM Owned'])
 
     # Verify the endorsement key.
     with tempfile.TemporaryFile() as stderr:
@@ -91,6 +92,12 @@ class TPMVerifyEK(unittest.TestCase):
 
     # Take ownership of the TPM (if not already taken).
     self.dut.CheckCall(['tpm-manager', 'initialize'], log=True)
+
+    # Wait until TPM password is ready.
+    sync_utils.PollForCondition(
+        poll_method=self._TPMStatus,
+        condition_method=lambda status: status['TPM Password'],
+        condition_name='TPM password readiness')
 
     # Verify TPM endorsement.
     self.dut.CheckCall(['tpm-manager', 'verify_endorsement'] + (
