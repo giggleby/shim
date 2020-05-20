c3949e13fbd2077af64cc28cba337a21837b912f
Stimim Chen <stimim@google.com>
2020-05-20 13:04:24 +0800

    gooftool: add VerifySnBits
    To check if cr50 sn bits are set properly.
    
    BUG=b:157210082
    TEST=make test
    
    Change-Id: I46fdb594632b4c71531e9bd00728566b83bba4f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/2209001
    Reviewed-by: Stimim Chen <stimim@chromium.org>
    Commit-Queue: Stimim Chen <stimim@chromium.org>
    Tested-by: Stimim Chen <stimim@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/2452319

--- py/gooftool/commands.py
+++ py/gooftool/commands.py
@@ -328,6 +328,11 @@ def VerifyReleaseChannel(options):
       options.enforced_release_channels)
 
 
+@Command('verify-sn-bits')
+def VerifySnBits(options):
+  GetGooftool(options).VerifySnBits()
+
+
 @Command('write_protect')
 def EnableFwWp(options):
   """Enable then verify firmware write protection."""
@@ -484,6 +489,7 @@ def WipeInit(options):
                                 options.station_port,
                                 options.wipe_finish_token)
 
+
 @Command('verify',
          CmdArg('--no_write_protect', action='store_true',
                 help='Do not check write protection switch state.'),
@@ -527,6 +533,7 @@ def Verify(options):
   VerifyTPM(options)
   VerifyVPD(options)
   VerifyReleaseChannel(options)
+  VerifySnBits(options)
 
 
 @Command('untar_stateful_files')
