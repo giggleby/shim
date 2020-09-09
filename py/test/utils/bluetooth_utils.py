arccode/factory

factory: Apply scan timeout in bluetooth test
Previously, We did not apply scan timeout to btmgmt and this causes some
of bluetooth module scan a long time in an environment of tons of
bluetooth devices.

BUG=b:163478166, b:166085631
TEST='factory run SMT.Bluetooth' and the scan time is matched

Change-Id: I200135085caa75363504f4ddb66874186e34bc9f
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/2371984
Tested-by: Cheng Yueh <cyueh@chromium.org>
Reviewed-by: Yilin Yang (kerker) <kerker@chromium.org>
Commit-Queue: Cheng Yueh <cyueh@chromium.org>

py/test/utils/bluetooth_utils.py
@@ -88,15 +88,18 @@ class BtMgmt:
     """Get the HCI device of the bluetooth adapter."""
     return self._hci_device
 
-  def FindDevices(self, index=0):
+  def FindDevices(self, index=0, timeout_secs=None):
     if self._hci_device:
       index = int(self._hci_device.lstrip('hci'))
 
     patt = re.compile(
         r'^hci\d+\sdev_found:\s(.+)\stype\s.+\srssi\s(\-\d+)\s.*$')
     devices = {}
-    for line in process_utils.CheckOutput(
-        ['btmgmt', '--index', str(index), 'find']).splitlines():
+    find_cmd = ['btmgmt', '--index', str(index)]
+    if find_cmd is not None:
+      find_cmd.extend(['--timeout', str(timeout_secs)])
+    find_cmd.append('find')
+    for line in process_utils.CheckOutput(find_cmd, log=True).splitlines():
       if line.startswith('hci'):
         result = patt.match(line)
         if not result:
@@ -112,6 +115,11 @@ class BtMgmt:
       elif line.startswith('name'):
         name = line.lstrip('name ')
         devices[mac]['Name'] = name
+
+    # The timeout just kill the interactive session but it may not stop
+    # discovery. Use stop-find afterwards to guarantee discovery ends.
+    process_utils.CheckOutput(
+        ['btmgmt', '--index', str(index), 'stop-find'], log=True)
 
     return devices
 

giggleby/shim

(cherry picked from commit a15b411aed94326297ce6876aaab3123025a65d9)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/2397256
Reviewed-by: Cheng Yueh <cyueh@chromium.org>
Commit-Queue: Pin-yen Lin <treapking@chromium.org>
Tested-by: Pin-yen Lin <treapking@chromium.org>

py/test/utils/bluetooth_utils.py
@@ -88,15 +88,18 @@ class BtMgmt:
     """Get the HCI device of the bluetooth adapter."""
     return self._hci_device
 
-  def FindDevices(self, index=0):
+  def FindDevices(self, index=0, timeout_secs=None):
     if self._hci_device:
       index = int(self._hci_device.lstrip('hci'))
 
     patt = re.compile(
         r'^hci\d+\sdev_found:\s(.+)\stype\s.+\srssi\s(\-\d+)\s.*$')
     devices = {}
-    for line in process_utils.CheckOutput(
-        ['btmgmt', '--index', str(index), 'find']).splitlines():
+    find_cmd = ['btmgmt', '--index', str(index)]
+    if timeout_secs is not None:
+      find_cmd.extend(['--timeout', str(timeout_secs)])
+    find_cmd.append('find')
+    for line in process_utils.CheckOutput(find_cmd, log=True).splitlines():
       if line.startswith('hci'):
         result = patt.match(line)
         if not result:
@@ -112,6 +115,11 @@ class BtMgmt:
       elif line.startswith('name'):
         name = line.lstrip('name ')
         devices[mac]['Name'] = name
+
+    # The timeout just kill the interactive session but it may not stop
+    # discovery. Use stop-find afterwards to guarantee discovery ends.
+    process_utils.CheckOutput(
+        ['btmgmt', '--index', str(index), 'stop-find'], log=True)
 
     return devices
 