123a8cc07cc3c08bedb60e4b182c3c3c240509dd
Cheng-Han Yang <chenghan@google.com>
2020-10-06 00:18:49 +0800

    pytests/update_fpmcu_firmware: Add a method to check version
    Similar to `update_cr50_firmware` pytest, we add a method to check if
    the fingerprint firmware version is the same as a specified firmware
    image. Also add a `from_release` argument to specify that the given
    firmware path is from the release image.
    
    BUG=b:169259389
    TEST=manual test on DUT
    
    Change-Id: I021e477b0cb4a0a1663f112f223b632c2737f01d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/2449655
    Reviewed-by: Bhanu Prakash Maiya <bhanumaiya@google.com>
    Commit-Queue: Cheng-Han Yang <chenghan@chromium.org>
    Tested-by: Cheng-Han Yang <chenghan@chromium.org>

--- py/test/pytests/update_fpmcu_firmware.py
+++ py/test/pytests/update_fpmcu_firmware.py
@@ -6,8 +6,14 @@
 
 Description
 -----------
-The FPMCU firmware image to update is either from a given path in the
-station or on the DUT, or from the release partition on the DUT.
+This test provides two functionalities, toggled by the test argument ``method``.
+
+1. In `update mode`, this test updates FPMCU firmware. The firmware image is
+   either from a given path in the station or on the DUT, or from the release
+   partition on the DUT.
+2. In `check mode`, this test checks if the running FPMCU firmware version is
+   the same as the given firmware image.
+
 
 Test Procedure
 --------------
@@ -16,14 +22,19 @@ This is an automatic test that doesn't need any user interaction.
 1. Firstly, this test will create a DUT link.
 2. If the FPMCU firmware image is from the station, the image would be sent
    to DUT.
-3. If the FPMCU firmware image is not specified, the test mounts the release
-   partition and get the FPMCU firmware image from there for update.
+3. If the FPMCU firmware image is not specified or the path is in release
+   partition, the test mounts the release partition and get the FPMCU firmware.
+4. If `method` is set to `UPDATE`, DUT runs `flash_fp_mcu` to update FPMCU
+   firmware using the specified FPMCU firmware image.
+5. If `method` is set to `CHECK_VERSION`, DUT runs `futility` and `ectool` to
+   check if the running FPMCU firmware version is the same as the specified
+   FPMCU firmware.
 
 Dependency
 ----------
 - DUT link must be ready before running this test.
 - `flash_fp_mcu` (from ec-utils-test package) on DUT.
-- `futility`, `crossystem`, and `cros_config` on DUT.
+- `futility`, `ectool`, `crossystem`, and `cros_config` on DUT.
 - FPMCU firmware image must be prepared.
 - Hardware write-protection must be disabled (`crossystem wpsw_cur` returns 0).
 
@@ -42,12 +53,24 @@ To update fingerprint firmware with a specified image in the station
   {
     "pytest_name": "update_fpmcu_firmware",
     "args": {
-      "firmware_file": "/path/on/station/to/image.bin"
+      "firmware_file": "/path/on/station/to/image.bin",
+      "from_release": false
+    }
+  }
+
+To check if fingerprint firmware version is the same as the fingerprint firmware
+in the release image::
+
+  {
+    "pytest_name": "update_fpmcu_firmware",
+    "args": {
+      "method": "CHECK_VERSION"
     }
   }
 """
 
 import logging
+import os
 
 from cros.factory.device import device_utils
 from cros.factory.test import session
@@ -56,16 +79,24 @@ from cros.factory.test import test_ui
 from cros.factory.test.utils import fpmcu_utils
 from cros.factory.utils.arg_utils import Arg
 from cros.factory.utils import sys_utils
-from cros.factory.utils.type_utils import Error
+from cros.factory.utils import type_utils
 
 FLASHTOOL = '/usr/local/bin/flash_fp_mcu'
 FPMCU_FW_DIR_UNDER_ROOTFS = 'opt/google/biod/fw'
 
 
 class UpdateFpmcuFirmwareTest(test_case.TestCase):
+  _METHOD_TYPE = type_utils.Enum(['UPDATE', 'CHECK_VERSION'])
+
   ARGS = [
       Arg('firmware_file', str, 'The full path of the firmware binary file.',
-          default=None)
+          default=None),
+      Arg('from_release', bool, 'Find the firmware from release rootfs',
+          default=True),
+      Arg('method', _METHOD_TYPE,
+          'Specify whether to update the FPMCU firmware or to check the '
+          'firmware version.',
+          default=_METHOD_TYPE.UPDATE)
   ]
 
   ui_class = test_ui.ScrollableLogUI
@@ -75,19 +106,18 @@ class UpdateFpmcuFirmwareTest(test_case.TestCase):
     self._fpmcu = fpmcu_utils.FpmcuDevice(self._dut)
 
   def runTest(self):
-    # Before updating FPMCU firmware, HWWP must be disabled.
-    if self._dut.CallOutput(['crossystem', 'wpsw_cur']).strip() != '0':
-      raise Error('Hardware write protection is enabled.')
-
     fpmcu_board = self._dut.CallOutput(
         ['cros_config', '/fingerprint', 'board'])
     if not fpmcu_board:
-      raise Error('No fingerprint board found in cros_config')
+      raise type_utils.Error('No fingerprint board found in cros_config')
 
-    if not self.args.firmware_file:
+    if self.args.firmware_file is None:
       logging.info('No specified path to FPMCU FW image')
-      logging.info('Get FPMCU FW image from the release rootfs partition.')
+      self.assertTrue(
+          self.args.from_release,
+          'Must set "from_release" to True if not specifying firmware_file')
 
+      logging.info('Get FPMCU FW image from the release rootfs partition.')
       with sys_utils.MountPartition(
           self._dut.partitions.RELEASE_ROOTFS.path, dut=self._dut) as root:
         pattern = self._dut.path.join(root, FPMCU_FW_DIR_UNDER_ROOTFS,
@@ -95,20 +125,34 @@ class UpdateFpmcuFirmwareTest(test_case.TestCase):
         fpmcu_fw_files = self._dut.Glob(pattern)
         self.assertEqual(len(fpmcu_fw_files), 1,
                          'No uniquely matched FPMCU firmware blob found')
-        self.UpdateFpmcuFirmware(fpmcu_fw_files[0])
+        self.args.firmware_file = os.path.join(
+            '/', os.path.relpath(fpmcu_fw_files[0], root))
     else:
       self.assertEqual(self.args.firmware_file[0], '/',
                        'firmware_file should be a full path')
+
+    if self.args.method == self._METHOD_TYPE.UPDATE:
+      method_func = self.UpdateFpmcuFirmware
+    else:
+      method_func = self.CheckFpmcuFirmwareVersion
+
+    if self.args.from_release:
+      with sys_utils.MountPartition(
+          self._dut.partitions.RELEASE_ROOTFS.path, dut=self._dut) as root:
+        method_func(os.path.join(root, self.args.firmware_file[1:]))
+    else:
       if self._dut.link.IsLocal():
-        self.UpdateFpmcuFirmware(self.args.firmware_file)
+        method_func(self.args.firmware_file)
       else:
         with self._dut.temp.TempFile() as dut_temp_file:
           self._dut.SendFile(self.args.firmware_file, dut_temp_file)
-          self.UpdateFpmcuFirmware(dut_temp_file)
+          method_func(dut_temp_file)
 
   def UpdateFpmcuFirmware(self, firmware_file):
     """Update FPMCU firmware by `flash_fp_mcu`."""
-    flash_cmd = [FLASHTOOL, firmware_file]
+    # Before updating FPMCU firmware, HWWP must be disabled.
+    if self._dut.CallOutput(['crossystem', 'wpsw_cur']).strip() != '0':
+      raise type_utils.Error('Hardware write protection is enabled.')
 
     try:
       old_ro_ver, old_rw_ver = self._fpmcu.GetFpmcuFirmwareVersion()
@@ -120,15 +164,20 @@ class UpdateFpmcuFirmwareTest(test_case.TestCase):
     logging.info('Ready to update FPMCU firmware to RO: %s, RW: %s.',
                  bin_ro_ver, bin_rw_ver)
 
+    flash_cmd = [FLASHTOOL, firmware_file]
     session.console.debug(self._dut.CallOutput(flash_cmd))
-    new_ro_ver, new_rw_ver = self._fpmcu.GetFpmcuFirmwareVersion()
 
-    self.assertEqual(new_ro_ver, bin_ro_ver,
-                     'New FPMCU RO: %s does not match the expected RO: %s.'
-                     % (new_rw_ver, bin_ro_ver))
-    self.assertEqual(new_rw_ver, bin_rw_ver,
-                     'New FPMCU RW: %s does not match the expected RW: %s.'
-                     % (new_rw_ver, bin_rw_ver))
+  def CheckFpmcuFirmwareVersion(self, firmware_file):
+    """Check if the current FPMCU firmware version matches the firmware blob."""
+    actual_ro_ver, actual_rw_ver = self._fpmcu.GetFpmcuFirmwareVersion()
+    expect_ro_ver, expect_rw_ver = self.GetFirmwareVersionFromFile(
+        firmware_file)
+    self.assertEqual(actual_ro_ver, expect_ro_ver,
+                     'FPMCU RO: %s does not match the expected RO: %s.'
+                     % (actual_ro_ver, expect_ro_ver))
+    self.assertEqual(actual_rw_ver, expect_rw_ver,
+                     'FPMCU RW: %s does not match the expected RW: %s.'
+                     % (actual_rw_ver, expect_rw_ver))
 
   def GetFirmwareVersionFromFile(self, firmware_file):
     """Read RO and RW FW version from the FW binary file."""
@@ -141,7 +190,7 @@ class UpdateFpmcuFirmwareTest(test_case.TestCase):
     get_fmap_cmd = ["futility", "dump_fmap", "-p", firmware_file, area_name]
     get_fmap_output = self._dut.CheckOutput(get_fmap_cmd)
     if not get_fmap_output:
-      raise Error('Fmap area name might be wrong?')
+      raise type_utils.Error('Fmap area name might be wrong?')
     unused_name, offset, size = get_fmap_output.split()
     get_ro_ver_cmd = ["dd", "bs=1", "skip=%s" % offset,
                       "count=%s" % size, "if=%s" % firmware_file]
