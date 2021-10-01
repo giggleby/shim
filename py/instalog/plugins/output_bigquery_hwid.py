d5e9db007114fc7979a10ddfc6d0d03a5874d5f5
Chun-Tsen Kuo <chuntsen@chromium.org>
2021-10-01 18:44:29 +0800

    Instalog: output_bigquery_hwid: Add a plugin to upload HWID data to BigQuery
    For the Component Map Tool, we add the plugin to upload decoded HWID
    data to BigQuery.
    
    BUG=b:201752895
    TEST=manually test
    
    Change-Id: Ia85d59125181fe74e65681849e7ffc28aa39421d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/3199265
    Reviewed-by: Kerker Yang <kerker@chromium.org>
    Tested-by: Chun-Tsen Kuo <chuntsen@chromium.org>
    Commit-Queue: Chun-Tsen Kuo <chuntsen@chromium.org>

---
+++ py/instalog/plugins/output_bigquery_hwid.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+#
+# Copyright 2021 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""BigQuery upload output plugin.
+
+Subclasses OutputBigQuery to create table rows for HWID events.
+"""
+
+import json
+
+# pylint: disable=import-error, no-name-in-module
+from google.cloud.bigquery.schema import SchemaField
+# pylint: enable=import-error, no-name-in-module
+
+from cros.factory.instalog import plugin_base
+from cros.factory.instalog.plugins import output_bigquery
+
+
+class OutputBigQueryHWID(output_bigquery.OutputBigQuery):
+
+  def GetTableSchema(self):
+    """Returns a list of fields in the table schema."""
+    return [
+        SchemaField('components', 'record', 'REPEATED', None, (
+            SchemaField('componentClass', 'string', 'NULLABLE', None, ()),
+            SchemaField('name', 'string', 'NULLABLE', None, ()),
+            SchemaField('componentId', 'integer', 'NULLABLE', None, ()),
+            SchemaField('qualificationId', 'integer', 'NULLABLE', None, ()),
+            SchemaField('fields', 'record', 'REPEATED', None, (
+                SchemaField('name', 'string', 'NULLABLE', None, ()),
+                SchemaField('value', 'string', 'NULLABLE', None, ()),
+            )),
+        )),
+        SchemaField('phase', 'string', 'REQUIRED', None, ()),
+        SchemaField('hwid', 'string', 'REQUIRED', None, ()),
+        SchemaField('time', 'timestamp', 'REQUIRED', None, ()),
+    ]
+
+  def ConvertEventToRow(self, event):
+    """Converts an event to its corresponding BigQuery table row JSON string."""
+    if not event.get('__hwid__', False):
+      return None
+
+    row = {}
+
+    row['components'] = []
+    for component_dict in event.get('components', []):
+      row['components'].append({})
+      row['components'][-1]['componentClass'] = component_dict.get(
+          'componentClass')
+      row['components'][-1]['name'] = component_dict.get('name')
+      if component_dict.get('hasAvl', False):
+        row['components'][-1]['componentId'] = component_dict.get(
+            'avlInfo', {}).get('componentId')
+        row['components'][-1]['qualificationId'] = component_dict.get(
+            'avlInfo', {}).get('qualificationId')
+      row['components'][-1]['fields'] = []
+      for field in component_dict.get('fields', []):
+        row['components'][-1]['fields'].append({})
+        row['components'][-1]['fields'][-1]['name'] = field.get('name')
+        row['components'][-1]['fields'][-1]['value'] = field.get('value')
+
+    row['phase'] = event.get('phase')
+    row['hwid'] = event.get('hwid')
+    row['time'] = event.get('time')
+
+    return json.dumps(row, allow_nan=False)
+
+
+if __name__ == '__main__':
+  plugin_base.main()
