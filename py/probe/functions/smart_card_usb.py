49a674262e26f72d5c7378c7fd884c433f44c1cb
Yong Hong <yhong@chromium.org>
2021-11-09 02:31:46 +0800

    py/probe: implement a probe function for USB smart card readers
    BUG=b:205503819
    TEST=make test
    TEST=manual test with modified `bInterfaceClass` value
    
    Change-Id: I0e8a4fee0363137720304cdb45c6f99acee702c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/3266661
    Reviewed-by: Pin-yen Lin <treapking@chromium.org>
    Tested-by: Yong Hong <yhong@chromium.org>
    Commit-Queue: Yong Hong <yhong@chromium.org>

---
+++ py/probe/functions/smart_card_usb.py
@@ -0,0 +1,64 @@
+# Copyright 2021 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import glob
+import logging
+import os
+import os.path
+
+from cros.factory.probe.functions import sysfs
+from cros.factory.probe.functions import usb
+from cros.factory.probe.lib import cached_probe_function
+
+
+_INTERFACE_CLASS_VALUE = 0x0b
+_INTERFACE_CLASS_FIELD = 'bInterfaceClass'
+
+
+class SmartCardUSBFunction(cached_probe_function.GlobPathCachedProbeFunction):
+  """Probes all USB smart card reader devices.
+
+  Description
+  -----------
+  This function goes through ``/sys/bus/usb/devices/`` to read attributes of
+  each usb device and find out all smart card readers there.  Each result
+  must contain these fields:
+
+  - ``device_path``: Pathname of the sysfs directory.
+  - ``idVendor``
+  - ``idProduct``
+
+  The result might also contain these optional fields if they are exported in
+  the sysfs entry:
+
+  - ``manufacturer``
+  - ``product``
+  - ``bcdDevice``
+  """
+  GLOB_PATH = '/sys/bus/usb/devices/*'
+
+  @classmethod
+  def ProbeDevice(cls, dir_path):
+    # An USB device might have multiple interfaces, each is represented as
+    # a sub-folder inside the device's sysfs node.  We treat a device a
+    # smart card reader if any of its interface has the specific class code.
+    for interface_path_candidate in glob.glob(
+        os.path.join(dir_path, f'{os.path.basename(dir_path)}:*')):
+      interface_probed_result = sysfs.ReadSysfs(interface_path_candidate,
+                                                [_INTERFACE_CLASS_FIELD])
+      if interface_probed_result is None:
+        continue
+      raw_interface_probed_result = interface_probed_result[
+          _INTERFACE_CLASS_FIELD]
+      try:
+        actual_interface_class_value = int(raw_interface_probed_result, 16)
+      except ValueError:
+        logging.warning('Unexpected %s value: %s.', _INTERFACE_CLASS_FIELD,
+                        raw_interface_probed_result)
+        continue
+      if actual_interface_class_value == _INTERFACE_CLASS_VALUE:
+        break
+    else:
+      return None
+    return usb.ReadUSBSysfs(dir_path)
---
+++ py/probe/functions/smart_card_usb_unittest.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# Copyright 2021 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import glob
+import os.path
+import tempfile
+import unittest
+from unittest import mock
+
+from cros.factory.probe.functions import smart_card_usb
+from cros.factory.probe.functions import sysfs
+from cros.factory.utils import file_utils
+
+
+class SmartCardUSBFunctionTest(unittest.TestCase):
+
+  def setUp(self):
+    # The fake root dir holds files that the probe function reads data from.
+    # The path prefix is deliberately designed to have a suffix "/./" so that
+    # ordinary paths, no matter if it's an absolute one or not, can append
+    # to it to "rebase".
+    self._fake_root_prefix = os.path.join(tempfile.mkdtemp(), '.', '')
+
+    self._real_glob = glob.glob
+    self._real_readsysfs = sysfs.ReadSysfs
+
+    patcher = mock.patch('cros.factory.probe.functions.sysfs.ReadSysfs',
+                         side_effect=self._FakeReadSysfs)
+    patcher.start()
+    self.addCleanup(patcher.stop)
+
+    patcher = mock.patch('glob.glob', side_effect=self._FakeGlob)
+    patcher.start()
+    self.addCleanup(patcher.stop)
+
+  def _CreateFileAndParentDirs(self, pathname, data):
+    file_utils.TryMakeDirs(self._PatchPath(os.path.dirname(pathname)))
+    file_utils.WriteFile(self._PatchPath(pathname), data)
+
+  def testProbeASmartCardReader(self):
+    # The following files represent a smart card reader device.
+    self._CreateFileAndParentDirs('/sys/bus/usb/devices/1-1/idVendor', '1234')
+    self._CreateFileAndParentDirs('/sys/bus/usb/devices/1-1/idProduct', '5678')
+    self._CreateFileAndParentDirs(
+        '/sys/bus/usb/devices/1-1/1-1:1.0/bInterfaceClass', '0b')
+    # The following files don't represent a smart card reader device.
+    self._CreateFileAndParentDirs('/sys/bus/usb/devices/1-2/idVendor', '1357')
+    self._CreateFileAndParentDirs('/sys/bus/usb/devices/1-2/idProduct', '2468')
+    self._CreateFileAndParentDirs(
+        '/sys/bus/usb/devices/1-2/1-2:1.0/bInterfaceClass', '09')
+    self._CreateFileAndParentDirs(
+        '/sys/bus/usb/devices/1-2/1-2:2.0/bInterfaceClass', 'xyz123')
+
+    probe_function = smart_card_usb.SmartCardUSBFunction()
+    results = probe_function.Probe()
+
+    self.assertCountEqual(results, [{
+        'idVendor': '1234',
+        'idProduct': '5678',
+        'bus_type': 'usb',
+        'device_path': '/sys/bus/usb/devices/1-1',
+    }])
+
+  def _FakeReadSysfs(self, dir_path, required_keys, optional_keys=None):
+    return self._real_readsysfs(
+        self._PatchPath(dir_path), required_keys, optional_keys=optional_keys)
+
+  def _FakeGlob(self, pattern):
+    return [
+        self._UnpatchPath(p) for p in self._real_glob(self._PatchPath(pattern))
+    ]
+
+  def _PatchPath(self, path):
+    return self._fake_root_prefix + path
+
+  def _UnpatchPath(self, path):
+    return path[len(self._fake_root_prefix):]
+
+
+if __name__ == '__main__':
+  unittest.main()
