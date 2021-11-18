d919bcd6479e903bc3745691ec711237344b158e
lschyi <lschyi@google.com>
2021-11-18 14:50:22 +0800

    unittest: fix resource warning in lan_scpi test
    The socket server used in the test file does not close it properly.
    Close ti with the right API and remove it from resource warning skip
    check list.
    
    BUG=b:205776738
    TEST=make test
    
    Change-Id: If4abb1e8898587191fb7f6c58c537a821131c91b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/3291185
    Tested-by: Sung-Chi Li <lschyi@chromium.org>
    Commit-Queue: Sung-Chi Li <lschyi@chromium.org>
    Reviewed-by: Chung-Sheng Wu <chungsheng@chromium.org>

--- py/test/rf/lan_scpi_unittest.py
+++ py/test/rf/lan_scpi_unittest.py
@@ -43,7 +43,7 @@ class MockServerHandler(socketserver.StreamRequestHandler):
 
   def __init__(self, *args, **kwargs):
     self.lookup = list(self.responses_lookup)
-    socketserver.StreamRequestHandler.__init__(self, *args, **kwargs)
+    super().__init__(*args, **kwargs)
 
   def handle(self):
     while True:
@@ -151,7 +151,8 @@ class LanScpiTest(unittest.TestCase):
 
   def tearDown(self):
     self.lan_scpi.Close()
-    self.mock_server.shutdown()
+    self.mock_server.server_close()
+
 
 if __name__ == '__main__':
   logging.basicConfig(level=logging.INFO)
