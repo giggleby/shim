e1cef848d7721ed463e5f2ccf3c8a700e241b631
Pin-Yen Lin <treapking@google.com>
2021-11-25 14:27:49 +0800

    dkps: Raise exception immediately in widevine_hex_filter
    When an invalid keybox is found in the keybox list, raise an exception
    immediately and reject all the keyboxes. This allows the developers to
    see the failures earlier.
    
    BUG=b:208770021
    TEST=make test
    
    Change-Id: I90e792c268331dafd2b8aec18ce7bc8e72df7d51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/3302081
    Reviewed-by: Gene Chang <genechang@google.com>
    Reviewed-by: Chih-Yao Chuang <jasonchuang@google.com>
    Tested-by: Pin-yen Lin <treapking@chromium.org>
    Commit-Queue: Pin-yen Lin <treapking@chromium.org>

--- py/dkps/filters/widevine_hex_filter.py
+++ py/dkps/filters/widevine_hex_filter.py
@@ -1,9 +1,7 @@
 # Copyright 2021 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-"""Filter module for the Widevine keybox in hex string format."""
-
-import logging
+"""Filter module for the Widevine keyboxes in hex string format."""
 
 
 def ComputeCRC(data):
@@ -18,15 +16,27 @@ def ComputeCRC(data):
 
 
 def Filter(drm_key_list):
-  filtered_list = []
+  """Filter function for Widevine keyboxes in hex string format
+
+  Although this function is named as `Filter`, it's actually a verifier that
+  checks the length and the CRC checksum of the keyboxes and raises an error
+  when an invalid keybox is found. We raise the exception immediately, rather
+  than filter out invalid keyboxes, because we don't think it is reasonable to
+  accept a partially correct XML keybox file.
+
+  Args:
+    drm_key_list: The Widevine keyboxes list in hex string format.
+
+  Returns:
+    The original key list if no invalid keybox is found.
+
+  Raises:
+    ValueError if one of the keyboxes is invalid."""
   for widevine_key in drm_key_list:
     if len(widevine_key) != 256:
-      logging.error('Keybox length incorrect: %s', widevine_key)
-      continue
+      raise ValueError('Keybox length incorrect: %s' % widevine_key)
 
-    if ComputeCRC(widevine_key[:248]) == widevine_key[248:]:
-      filtered_list.append(widevine_key)
-    else:
-      logging.error('CRC verification failed on key: %s', widevine_key)
+    if ComputeCRC(widevine_key[:248]) != widevine_key[248:]:
+      raise ValueError('CRC verification failed on key: %s' % widevine_key)
 
-  return filtered_list
+  return drm_key_list
--- py/dkps/filters/widevine_hex_filter_unittest.py
+++ py/dkps/filters/widevine_hex_filter_unittest.py
@@ -18,29 +18,35 @@ except Exception:
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 
-CORRECT_CRC_KEYBOXES = ['5769646576696e65546573744f6e6c794b6579626f7830303000000000000000e4ff574c322ef53426212cb3ed37f35e0000000200001ee8ca1e717cfbe8a394520a6b7137d269fa5ac6b54c6b46639bbe803dbb4ff74c5f6f550e3d3d9acf81125d52e0478cda0bf4314113d0d52da05b209aed515d13d66b626f7839f294a7']  # pylint: disable=line-too-long
+CORRECT_CRC_KEYBOX = ['5769646576696e65546573744f6e6c794b6579626f7830303000000000000000e4ff574c322ef53426212cb3ed37f35e0000000200001ee8ca1e717cfbe8a394520a6b7137d269fa5ac6b54c6b46639bbe803dbb4ff74c5f6f550e3d3d9acf81125d52e0478cda0bf4314113d0d52da05b209aed515d13d66b626f7839f294a7']  # pylint: disable=line-too-long
 
 WRONG_CRC_KEYBOXES = ['58595a4244313233343830384b564a483030383332340000000000000000000076871bcafd8333332cf040c03c5421ca0db0958013e7636212e389638759264916a6b63c3e271c2115f30b3496857de0c893e84dd712f6b944216afd271384761d7ddcf045b7827636212e389638754720835e583c0a64d46b626f783ed12b9d', '58595a5f4244313233345f3830384b564a48303038333235000000000000000087123bcafd8333332cf040c03c5421dbaa80801ace39b4cef6d527364ed9ce79c0fa38d41871c7b81dc399dc0c7885a4a8eaf82a6973808ea694cef6d527364ed9c5c0926370325e30a368b8e07d2384b724812e754638926b626f783ed12b9d']  # pylint: disable=line-too-long
 
-SHORT_KEYBOXES = ['deadbeef']
+SHORT_KEYBOX = ['deadbeef']
 
 
-class WidevineParserTest(unittest.TestCase):
+class WidevineHexFilterTest(unittest.TestCase):
 
-  def testFilter(self):
-    mock_keybox_list = (
-        CORRECT_CRC_KEYBOXES + WRONG_CRC_KEYBOXES + SHORT_KEYBOXES)
-
+  def setUp(self):
     # TODO(treapking): This makes the unit test work in chroot. Remove this if
     # we can run unit test in container.
-    if crcmod:
-      filtered_list = widevine_hex_filter.Filter(mock_keybox_list)
-    else:
-      with mock.patch(widevine_hex_filter.__name__ + '.ComputeCRC',
-                      return_value='39f294a7'):
-        filtered_list = widevine_hex_filter.Filter(mock_keybox_list)
+    if not crcmod:
+      patcher = mock.patch(widevine_hex_filter.__name__ + '.ComputeCRC',
+                           return_value='39f294a7')
+      patcher.start()
 
-    self.assertEqual(filtered_list, CORRECT_CRC_KEYBOXES)
+  def testFilter(self):
+    filtered_list = widevine_hex_filter.Filter(CORRECT_CRC_KEYBOX)
+
+    self.assertEqual(filtered_list, CORRECT_CRC_KEYBOX)
+
+  def testWrongCRC(self):
+    with self.assertRaisesRegex(ValueError, 'CRC verification failed'):
+      widevine_hex_filter.Filter(WRONG_CRC_KEYBOXES)
+
+  def testShortKeybox(self):
+    with self.assertRaisesRegex(ValueError, 'Keybox length incorrect'):
+      widevine_hex_filter.Filter(SHORT_KEYBOX)
 
 
 if __name__ == '__main__':
