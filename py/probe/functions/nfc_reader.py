d3a982d498424b68d80707fbeed15e95cf9a3689
Cheng Yueh <cyueh@chromium.org>
2021-12-01 13:39:47 +0800

    py/probe: Implement a probe function for USB NFC readers
    We have internal NFC readers in a device. Add a function to probe them.
    
    BUG=b:208329409
    TEST=probe search nfc_reader
    TEST=make test
    
    Change-Id: I165e649887f388f4edfcf9bd887f511960de0be3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/3310339
    Reviewed-by: Pin-yen Lin <treapking@chromium.org>
    Tested-by: Cheng Yueh <cyueh@chromium.org>
    Commit-Queue: Cheng Yueh <cyueh@chromium.org>

---
+++ py/probe/functions/nfc_reader.py
@@ -0,0 +1,75 @@
+# Copyright 2021 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import glob
+import logging
+import os.path
+
+from cros.factory.probe.functions import sysfs
+from cros.factory.probe.functions import usb
+from cros.factory.probe.lib import cached_probe_function
+from cros.factory.utils import type_utils
+
+
+RemovableResult = type_utils.Enum(('fixed', 'removable', 'unknown'))
+
+_INTERFACE_CLASS_VALUE = 0x03
+_INTERFACE_CLASS_FIELD = 'bInterfaceClass'
+_REMOVABLE_FIELD = 'removable'
+
+
+class NFCUSBFunction(cached_probe_function.GlobPathCachedProbeFunction):
+  """Probes all USB NFC reader devices.
+
+  Description
+  -----------
+  This function goes through ``/sys/bus/usb/devices/`` to read attributes of
+  each usb device and find out all NFC readers there.  Each result must contain
+  these fields:
+
+  - ``device_path``: Pathname of the sysfs directory.
+  - ``idVendor``
+  - ``idProduct``
+
+  The result might also contain these optional fields if they are exported in
+  the sysfs entry:
+
+  - ``manufacturer``
+  - ``product``
+  - ``bcdDevice``
+  """
+  GLOB_PATH = '/sys/bus/usb/devices/*'
+
+  @classmethod
+  def ProbeDevice(cls, dir_path):
+    removable_probed_result = sysfs.ReadSysfs(dir_path, [_REMOVABLE_FIELD])
+    logging.debug('%r for %r is %r', _REMOVABLE_FIELD, dir_path,
+                  removable_probed_result)
+    if removable_probed_result is None:
+      return None
+    if removable_probed_result[_REMOVABLE_FIELD] != RemovableResult.fixed:
+      return None
+
+    # An USB device might have multiple interfaces, each is represented as
+    # a sub-folder inside the device's sysfs node.  We treat a device a NFC
+    # reader if any of its interface has the specific class code.
+    for interface_path_candidate in glob.glob(
+        os.path.join(dir_path, f'{os.path.basename(dir_path)}:*')):
+      interface_probed_result = sysfs.ReadSysfs(interface_path_candidate,
+                                                [_INTERFACE_CLASS_FIELD])
+      if interface_probed_result is None:
+        continue
+      raw_interface_class_value = interface_probed_result[
+          _INTERFACE_CLASS_FIELD]
+      try:
+        actual_interface_class_value = int(raw_interface_class_value, 16)
+      except ValueError:
+        logging.warning('Unexpected %s value: %s.', _INTERFACE_CLASS_FIELD,
+                        raw_interface_class_value)
+        continue
+      if actual_interface_class_value == _INTERFACE_CLASS_VALUE:
+        break
+    else:
+      return None
+    return usb.ReadUSBSysfs(dir_path)
---
+++ py/probe/functions/nfc_reader_unittest.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+# Copyright 2021 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import glob
+import os.path
+import shutil
+import tempfile
+import unittest
+from unittest import mock
+
+from cros.factory.probe.functions import nfc_reader
+from cros.factory.probe.functions import sysfs
+from cros.factory.utils import file_utils
+
+
+class NFCUSBFunctionTest(unittest.TestCase):
+
+  def setUp(self):
+    # The fake root dir holds files that the probe function reads data from.
+    # The path prefix is deliberately designed to have a suffix "/./" so that
+    # ordinary paths, no matter if it's an absolute one or not, can append
+    # to it to "rebase".
+    self._temp_dir = tempfile.mkdtemp()
+    self._fake_root_prefix = os.path.join(self._temp_dir, '.', '')
+
+    self._real_glob = glob.glob
+    self._real_readsysfs = sysfs.ReadSysfs
+
+    patcher = mock.patch('cros.factory.probe.functions.sysfs.ReadSysfs',
+                         side_effect=self._FakeReadSysfs)
+    patcher.start()
+    self.addCleanup(patcher.stop)
+
+    patcher = mock.patch('glob.glob', side_effect=self._FakeGlob)
+    patcher.start()
+    self.addCleanup(patcher.stop)
+
+  def tearDown(self):
+    shutil.rmtree(self._temp_dir)
+
+  def _CreateFileAndParentDirs(self, pathname, data):
+    file_utils.TryMakeDirs(self._PatchPath(os.path.dirname(pathname)))
+    file_utils.WriteFile(self._PatchPath(pathname), data)
+
+  def testProbeANfcReader(self):
+    # The following files represent a NFC reader device.
+    self._CreateFileAndParentDirs('/sys/bus/usb/devices/3-5/idVendor', '1234')
+    self._CreateFileAndParentDirs('/sys/bus/usb/devices/3-5/idProduct', '5678')
+    self._CreateFileAndParentDirs('/sys/bus/usb/devices/3-5/removable', 'fixed')
+    self._CreateFileAndParentDirs(
+        '/sys/bus/usb/devices/3-5/3-5:1.0/bInterfaceClass', '03')
+    # The following files don't represent a NFC reader device because it's
+    # removable.
+    self._CreateFileAndParentDirs('/sys/bus/usb/devices/3-9/idVendor', '1357')
+    self._CreateFileAndParentDirs('/sys/bus/usb/devices/3-9/idProduct', '2468')
+    self._CreateFileAndParentDirs('/sys/bus/usb/devices/3-9/removable',
+                                  'removable')
+    self._CreateFileAndParentDirs(
+        '/sys/bus/usb/devices/3-9/3-9:1.0/bInterfaceClass', '03')
+    self._CreateFileAndParentDirs(
+        '/sys/bus/usb/devices/3-9/3-9:2.0/bInterfaceClass', 'xyz123')
+    # The following files don't represent a NFC reader device because its
+    # bInterfaceClass is not 03.
+    self._CreateFileAndParentDirs('/sys/bus/usb/devices/3-6/idVendor', '4321')
+    self._CreateFileAndParentDirs('/sys/bus/usb/devices/3-6/idProduct', '8765')
+    self._CreateFileAndParentDirs('/sys/bus/usb/devices/3-6/removable', 'fixed')
+    self._CreateFileAndParentDirs(
+        '/sys/bus/usb/devices/3-6/3-6:1.0/bInterfaceClass', '0b')
+
+    probe_function = nfc_reader.NFCUSBFunction()
+    results = probe_function.Probe()
+
+    self.assertCountEqual(results, [{
+        'idVendor': '1234',
+        'idProduct': '5678',
+        'bus_type': 'usb',
+        'device_path': '/sys/bus/usb/devices/3-5',
+    }])
+
+  def _FakeReadSysfs(self, dir_path, required_keys, optional_keys=None):
+    return self._real_readsysfs(
+        self._PatchPath(dir_path), required_keys, optional_keys=optional_keys)
+
+  def _FakeGlob(self, pattern):
+    return [
+        self._UnpatchPath(p) for p in self._real_glob(self._PatchPath(pattern))
+    ]
+
+  def _PatchPath(self, path):
+    return self._fake_root_prefix + path
+
+  def _UnpatchPath(self, path):
+    return path[len(self._fake_root_prefix):]
+
+
+if __name__ == '__main__':
+  unittest.main()
