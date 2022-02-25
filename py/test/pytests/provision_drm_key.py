5c4f67e5f0dc3c66f2c72f11d7f9bd9c57a64a97
Pin-Yen Lin <treapking@google.com>
2022-02-25 16:12:53 +0800

    provision_drm_key: Add `from_device_data` argument
    Add a new argument to make the test read the keybox information from
    factory device data instead of requesting from DKPS proxy server.
    
    BUG=b:220988854
    TEST=make test
    TEST=(0) Manually copy Crypto and crcmod package to DUT
         (1) Request keybox from DKPS proxy server
    TEST=(0) Manually copy Crypto and crcmod package to DUT
         (1) Set device data manually
         (2) Run ProvisionDRMKey with `from_device_data=true`
    
    Change-Id: I6568711dc9cd1a5b7d069e01462a9ffb6beef5b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/3490416
    Reviewed-by: Chih-Yao Chuang <jasonchuang@google.com>
    Reviewed-by: Gene Chang <genechang@google.com>
    Tested-by: Pin-yen Lin <treapking@chromium.org>
    Commit-Queue: Pin-yen Lin <treapking@chromium.org>

--- py/test/pytests/provision_drm_key.py
+++ py/test/pytests/provision_drm_key.py
@@ -15,7 +15,11 @@ This is an automatic test that doesn't need any user interaction.
 This test does the following procedures:
 
 1. Get the SoC serial number and SoC model ID via OEMCrypto API.
-2. Request the Widevine keybox from the DKPS proxy server.
+2. If `from_device_data` is `True`:
+   (1) Read the Widevine keybox from the factory device data.
+   (2) Verify the keybox with the checksum.
+   (3) Encrypt the keybox with the transport key.
+   Otherwise, request the Widevine keybox from the DKPS proxy server.
 3. Pass the keybox to OEMCrypto TA to get an encrypted keybox.
 4. Compute the CRC32 code of the encrypted keybox for checking the keybox
    validity in RMA.
@@ -49,6 +53,16 @@ To request the keybox by different domains of dut::
     }
   }
 
+To read the keybox from the factory device data::
+
+  {
+    "pytest_name": "provision_drm_key",
+    "args": {
+      "from_device_data": true
+    }
+  }
+
+
 """
 
 import logging
@@ -57,6 +71,8 @@ import xmlrpc.client
 import zlib
 
 from cros.factory.device import device_utils
+from cros.factory.dkps import widevine_utils
+from cros.factory.test import device_data
 from cros.factory.test import test_case
 from cros.factory.test.utils import oemcrypto_utils
 from cros.factory.test.utils.url_spec import URLSpec
@@ -87,26 +103,53 @@ class ProvisionDRMKey(test_case.TestCase):
       Arg(
           'proxy_server_url', (str, dict),
           'A URL or a map from dut domain to URL, to config the URL of'
-          'DKPS proxy server.')
+          'DKPS proxy server.', default=None),
+      Arg(
+          'from_device_data', bool,
+          'True to read the keybox from factory device data. Otherwise request '
+          'the keybox from DKPS proxy server.', default=False)
   ]
 
   def setUp(self):
     self.dut = device_utils.CreateDUTInterface()
-    try:
-      server_url = URLSpec.FindServerURL(self.args.proxy_server_url, self.dut)
-    except ValueError as e:
-      raise ValueError('Server Url not found, please check arguments.', e)
+    if not self.args.from_device_data:
+      try:
+        server_url = URLSpec.FindServerURL(self.args.proxy_server_url, self.dut)
+      except ValueError as e:
+        raise ValueError('Server Url not found, please check arguments.', e)
 
-    logging.info('Proxy server URL: %s', server_url)
-    self.dkps_proxy = xmlrpc.client.ServerProxy(server_url)
+      logging.info('Proxy server URL: %s', server_url)
+      self.dkps_proxy = xmlrpc.client.ServerProxy(server_url)
 
     self.oemcrypto_client = oemcrypto_utils.OEMCryptoClient()
+
+  def GetKeyboxFromDeviceData(self):
+
+    def FromWidevineDeviceData(key):
+      device_data_key = device_data.JoinKeys(device_data.KEY_FACTORY,
+                                             'widevine_' + key)
+      return device_data.GetDeviceData(device_data_key, throw_if_none=True)
+
+    keybox = widevine_utils.FormatDeviceID(FromWidevineDeviceData('device_id'))
+    for key in ['key', 'id', 'magic', 'crc']:
+      keybox += FromWidevineDeviceData(key)
+
+    if not widevine_utils.IsValidKeybox(keybox):
+      self.FailTask('CRC verification failed. keybox: %s.' % keybox)
+
+    return keybox
 
   def runTest(self):
     soc_id, soc_serial = self.oemcrypto_client.GetFactoryTransportKeyMaterial()
 
-    encrypted_keybox = self.dkps_proxy.Request(
-        GetDeviceSerial(self.dut.info), soc_serial, soc_id)
+    if self.args.from_device_data:
+      keybox = self.GetKeyboxFromDeviceData()
+      transport_key = widevine_utils.TransportKeyKDF(soc_serial, soc_id)
+      encrypted_keybox = widevine_utils.EncryptKeyboxWithTransportKey(
+          keybox, transport_key)
+    else:
+      encrypted_keybox = self.dkps_proxy.Request(
+          GetDeviceSerial(self.dut.info), soc_serial, soc_id)
     wrapped_keybox = self.oemcrypto_client.WrapFactoryKeybox(encrypted_keybox)
     wrapped_keybox += format(zlib.crc32(bytes.fromhex(wrapped_keybox)), '08x')
 
--- py/test/pytests/provision_drm_key_unittest.py
+++ py/test/pytests/provision_drm_key_unittest.py
@@ -42,8 +42,9 @@ class GetDeviceSerialTest(unittest.TestCase):
 
 class FakeArgs:
 
-  def __init__(self, url_spec=None):
+  def __init__(self, url_spec=None, from_device_data=False):
     self.proxy_server_url = url_spec
+    self.from_device_data = from_device_data
 
 
 class MockProxy:
@@ -54,15 +55,25 @@ class MockProxy:
     return '0a' * 128
 
 
-class MockOEMCryptoClient:
+def GetGoodDeviceData(key, throw_if_none=True):
+  del throw_if_none  # unused
 
-  def GetFactoryTransportKeyMaterial(self):
-    return 16, '7f' * 32
+  _DEVICE_DATA = {
+      'factory.widevine_device_id': 'WidevineTestOnlyKeybox000',
+      'factory.widevine_key': 'e4ff574c322ef53426212cb3ed37f35e',
+      'factory.widevine_id':
+          '0000000200001ee8ca1e717cfbe8a394520a6b7137d269fa5ac6b54c6b46639bbe80'
+          '3dbb4ff74c5f6f550e3d3d9acf81125d52e0478cda0bf4314113d0d52da05b209aed'
+          '515d13d6',
+      'factory.widevine_magic': '6b626f78',
+      'factory.widevine_crc': '39f294a7',
+  }
+  return _DEVICE_DATA[key]
 
-  def WrapFactoryKeybox(self, encrypted_keybox):
-    del encrypted_keybox  # unused
-    # Re-encrypted keybox will have 176 bytes.
-    return '7f' * 176
+
+def GetBadDeviceData(unused_key, throw_if_none=True):
+  del unused_key, throw_if_none  # unused
+  return ''
 
 
 class ProvisionDRMKeyTest(unittest.TestCase):
@@ -70,11 +81,34 @@ class ProvisionDRMKeyTest(unittest.TestCase):
   def setUp(self):
     self.test = provision_drm_key.ProvisionDRMKey()
     self.test.dut = mock.Mock()
-    self.test.oemcrypto_client = MockOEMCryptoClient()
+    mock_oemcrypto = mock.Mock()
+    mock_oemcrypto.GetFactoryTransportKeyMaterial.return_value = (16, '7f' * 32)
+    mock_oemcrypto.WrapFactoryKeybox.return_value = '7f' * 176
+    self.test.oemcrypto_client = mock_oemcrypto
     self.test.args = FakeArgs()
     patcher = mock.patch(
         'cros.factory.test.utils.oemcrypto_utils.OEMCryptoClient')
     patcher.start()
+
+  @mock.patch('cros.factory.test.device_data.GetDeviceData')
+  def testGetKeyboxFromDeviceDataNormal(self, mock_get_device_data):
+    mock_get_device_data.side_effect = GetGoodDeviceData
+
+    keybox = self.test.GetKeyboxFromDeviceData()
+
+    self.assertEqual(
+        keybox,
+        '5769646576696e65546573744f6e6c794b6579626f7830303000000000000000e4ff57'
+        '4c322ef53426212cb3ed37f35e0000000200001ee8ca1e717cfbe8a394520a6b7137d2'
+        '69fa5ac6b54c6b46639bbe803dbb4ff74c5f6f550e3d3d9acf81125d52e0478cda0bf4'
+        '314113d0d52da05b209aed515d13d66b626f7839f294a7')
+
+  @mock.patch('cros.factory.test.device_data.GetDeviceData')
+  def testGetKeyboxFromDeviceDataInvalid(self, mock_get_device_data):
+    mock_get_device_data.side_effect = GetBadDeviceData
+
+    with self.assertRaisesRegex(Exception, 'CRC verification failed'):
+      self.test.GetKeyboxFromDeviceData()
 
   @mock.patch('cros.factory.test.utils.url_spec.URLSpec.FindServerURL')
   @mock.patch('xmlrpc.client.ServerProxy')
@@ -93,14 +127,32 @@ class ProvisionDRMKeyTest(unittest.TestCase):
                            'Server Url not found, please check arguments.',
                            self.test.setUp)
 
-  def testRunTest(self):
+  def testRunTestFromDKPSProxy(self):
     self.test.dkps_proxy = MockProxy()
 
     self.test.runTest()
 
-    # 'df9c1a9e` is the CRC32 sum of b'\x7f' * 176 in hex.
+    # 'df9c1a9e' is the CRC32 sum of b'\x7f' * 176 in hex.
     self.test.dut.vpd.ro.Update.assert_called_with(
         {'widevine_keybox': '7f' * 176 + 'df9c1a9e'})
+
+  @mock.patch(
+      provision_drm_key.__name__ + '.ProvisionDRMKey.GetKeyboxFromDeviceData')
+  def testRunTestFromDeviceData(self, mock_get_keybox):
+    self.test.args = FakeArgs(from_device_data=True)
+    mock_get_keybox.return_value = '0a' * 128
+
+    self.test.runTest()
+
+    self.test.oemcrypto_client.WrapFactoryKeybox.assert_called_with(
+        '115709591f9ba24e655619e1748a287897b6ec5d0ff87201633a176a4f35a2bf8c9c16'
+        '666a78d74a69144fba04258cad4c9def6c55fbde6eaaafc56043dc95076a53e3018ea7'
+        '701450f137d296d09422718d941971e4d39cb7cc33d557631da8d26b5eac60782ca778'
+        'a748505eabeb85d57cad0d2a2980bd76a5e3105a19f09f')
+    # 'df9c1a9e' is the CRC32 sum of b'\x7f' * 176 in hex.
+    self.test.dut.vpd.ro.Update.assert_called_with(
+        {'widevine_keybox': '7f' * 176 + 'df9c1a9e'})
+    mock_get_keybox.assert_called_once()
 
 
 if __name__ == '__main__':
