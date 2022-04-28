c444e6347f857e2ca1ae9cf645e23ab265a308ca
Liang-Chieh Chen <jamesqaq@google.com>
2022-04-28 19:31:44 +0800

    bundle_creator: add hwid_api_connector_unittest
    Add a unit test of `connector/hwid_api_connector.py`.
    
    BUG=b:214528226
    TEST=(1) ./deploy/bundle_creator.sh test-docker
    
    Change-Id: Ie9d78d031cd57cbbd6789bf7186c56a709d3b575
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/3614627
    Commit-Queue: Liang-Chieh Chen <jamesqaq@chromium.org>
    Reviewed-by: Yu-An Wang <wyuang@google.com>
    Tested-by: Liang-Chieh Chen <jamesqaq@chromium.org>

---
+++ py/bundle_creator/connector/hwid_api_connector_unittest.py
@@ -0,0 +1,96 @@
+# Copyright 2022 The ChromiumOS Authors.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import io
+import json
+from typing import List, Tuple
+import unittest
+from unittest import mock
+import urllib.error
+
+from cros.factory.bundle_creator.connector import hwid_api_connector
+
+_TOKEN = 'ZMhb.D.xWPbbBTkiJsPri-KEYyLlLL99zXrnPqotEf1BKgpqmk'
+
+
+def _MockGoogleAuthDefault(scopes: List[str]) -> Tuple[mock.Mock, None]:
+  if 'https://www.googleapis.com/auth/chromeoshwid' in scopes:
+    credentials = mock.Mock(token=_TOKEN)
+    return (credentials, None)
+  raise ValueError('The scopes don\'t contain `chromeoshwid`.')
+
+
+class HWIDAPIConnectorTest(unittest.TestCase):
+
+  _HWID_ENDPOINT = 'https://fake_hwid_endpoint'
+  _ORIGINAL_REQUESTER = 'foo@bar'
+  _BUNDLE_RECORD = '{"fake_key": "fake_value"}'
+
+  def setUp(self):
+    mock_urllib_request_patcher = mock.patch('urllib.request')
+    self._urllib_request = mock_urllib_request_patcher.start()
+    self.addCleanup(mock_urllib_request_patcher.stop)
+
+    self._connector = hwid_api_connector.HWIDAPIConnector(self._HWID_ENDPOINT)
+
+  @mock.patch('google.auth.default')
+  def testCreateHWIDFirmwareInfoCL_verifyRequestWithExpectedArguments(
+      self, mock_google_auth_default):
+    mock_google_auth_default.side_effect = _MockGoogleAuthDefault
+    mock_response = mock.Mock()
+    mock_response.read.return_value = '{}'
+    self._urllib_request.urlopen.return_value = mock_response
+
+    self._connector.CreateHWIDFirmwareInfoCL(self._BUNDLE_RECORD,
+                                             self._ORIGINAL_REQUESTER)
+
+    self.assertEqual(
+        self._urllib_request.Request.call_args.args[0],
+        f'{self._HWID_ENDPOINT}/v2/createHwidDbFirmwareInfoUpdateCl')
+    self.assertEqual(
+        self._urllib_request.Request.call_args.kwargs['headers']
+        ['Authorization'], f'Bearer {_TOKEN}')
+    data = json.loads(self._urllib_request.Request.call_args.kwargs['data'])
+    self.assertEqual(data['bundle_record'], self._BUNDLE_RECORD)
+    self.assertEqual(data['original_requester'], self._ORIGINAL_REQUESTER)
+
+  def testCreateHWIDFirmwareInfoCL_succeed_returnsExpectedClUrl(self):
+    cl_number = 1234567
+    mock_response = mock.Mock()
+    mock_response.read.return_value = json.dumps(
+        {'commits': {
+            'project': {
+                'clNumber': cl_number
+            }
+        }})
+    self._urllib_request.urlopen.return_value = mock_response
+
+    cl_url = self._connector.CreateHWIDFirmwareInfoCL(self._BUNDLE_RECORD,
+                                                      self._ORIGINAL_REQUESTER)
+
+    expected_url = (
+        'https://chrome-internal-review.googlesource.com/c/chromeos/'
+        f'chromeos-hwid/+/{cl_number}')
+    self.assertEqual(cl_url, [expected_url])
+
+  def testCreateHWIDFirmwareInfoCL_httpError_raisesExpectedException(self):
+    error_msg = {
+        'error': {
+            'code': 403
+        }
+    }
+    forbidden_error = urllib.error.HTTPError(
+        url='', code=403, msg='', hdrs={}, fp=io.StringIO(
+            json.dumps(error_msg)))
+    self._urllib_request.urlopen.side_effect = forbidden_error
+
+    with self.assertRaises(hwid_api_connector.HWIDAPIRequestException) as e:
+      self._connector.CreateHWIDFirmwareInfoCL(self._BUNDLE_RECORD,
+                                               self._ORIGINAL_REQUESTER)
+
+    self.assertEqual(json.loads(str(e.exception)), error_msg)
+
+
+if __name__ == '__main__':
+  unittest.main()
