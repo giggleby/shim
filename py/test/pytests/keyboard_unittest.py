0e65d2072f6f1f21194b8cfcc168da01d52a492d
jasonchuang <jasonchuang@google.com>
2022-05-16 16:30:38 +0800

    pytest: refactor keyboard.py
    1. Rename down_keys to hold_keys
    Down is the action that press the key, the action not release the
    key is called hold.
    2.Rename number_to_press to need_press_keys
    We don't need to maintain the keys that are skipped.
    This will be implemented in the next CL.
    3. Merge all_keys into need_press_keys
    need_press_keys is generated by all_keys, we don't need a set and
    a dict with the same keys.
    4. Handle order in OnKeyup
    The intuition is raise if strict_sequential_press, otherwise do
    nothing with the wrong order. It's a little complicated to implement
    this logic in 2 functions by handling ignored_down_keys.
    5. Maintain next_index
    We can get next key needed to press in constant time instead of
    loop in key_order_list.
    
    BUG=b:232199509
    TEST=test on device with arg allow_multi_keys, multi_keys_delay,
    sequential_press, strict_sequential_press, repeat_times, has_numpad
    
    Change-Id: Id1051adbdbb212116d5035f2a11db076046d14f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/3645633
    Reviewed-by: Pin-yen Lin <treapking@chromium.org>
    Tested-by: Chih-Yao Chuang <jasonchuang@google.com>
    Commit-Queue: Chih-Yao Chuang <jasonchuang@google.com>

--- py/test/pytests/keyboard_unittest.py
+++ py/test/pytests/keyboard_unittest.py
@@ -4,54 +4,121 @@
 # found in the LICENSE file.
 
 import unittest
+from unittest import mock
 
 from cros.factory.test.pytests import keyboard
-from cros.factory.utils import schema
 
+
+class FakeArgs:
+
+  def __init__(self):
+    self.repeat_times = {}
+    self.skip_keycodes = [3]
+    self.vivaldi_keyboard = False
+    self.has_numpad = False
+    self.allow_multi_keys = False
+    self.sequential_press = False
+    self.multi_keys_delay = 0
+    self.layout = None
+    self.strict_sequential_press = False
+    self.board = ''
+    self.device_filter = None
+    self.skip_power_key = False
+    self.skip_keycodes = []
+    self.replacement_keymap = {}
+    self.detect_long_press = None
 
 class KeyboardUnitTest(unittest.TestCase):
 
-  def testValidDataWithKeymapSchema(self):
-    valid_data = [{
-        "0": "1"
-    }, {
-        "0b1": "0b1"
-    }, {
-        "0o1": "0o1"
-    }, {
-        "0x1": "0x1"
-    }]
-    for data in valid_data:
-      try:
-        # pylint: disable=protected-access
-        self.assertEqual(None,
-                         keyboard._REPLACEMENT_KEYMAP_SCHEMA.Validate(data))
-      except Exception as err:
-        raise Exception('data is not valid: %r' % data) from err
+  def MockFunction(self, function_name, return_value=None):
+    if return_value is None:
+      patcher = mock.patch(function_name)
+    else:
+      patcher = mock.patch(function_name, return_value=return_value)
+    patcher.start()
+    self.addCleanup(patcher.stop)
 
-  def testInvalidDataWithKeymapSchema(self):
-    invalid_data = [{
-        "x": "1"
-    }, {
-        "01": "1"
-    }, {
-        1: "1"
-    }, {
-        "1": "x"
-    }, {
-        "1": "01"
-    }, {
-        "1": 1
-    }]
-    for data in invalid_data:
-      try:
-        # pylint: disable=protected-access
-        self.assertRaisesRegex(schema.SchemaException, '^.*$',
-                               keyboard._REPLACEMENT_KEYMAP_SCHEMA.Validate,
-                               data)
-      except Exception as err:
-        raise Exception('data is not invalid: %r' % data) from err
+  def setUp(self):
+    self.test = keyboard.KeyboardTest()
+    self.test.args = FakeArgs()
+    self.test.event_loop = mock.Mock()
+    self.MockFunction('cros.factory.test.utils.evdev_utils.FindDevice')
+    self.MockFunction(
+        'cros.factory.test.utils.evdev_utils.InputDeviceDispatcher')
+    self.MockFunction(f'{keyboard.__name__}.KeyboardTest.GetKeyboardLayout')
+    self.MockFunction('cros.factory.test.test_case.TestCase.ui')
+    self.MockFunction('cros.factory.testlog.testlog.UpdateParam')
+    self.MockFunction(f'{keyboard.__name__}.KeyboardTest.PassTask')
+    all_keys = [1, 2, 3, 4]
+    self.MockFunction(f'{keyboard.__name__}.KeyboardTest.ReadBindings',
+                      {key: {}
+                       for key in all_keys})
 
+  def PressKey(self, keycode):
+    self.test.OnKeydown(keycode)
+    self.test.OnKeyup(keycode)
+
+  @mock.patch(f'{keyboard.__name__}.KeyboardTest.PassTask')
+  @mock.patch(f'{keyboard.__name__}.KeyboardTest.ReadKeyOrder')
+  def testStrictSequentialPress(self, mock_key_order, mock_pass):
+    self.test.args.repeat_times = {
+        'default': 2,
+        '1': 1,
+        '3': 1,
+        '4': 3
+    }
+    self.test.args.strict_sequential_press = True
+    mock_key_order.return_value = [1, 2, 3]
+    self.test.setUp()
+
+    self.PressKey(4)
+    self.PressKey(1)
+    self.PressKey(2)
+    self.PressKey(4)
+    self.PressKey(2)
+    self.PressKey(3)
+    self.PressKey(4)
+    mock_pass.assert_called_once()
+
+  @mock.patch(f'{keyboard.__name__}.KeyboardTest.ReadKeyOrder')
+  def testStrictSequentialPressWrongOrder(self, mock_key_order):
+    self.test.args.repeat_times = {
+        '1': 1,
+        '3': 2
+    }
+    self.test.args.strict_sequential_press = True
+    mock_key_order.return_value = [1, 3, 2]
+    self.test.setUp()
+
+    self.PressKey(4)
+    self.PressKey(1)
+    self.PressKey(3)
+    with self.assertRaisesRegex(Exception, 'Expect keycode 3 but get 2'):
+      self.PressKey(2)
+
+  @mock.patch(f'{keyboard.__name__}.KeyboardTest.PassTask')
+  @mock.patch(f'{keyboard.__name__}.KeyboardTest.ReadKeyOrder')
+  def testSequentialPress(self, mock_key_order, mock_pass):
+    self.test.args.sequential_press = True
+    mock_key_order.return_value = [1, 2, 3]
+    self.test.setUp()
+
+    self.PressKey(4)
+    self.PressKey(3)
+    self.PressKey(2)
+    self.PressKey(1)
+    mock_pass.assert_not_called()
+    self.PressKey(2)
+    self.PressKey(3)
+    mock_pass.assert_called_once()
+
+  @mock.patch(f'{keyboard.__name__}.KeyboardTest.PassTask')
+  def testSkipKeycodes(self, mock_pass):
+    self.test.args.skip_keycodes = [1, 2, 3]
+    self.test.setUp()
+
+    self.PressKey(4)
+    mock_pass.assert_called_once()
 
 if __name__ == '__main__':
   unittest.main()
