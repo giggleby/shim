1f9417c9e74a65943c82ec81d9739128b59a3e62
Cheng Yueh <cyueh@chromium.org>
2022-05-23 19:23:30 +0800

    generic_network_device: Unique devices by path
    Users may create virtual interfaces via `iw phy phy0 interface add
    <name> type <type>`. In this case, _GetIwconfigDevices returns multiple
    devices which link to the same device path.
    
    We should unique the probed device by path.
    
    BUG=b:232358531
    TEST=(0) iw phy phy0 interface add antmontest type monitor
         (1) /usr/local/factory/probe probe --comps wireless
         (2) two wireless without this CL
         (3) one wireless with this CL
    
    Change-Id: I71688e1b1e217ae20b0f4bc9c77f6a21fbca19de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/3660718
    Commit-Queue: Cheng Yueh <cyueh@chromium.org>
    Reviewed-by: Kevin Lin <kevinptt@chromium.org>
    Auto-Submit: Cheng Yueh <cyueh@chromium.org>
    Tested-by: Cheng Yueh <cyueh@chromium.org>

--- py/probe/functions/generic_network_device_unittest.py
+++ py/probe/functions/generic_network_device_unittest.py
@@ -4,8 +4,11 @@
 # found in the LICENSE file.
 
 import unittest
+from unittest import mock
 
+from cros.factory.probe import function
 from cros.factory.probe.functions import generic_network_device
+from cros.factory.utils.type_utils import Obj
 
 
 class NetworkDevicesTest(unittest.TestCase):
@@ -34,6 +37,63 @@ class NetworkDevicesTest(unittest.TestCase):
     self.assertNotEqual(
         generic_network_device.NetworkDevices.GetPCIPath(
             'wifi', pci_bad_wifi_real_path), pci_bad_wifi_pci_path)
+
+  @mock.patch(function.__name__ + '.InterpretFunction')
+  @mock.patch(generic_network_device.__name__ + '.NetworkDevices.GetDevices')
+  @mock.patch(generic_network_device.__name__ + '.NetworkDevices.GetPCIPath')
+  def testReadSysfsDeviceIds(self, get_pci_path_mock: mock.MagicMock,
+                             get_devices: mock.MagicMock,
+                             interpret_function_mock: mock.MagicMock):
+
+    def FakeGetPCIPath(devtype, path):
+      if devtype == 'wifi':
+        # Assume that wlan0 and antmon0 are the same device.
+        if 'wlan0' in path or 'antmon0' in path:
+          return '/sys/devices/pci0123:45/1789:ab:cd.f'
+        return '/sys/devices/pci0123:45/2789:ab:cd.f'
+      if devtype == 'cellular':
+        return '/sys/devices/pci0123:45/3789:ab:cd.f'
+      return '/sys/devices/pci0123:45/4789:ab:cd.f'
+
+    get_pci_path_mock.side_effect = FakeGetPCIPath
+    get_devices.return_value = [
+        Obj(devtype='wifi', path='/sys/class/net/wlan0/device'),
+        Obj(devtype='wifi', path='/sys/class/net/antmon0/device'),
+        Obj(devtype='wifi', path='/sys/class/net/wlan1/device'),
+        Obj(devtype='cellular', path='/sys/class/net/wwan0/device'),
+        Obj(devtype='ethernet', path='/sys/class/net/eth0/device'),
+    ]
+
+    def FakeProbeDevice(unused_devtype, path):
+      template = {
+          'bus_type': 'pci',
+          'class': '0x028000',
+          'device': '0x1234',
+          'revision_id': '0x00',
+          'subsystem_device': '0x0000',
+          'vendor': '0x4321',
+      }
+      template.update({'device_path': path})
+      return template
+
+    def FakeInterpretFunction(data):
+      return lambda: [FakeProbeDevice(list(data)[0], list(data.values())[0])]
+
+    interpret_function_mock.side_effect = FakeInterpretFunction
+
+    self.assertEqual(
+        generic_network_device.NetworkDevices.ReadSysfsDeviceIds('wifi'), [
+            FakeProbeDevice('wifi', '/sys/devices/pci0123:45/1789:ab:cd.f'),
+            FakeProbeDevice('wifi', '/sys/devices/pci0123:45/2789:ab:cd.f')
+        ])
+
+    self.assertEqual(
+        generic_network_device.NetworkDevices.ReadSysfsDeviceIds('cellular'),
+        [FakeProbeDevice('cellular', '/sys/devices/pci0123:45/3789:ab:cd.f')])
+
+    self.assertEqual(
+        generic_network_device.NetworkDevices.ReadSysfsDeviceIds('ethernet'),
+        [FakeProbeDevice('ethernet', '/sys/devices/pci0123:45/4789:ab:cd.f')])
 
 
 if __name__ == '__main__':
