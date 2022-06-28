1ed9b5f2e29d5c79b80a7b203bf9fc353dd1e31d
Chun-Tsen Kuo <chuntsen@chromium.org>
2022-06-28 17:50:12 +0800

    Instalog: Add a buffer plugin to process factory report events
    Add a priority multi-file-based buffer for parsing factory reports.
    The priority in this plugin:
      0: Factory Report Event from output_factory_report.
      1: Process Event from output_factory_report.
      2: Archive Events from input_gcs_scan, and the upload time is after
         the separate time.
      3: Others.
    The first two priority events are much more than the others, so we need
    to process them first. Otherwise, the Instalog instance will run out of
    disk.
    
    BUG=b:189805423
    TEST=maunally test
    
    Change-Id: I7fa95e7ee4a3db961bee312ce4c4d4d4cf161cf6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/3731749
    Reviewed-by: Sung-Chi Li <lschyi@chromium.org>
    Auto-Submit: Chun-Tsen Kuo <chuntsen@chromium.org>
    Commit-Queue: Chun-Tsen Kuo <chuntsen@chromium.org>
    Tested-by: Chun-Tsen Kuo <chuntsen@chromium.org>

---
+++ py/instalog/plugins/buffer_priority_factory_report.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+#
+# Copyright 2022 The ChromiumOS Authors.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Priority multi-file-based factory report buffer."""
+
+import datetime
+
+from cros.factory.instalog import json_utils
+from cros.factory.instalog import plugin_base
+from cros.factory.instalog.plugins import buffer_priority_file
+from cros.factory.instalog.utils import arg_utils
+from cros.factory.instalog.utils.arg_utils import Arg
+from cros.factory.instalog.utils import time_utils
+
+
+DEFAULT_SEPARATE_TIME = '2022-01-01'
+
+
+class BufferPriorityFactoryReport(buffer_priority_file.BufferPriorityFile):
+  ARGS = arg_utils.MergeArgs(buffer_priority_file.BufferPriorityFile.ARGS, [
+      Arg(
+          'separate_time', str,
+          'The time to separate the priority. The earlier events will have '
+          'higher priority.', default=DEFAULT_SEPARATE_TIME)
+  ])
+
+  def __init__(self, *args, **kwargs):
+    self.separate_time = None
+    super().__init__(*args, **kwargs)
+
+  def EventLevel(self, event):
+    self.separate_time = time_utils.DatetimeToUnixtime(
+        datetime.datetime.strptime(self.args.separate_time,
+                                   json_utils.FORMAT_DATE))
+    if event.get('__report__') is True:
+      return 0
+    if event.get('__process__') is True:
+      return 1
+    if 'time' in event and event.get('time') > self.separate_time:
+      return 2
+    return 3
+
+
+if __name__ == '__main__':
+  plugin_base.main()
