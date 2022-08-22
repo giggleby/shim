280e05cdbba6061bae32b3cedbf4169df6563035
Yong Hong <yhong@chromium.org>
2022-08-22 16:56:28 +0800

    probe_info_service: limit # of mutations in datestore batch mode
    `google.cloud.datastore.batch` accepts at most 500 mutations.  The
    limitation blocks the migration scripts from modifying all entities
    at once.  This CL revises the migration scripts to modify and save
    entities with multiple batches to fix the error.
    
    BUG=b:242303302
    TEST=manual test
    
    Change-Id: Ieb5c17c41a7f67160003b9fb1851b5858ccc4ee2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/3841261
    Reviewed-by: Clark Chung <ckclark@chromium.org>
    Tested-by: Yong Hong <yhong@chromium.org>
    Auto-Submit: Yong Hong <yhong@chromium.org>
    Commit-Queue: Yong Hong <yhong@chromium.org>

--- py/probe_info_service/app_engine/migration_scripts/migration_script_0001.py
+++ py/probe_info_service/app_engine/migration_scripts/migration_script_0001.py
@@ -3,6 +3,7 @@
 # found in the LICENSE file.
 """Converts legacy probe info entities to `models.AVLProbeEntity`s."""
 
+import itertools
 import logging
 
 from google.cloud import datastore
@@ -17,51 +18,57 @@ class Migrator(migration_script_types.Migrator):
   _FROM_KIND = 'component_probe_info'
   _TO_KIND = 'AVLProbeEntity'
   _TO_PARENT_KIND = 'AVLProbeEntityCIDGroup'
+  _MAX_NUM_ENTITIES_PER_BATCH = 200
+
+  def _MigrateEntity(self, client, batch, legacy_entity):
+    try:
+      comp_id, qual_id = map(int, (legacy_entity.key.name or '').split('-'))
+    except ValueError as ex:
+      raise ValueError(
+          f'Get entity key with incorrect format: {legacy_entity.key!r}.'
+      ) from ex
+    comp_probe_info = stubby_pb2.ComponentProbeInfo()
+    comp_probe_info.ParseFromString(legacy_entity['bytes'])
+    if (comp_probe_info.component_identity.component_id != comp_id or
+        comp_probe_info.component_identity.qual_id != qual_id):
+      raise ValueError('Detect key and properties mismatch from '
+                       f'{self._FROM_KIND} entity.')
+    entity = client.entity(
+        client.key(self._TO_PARENT_KIND, str(comp_id), self._TO_KIND,
+                   str(qual_id)))
+    entity['cid'] = comp_id
+    entity['qid'] = qual_id
+    entity['readable_label'] = ''
+    if config.Config().is_prod:
+      entity['probe_info'] = comp_probe_info.probe_info.SerializeToString()
+    else:
+      entity['probe_info'] = text_format.MessageToString(
+          comp_probe_info.probe_info)
+    batch.put(entity)
+    batch.delete(legacy_entity.key)
 
   def Run(self) -> migration_script_types.MigrationResultCase:
     client = datastore.Client()
     try:
-      legacy_entities = client.query(kind=self._FROM_KIND).fetch()
+      legacy_entities_iter = client.query(kind=self._FROM_KIND).fetch()
     except Exception:
       logging.exception('Unable to fetch %r entities.', self._FROM_KIND)
       return migration_script_types.MigrationResultCase.FAILED_ROLLBACKED
 
-    batch = client.batch()
-    batch.begin()
     num_converted_entities = 0
+    might_have_remaining_legacy_entities = True
     try:
-      for legacy_entity in legacy_entities:
-        try:
-          comp_id, qual_id = map(int, (legacy_entity.key.name or '').split('-'))
-        except ValueError as ex:
-          raise ValueError(
-              f'Get entity key with incorrect format: {legacy_entity.key!r}.'
-          ) from ex
-        comp_probe_info = stubby_pb2.ComponentProbeInfo()
-        comp_probe_info.ParseFromString(legacy_entity['bytes'])
-        if (comp_probe_info.component_identity.component_id != comp_id or
-            comp_probe_info.component_identity.qual_id != qual_id):
-          raise ValueError('Detect key and properties mismatch from '
-                           f'{self._FROM_KIND} entity.')
-        entity = client.entity(
-            client.key(self._TO_PARENT_KIND, str(comp_id), self._TO_KIND,
-                       str(qual_id)))
-        entity['cid'] = comp_id
-        entity['qid'] = qual_id
-        entity['readable_label'] = ''
-        if config.Config().is_prod:
-          entity['probe_info'] = comp_probe_info.probe_info.SerializeToString()
-        else:
-          entity['probe_info'] = text_format.MessageToString(
-              comp_probe_info.probe_info)
-        batch.put(entity)
-        batch.delete(legacy_entity.key)
-        num_converted_entities += 1
-      batch.commit()
+      while might_have_remaining_legacy_entities:
+        with client.batch() as batch:
+          might_have_remaining_legacy_entities = False
+          for legacy_entity in itertools.islice(
+              legacy_entities_iter, self._MAX_NUM_ENTITIES_PER_BATCH):
+            might_have_remaining_legacy_entities = True
+            self._MigrateEntity(client, batch, legacy_entity)
+            num_converted_entities += 1
     except Exception:
       logging.exception('Failed to perform the migration from %r to %r.',
                         self._FROM_KIND, self._TO_KIND)
-      batch.rollback()
       return migration_script_types.MigrationResultCase.FAILED_ROLLBACKED
     logging.info('Migrated %d entries from %s to %s.', num_converted_entities,
                  self._FROM_KIND, self._TO_KIND)
--- py/probe_info_service/app_engine/migration_scripts/migration_script_0002.py
+++ py/probe_info_service/app_engine/migration_scripts/migration_script_0002.py
@@ -3,6 +3,7 @@
 # found in the LICENSE file.
 """Appends some fields in `models.AVLProbeEntry`."""
 
+import itertools
 import logging
 
 from google.cloud import datastore
@@ -12,33 +13,36 @@ from cros.factory.probe_info_service.app_engine.migration_scripts import migrati
 
 class Migrator(migration_script_types.Migrator):
   _KIND = 'AVLProbeEntity'
+  _MAX_NUM_ENTITIES_PER_BATCH = 200
 
   def Run(self) -> migration_script_types.MigrationResultCase:
     client = datastore.Client()
     try:
       query = client.query(kind=self._KIND)
-      entities = query.fetch()
+      entities_iter = query.fetch()
     except Exception:
       logging.exception('Unable to fetch %r entities.', self._KIND)
       return migration_script_types.MigrationResultCase.FAILED_ROLLBACKED
 
-    batch = client.batch()
-    batch.begin()
     num_handled_entities = 0
+    might_have_remaining_entities = True
     try:
-      for entity in entities:
-        entity.update({
-            'is_valid': True,
-            'is_tested': False,
-            'is_justified_for_overridden': False,
-        })
-        batch.put(entity)
-        num_handled_entities += 1
-      batch.commit()
+      while might_have_remaining_entities:
+        with client.batch() as batch:
+          might_have_remaining_entities = False
+          for entity in itertools.islice(entities_iter,
+                                         self._MAX_NUM_ENTITIES_PER_BATCH):
+            might_have_remaining_entities = True
+            entity.update({
+                'is_valid': True,
+                'is_tested': False,
+                'is_justified_for_overridden': False,
+            })
+            batch.put(entity)
+            num_handled_entities += 1
     except Exception:
       logging.exception('Failed to perform the migration to add fields to %r.',
                         self._KIND)
-      batch.rollback()
       return migration_script_types.MigrationResultCase.FAILED_ROLLBACKED
     logging.info('Added newly introduced fields to %d %s entries.',
                  num_handled_entities, self._KIND)
