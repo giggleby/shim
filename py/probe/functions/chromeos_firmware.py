e8ee32224852d11e538582e596e617f817b2c39b
Yu-An Wang <wyuang@google.com>
2022-08-26 13:16:26 +0800

    probe: probe fingerprint firmware
    This CL includes the following changes:
    1. Reimplement the logic in CL:3376006 to get reference FP firmware
    2. Probe the version string and RO hash of FMAP + EC_RO sections, and
       add it to default probe statement
    3. Complete the unit test for chromeos_firmware probe function
    
    BUG=b:237080363
    TEST=make test
    TEST=probe eval-function chromeos_firmware ro_fp_firmware
    
    Change-Id: Ifef8b61aed11bde3f87c35f36dbfe4cfc32574d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/3857331
    Reviewed-by: Yong Hong <yhong@chromium.org>
    Commit-Queue: Yu-An Wang <wyuang@google.com>
    Tested-by: Yu-An Wang <wyuang@google.com>

--- py/probe/functions/chromeos_firmware.py
+++ py/probe/functions/chromeos_firmware.py
@@ -2,20 +2,35 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import glob
 import hashlib
 import logging
+import os
 import re
 import tempfile
 
+from cros.factory.gooftool import common as gooftool_common
+from cros.factory.gooftool import cros_config as cros_config_module
 from cros.factory.gooftool import crosfw
 from cros.factory.probe.lib import cached_probe_function
 from cros.factory.utils.arg_utils import Arg
 from cros.factory.utils import file_utils
+from cros.factory.utils import fmap
 from cros.factory.utils import process_utils
+from cros.factory.utils import sys_utils
 from cros.factory.utils import type_utils
 
-FIELDS = type_utils.Enum(
-    ['firmware_keys', 'ro_main_firmware', 'ro_ec_firmware', 'ro_pd_firmware'])
+
+FIELDS = type_utils.Enum([
+    'firmware_keys', 'ro_main_firmware', 'ro_ec_firmware', 'ro_pd_firmware',
+    'ro_fp_firmware'
+])
+
+ROOTFS_FP_FIRMWARE_DIR = 'opt/google/biod/fw'
+
+
+class FPReferenceFirmwareError(Exception):
+  pass
 
 
 def _FwKeyHash(fw_file_path, key_name):
@@ -72,6 +87,59 @@ def _EcRoHash(image):
       'version': _AddFirmwareIdTag(image).lstrip('#')}
 
 
+def _GetFPReferenceFirmware(firmware_dir, fingerprint_board_name):
+  """Gets full path of the reference fingreprint firmware.
+
+  Searches the firmware under ROOTFS_FP_FIRMWARE_DIR. The firmware is in the
+  form of <fingerprint_board_name>_<RO version>-RO_<RW version>-RW.bin. The
+  <RO version> and <RW version> need not to be strictly matched.
+
+  Raises:
+    FPReferenceFirmwareError: if there are multiple files or no file matched.
+  """
+  firmware_files = glob.glob(
+      f'{firmware_dir}/{fingerprint_board_name}_*-RO_*-RW.bin')
+  if len(firmware_files) != 1:
+    raise FPReferenceFirmwareError(
+        f'No firmware found under {firmware_dir}' if not firmware_files else
+        f'Multiple firmwares found under {firmware_dir}')
+  return firmware_files[0]
+
+
+def DumpFPFirmware():
+  """Dumps the fingerprint sensor on the device.
+
+  Some sections of FP firmware are locked so that there's no way to dump the
+  whole firmware image. We take FMAP of the firmware in release image as
+  reference, then dump the neceesary sections only.
+
+  Returns:
+    Path of the dumped firmware.
+
+  Raises:
+    FPReferenceFirmwareError if unable to find reference firmware.
+  """
+  release_rootfs = gooftool_common.Util().GetReleaseRootPartitionPath()
+  with sys_utils.MountPartition(release_rootfs) as root:
+    cros_config = cros_config_module.CrosConfig()
+    board_name = cros_config.GetFingerPrintBoard()
+    firmware_dir = os.path.join(root, ROOTFS_FP_FIRMWARE_DIR)
+    reference_firmware = _GetFPReferenceFirmware(firmware_dir, board_name)
+    firmware_blob = file_utils.ReadFile(reference_firmware, encoding=None)
+    firmware_image = fmap.FirmwareImage(firmware_blob)
+
+  # We need EC_RO and RO_FRID, and RO_FRID is included in EC_RO already.
+  ro_offset, ro_size = firmware_image.get_section_area('EC_RO')
+
+  dump_path = f'{tempfile.mkdtemp()}/cache_fp_ro_dump.bin'
+  process_utils.CheckCall([
+      'ectool', '--name=cros_fp', 'flashread',
+      str(ro_offset),
+      str(ro_size), dump_path
+  ])
+  return dump_path
+
+
 def CalculateFirmwareHashes(fw_file_path):
   """Calculate the volatile hashes corresponding to a firmware blob.
 
@@ -124,6 +192,9 @@ class ChromeosFirmwareFunction(cached_probe_function.LazyCachedProbeFunction):
     the readonly PD firmware and also the version of the firmware.  Since
     not all devices have a PD flash chip, it's possible that the output of this
     function is empty.
+
+  - If ``field="ro_fp_firmware"``, this function outputs the sha256 hash of
+    the readonly fingerprint firmware and also the version of the firmware.
 
   Examples
   --------
@@ -196,8 +267,10 @@ class ChromeosFirmwareFunction(cached_probe_function.LazyCachedProbeFunction):
     if category == FIELDS.ro_main_firmware:
       fw_file_path = crosfw.LoadMainFirmware().GetFileName(
           sections=['RO_SECTION'])
-    if category == FIELDS.ro_ec_firmware:
+    elif category == FIELDS.ro_ec_firmware:
       fw_file_path = crosfw.LoadEcFirmware().GetFileName()
-    if category == FIELDS.ro_pd_firmware:
+    elif category == FIELDS.ro_pd_firmware:
       fw_file_path = crosfw.LoadPDFirmware().GetFileName()
+    elif category == FIELDS.ro_fp_firmware:
+      fw_file_path = DumpFPFirmware()
     return CalculateFirmwareHashes(fw_file_path)
---
+++ py/probe/functions/chromeos_firmware_unittest.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python3
+# Copyright 2022 The ChromiumOS Authors.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import hashlib
+import os
+import tempfile
+import unittest
+from unittest import mock
+
+from cros.factory.probe.functions import chromeos_firmware
+from cros.factory.utils import file_utils
+
+
+class ChromeosFirmwareTest(unittest.TestCase):
+
+  DEV_KEY = 'b11d74edd286c144e1135b49e7f0bc20cf041f10'
+  ROOTFS_FP_FIRMWARE_DIR = 'opt/google/biod/fw'
+
+  def setUp(self):
+    self.fake_rootfs = tempfile.mkdtemp()
+
+    patcher = mock.patch(
+        'cros.factory.gooftool.common.Util.GetReleaseRootPartitionPath')
+    self.fake_get_release_rootfs = patcher.start()
+    self.fake_get_release_rootfs.return_value = self.fake_rootfs
+    self.addCleanup(patcher.stop)
+
+    patcher = mock.patch('cros.factory.utils.sys_utils.MountPartition')
+    self.fake_mount = patcher.start()
+    self.fake_mount.return_value.__enter__.return_value = self.fake_rootfs
+    self.addCleanup(patcher.stop)
+
+    patcher = mock.patch('cros.factory.gooftool.cros_config.CrosConfig')
+    self.mock_cros_config = patcher.start()
+    self.mock_cros_config.return_value.GetFingerPrintBoard.return_value = (
+        'board')
+    self.addCleanup(patcher.stop)
+
+    patcher = mock.patch('cros.factory.gooftool.crosfw.FirmwareImage')
+    self.mock_fw_image = patcher.start()
+    self.addCleanup(patcher.stop)
+
+    patcher = mock.patch('cros.factory.utils.process_utils.CheckOutput')
+    self.mock_check_output = patcher.start()
+    self.addCleanup(patcher.stop)
+
+  def _CreateFakeFPFirmware(self, filename):
+    fw_dir = os.path.join(self.fake_rootfs, self.ROOTFS_FP_FIRMWARE_DIR)
+    file_utils.TryMakeDirs(fw_dir)
+    file_utils.TouchFile(os.path.join(fw_dir, filename))
+
+  def testGetFirmwareKeys(self):
+    self.mock_check_output.return_value = 'Key sha1sum: somehashvalue'
+    keys = chromeos_firmware.GetFirmwareKeys('unused_path')
+    self.assertEqual(keys, {
+        'key_recovery': 'kv3#somehashvalue',
+        'key_root': 'kv3#somehashvalue'
+    })
+
+  def testGetFirmwareKeys_DevKeys(self):
+    self.mock_check_output.return_value = f'Key sha1sum: {self.DEV_KEY}'
+    keys = chromeos_firmware.GetFirmwareKeys('unused_path')
+    self.assertEqual(
+        keys, {
+            'key_recovery': f'kv3#{self.DEV_KEY}#devkeys/rootkey',
+            'key_root': f'kv3#{self.DEV_KEY}#devkeys/rootkey',
+        })
+
+  def testGetFirmwareKeys_MissingSha1Sum(self):
+    self.mock_check_output.return_value = 'Key not_sha1sum: somehashvalue'
+    keys = chromeos_firmware.GetFirmwareKeys('unused_path')
+    self.assertEqual(keys, {
+        'key_recovery': None,
+        'key_root': None
+    })
+
+  @mock.patch('cros.factory.utils.file_utils.ReadFile', mock.Mock())
+  def testCalculateFirmwareHashes_EcRoHash(self):
+
+    def MockHasSection(section):
+      return section in ('EC_RO', 'RO_FRID')
+
+    def MockGetSection(section):
+      return {
+          'EC_RO': b'ec_ro',
+          'RO_FRID': b'version_string'
+      }[section]
+
+    mock_fw_image = self.mock_fw_image.return_value
+    mock_fw_image.has_section.side_effect = MockHasSection
+    mock_fw_image.get_section.side_effect = MockGetSection
+    mock_fw_image.get_fmap_blob.return_value = b''
+
+    expected = {
+        'hash': hashlib.sha256(b'ec_ro').hexdigest(),
+        'version': 'version_string'
+    }
+    self.assertEqual(expected,
+                     chromeos_firmware.CalculateFirmwareHashes('unused_path'))
+
+  @mock.patch('cros.factory.utils.file_utils.ReadFile', mock.Mock())
+  def testCalculateFirmwareHashes_MainRoHash(self):
+
+    def MockHasSection(section):
+      return section in ('GBB', 'RO_FRID', 'RO_SECTION')
+
+    def MockGetSection(section):
+      return {
+          'GBB': b'gbb',
+          'RO_SECTION': b'ro_section',
+          'RO_FRID': b'version_string'
+      }[section]
+
+    mock_fw_image = self.mock_fw_image.return_value
+    mock_fw_image.has_section.side_effect = MockHasSection
+    mock_fw_image.get_section.side_effect = MockGetSection
+    mock_fw_image.get_fmap_blob.return_value = b''
+
+    expected = {
+        'hash': hashlib.sha256(b'ro_section').hexdigest(),
+        'version': 'version_string'
+    }
+    self.assertEqual(expected,
+                     chromeos_firmware.CalculateFirmwareHashes('unused_path'))
+
+  @mock.patch('cros.factory.utils.file_utils.ReadFile', mock.Mock())
+  def testCalculateFirmwareHashes_FailedToLoadFW(self):
+    self.mock_fw_image.side_effect = Exception
+    self.assertIsNone(chromeos_firmware.CalculateFirmwareHashes('unused_path'))
+
+  @mock.patch('cros.factory.utils.fmap.FirmwareImage')
+  @mock.patch('cros.factory.utils.process_utils.CheckCall', mock.Mock())
+  def testDumpFPFirmware_Succeed(self, mock_fmap_fw_image):
+    self._CreateFakeFPFirmware('board_1111-RO_1111-RW.bin')
+    mock_fmap_fw_image.return_value.get_section_area.return_value = (0, 1)
+    chromeos_firmware.DumpFPFirmware()
+
+  def testDumpFPFirmware_NoFirmwareMatched(self):
+    with self.assertRaises(chromeos_firmware.FPReferenceFirmwareError):
+      chromeos_firmware.DumpFPFirmware()
+
+  def testDumpFPFirmware_MultipleFirmwareMatched(self):
+    self._CreateFakeFPFirmware('board_1111-RO_1111-RW.bin')
+    self._CreateFakeFPFirmware('board_2222-RO_2222-RW.bin')
+    with self.assertRaises(chromeos_firmware.FPReferenceFirmwareError):
+      chromeos_firmware.DumpFPFirmware()
+
+
+if __name__ == '__main__':
+  unittest.main()
