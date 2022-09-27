808208e45b04c829831b3e85f816ae004f3589d5
Wen-Ting Tseng <wdzeng@chromium.org>
2022-09-27 13:07:00 +0800

    gooftool: Update unittest for gooftool write_protect_target
    This CL updates the unittest for the refactoring of gooftool
    write_protect_target.
    
    BUG=b:239791358
    TEST=make test
    
    Change-Id: I4ea7807396c81f7882e81f088b1a14d568c06ca4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/3915997
    Reviewed-by: Sung-Chi Li <lschyi@chromium.org>
    Commit-Queue: Wen-Ting Tseng <wdzeng@chromium.org>
    Tested-by: Wen-Ting Tseng <wdzeng@chromium.org>

--- py/gooftool/write_protect_target_unittest.py
+++ py/gooftool/write_protect_target_unittest.py
@@ -4,7 +4,6 @@
 # found in the LICENSE file.
 
 import unittest
-from unittest import mock
 
 from cros.factory.gooftool import write_protect_target
 
@@ -13,35 +12,11 @@ class WriteProtectTargetUnittest(unittest.TestCase):
 
   def testCreateAvailableTargets(self):
     for target in write_protect_target.WriteProtectTargetType:
-      if target.name != 'FPMCU':
-        write_protect_target.CreateWriteProtectTarget(target)
-
-  @mock.patch('cros.factory.gooftool.write_protect_target.Shell')
-  def testCreateBloonchipperWriteProtectTarget(self, mock_shell):
-    mock_shell.return_value.stdout = 'bloonchipper'
-    target = write_protect_target.CreateWriteProtectTarget(
-        write_protect_target.WriteProtectTargetType.FPMCU)
-    self.assertIsInstance(target,
-                          write_protect_target.BloonchipperWriteProtectTarget)
-
-  @mock.patch('cros.factory.gooftool.write_protect_target.Shell')
-  def testCreateDartmonkeyWriteProtectTarget(self, mock_shell):
-    mock_shell.return_value.stdout = 'dartmonkey'
-    target = write_protect_target.CreateWriteProtectTarget(
-        write_protect_target.WriteProtectTargetType.FPMCU)
-    self.assertIsInstance(target,
-                          write_protect_target.DartmonkeyWriteProtectTarget)
+      write_protect_target.CreateWriteProtectTarget(target)
 
   def testCreateTargetWithWrongType(self):
     with self.assertRaises(TypeError):
-      write_protect_target.CreateWriteProtectTarget('random_value')
-
-  @mock.patch('cros.factory.gooftool.write_protect_target.Shell')
-  def testUnimplmentedFPMCUTarget(self, mock_shell):
-    mock_shell.return_value.stdout = 'unimplemented'
-    with self.assertRaises(ValueError):
-      write_protect_target.CreateWriteProtectTarget(
-          write_protect_target.WriteProtectTargetType.FPMCU)
+      write_protect_target.CreateWriteProtectTarget('inexistent_type')
 
 
 if __name__ == '__main__':
