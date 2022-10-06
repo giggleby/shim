b4d20955172141c1f2be4caee51758679ed50e00
Wen-Ting Tseng <wdzeng@chromium.org>
2022-10-06 16:03:23 +0800

    factory: Update license boilerplate text in source code files
    The policies ware changed in CL:3646110. Currently header comments for
    copyright in many files do not meet the latest policies.
    
    - There is a space between `Chromium` and `OS`. It should be
      `ChromiumOS`.
    - The `All Rights Reserved.` is redundant and should be removed.
    
    This CL fixes the above problems for each files in the factory project
    by replacing
    `Copyright (\d+) The Chromium OS Authors. All rights reserved.` with
    `Copyright $1 The ChromiumOS Authors`.
    
    All files changed are limited to a 1-line deletion and a 1-line
    addition; that is, a 1-line replacement. See the following list for
    exceptions. This can be examined by
    `git diff --stat <before> <after> | grep -v '| 2 +-'`.
    
    - LICENSE: (2-2+)
      Change `Google Inc.` to `Google LLC`.
    - setup/create_hwid_bundle.sh: (2-2+)
      There are two copyright notations in this file.
    
    This CL passes the '_check_cros_license' hook check.
    
    BUG=None
    TEST=None
    
    Change-Id: Ic679c46b9c36d05887f775bb3de568ae8b9a3c57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/3935952
    Reviewed-by: Stimim Chen <stimim@chromium.org>
    Tested-by: Wen-Ting Tseng <wdzeng@chromium.org>
    Commit-Queue: Wen-Ting Tseng <wdzeng@chromium.org>

--- py/hwid_extractor/request_handler.py
+++ py/hwid_extractor/request_handler.py
@@ -1,4 +1,4 @@
-# Copyright 2021 The Chromium OS Authors. All rights reserved.
+# Copyright 2021 The ChromiumOS Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 """A tool to quickly extract HWID and serial no. from DUT."""
--- py/umpire/server/service/umpire_sync.py
+++ py/umpire/server/service/umpire_sync.py
@@ -1,4 +1,4 @@
-# Copyright 2021 The Chromium OS Authors. All rights reserved.
+# Copyright 2021 The ChromiumOS Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 """Synchronize all secondary umpires to the same version of bundles
