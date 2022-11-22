36ad1e1c37a4cc98be32e134a0160e4d1ae506af
Cheng Yueh <cyueh@google.com>
2022-11-22 19:05:59 +0800

    display_manager: Add more rpc functions
    These functions can be used in pytests and cmdline tools.
    
    BUG=b:253938058
    TEST=(1) Test that mirror mode is set when toolkit installed
         (2) Test that mirror mode is unset when running the HDMI item
    
    Change-Id: I6ec164b1615c191dcf20f658bb2b602e4c127805
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/4105362
    Reviewed-by: Phoebe Wang <phoebewang@chromium.org>
    Tested-by: Cheng Yueh <cyueh@chromium.org>
    Auto-Submit: Cheng Yueh <cyueh@chromium.org>
    Commit-Queue: Phoebe Wang <phoebewang@chromium.org>

--- py/goofy/plugins/display_manager.py
+++ py/goofy/plugins/display_manager.py
@@ -2,11 +2,39 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import enum
 import logging
+from typing import Any, Dict, List, Optional
 
+from cros.factory.goofy import goofy_rpc
 from cros.factory.goofy.plugins import plugin
 from cros.factory.test import state
+from cros.factory.utils import sync_utils
 from cros.factory.utils import type_utils
+
+
+_KEEP_LIST = (
+    'edid',
+    'id',
+    'isInternal',
+    'isPrimary',
+    'mirroringDestinationIds',
+    'mirroringSourceId',
+    'name',
+)
+
+
+class MirrorMode(str, enum.Enum):
+  off = 'off'
+  normal = 'normal'
+
+  def __str__(self):
+    return self.value
+
+
+def HasMirror(server_proxy: goofy_rpc.GoofyRPC):
+  display_info = server_proxy.DeviceGetDisplayInfo()
+  return any(info['mirroringDestinationIds'] for info in display_info)
 
 
 class DisplayManager(plugin.Plugin):
@@ -20,18 +48,90 @@ class DisplayManager(plugin.Plugin):
   def OnStop(self):
     super().OnStop()
     if self.mirror_mode:
-      err = state.GetInstance().DeviceSetDisplayMirrorMode({'mode': 'off'})
-      if err is not None:
-        logging.warning('Failed to turn off the mirror mode: %s', err)
-      else:
-        logging.info('Turned off display mirror mode.')
+      self.SetMirrorMode(MirrorMode.off, timeout=-1)
 
   @type_utils.Overrides
   def OnStart(self):
     super().OnStart()
     if self.mirror_mode:
-      err = state.GetInstance().DeviceSetDisplayMirrorMode({'mode': 'normal'})
-      if err is not None:
-        logging.warning('Failed to turn on the mirror mode: %s', err)
-      else:
-        logging.info('Turned on display mirror mode.')
+      self.SetMirrorMode(MirrorMode.normal, timeout=-1)
+
+  @plugin.RPCFunction
+  def SetMirrorMode(self, mode: str, timeout: Optional[int]):
+    """Sets mirror mode.
+
+    Args:
+      mode: One of value of MirrorMode.
+      timeout: maximum number of seconds to wait, None means forever, -1 means
+      nonblocking.
+
+    Raises:
+      ValueError if mode is not a value of MirrorMode.
+    """
+    server_proxy: goofy_rpc.GoofyRPC = state.GetInstance()
+    # Value type check.
+    MirrorMode(mode)
+    info = {
+        'mode': mode
+    }
+    err = server_proxy.DeviceSetDisplayMirrorMode(info)
+    if err is not None:
+      logging.warning('Failed to turn %s the mirror mode: %s', mode, err)
+    else:
+      logging.info('Turned %s display mirror mode.', mode)
+
+    if timeout == -1:
+      return
+
+    def MirrorModeMatchEvent():
+      has_mirror = HasMirror(server_proxy)
+      return (mode == MirrorMode.off) != has_mirror
+
+    sync_utils.WaitFor(MirrorModeMatchEvent, timeout)
+
+  @plugin.RPCFunction
+  def SetMainDisplay(self, display_id: str, timeout: Optional[int]):
+    """Sets main display.
+
+    Args:
+      display_id: The id of the display. Use ListDisplayInfo to find id.
+      timeout: In seconds. -1 means nonblocking.
+    """
+    server_proxy: goofy_rpc.GoofyRPC = state.GetInstance()
+    err = server_proxy.DeviceSetDisplayProperties(display_id,
+                                                  {'isPrimary': True})
+    if err is not None:
+      raise RuntimeError(f'Failed to set the main display: {err}')
+
+    if timeout == -1:
+      return
+
+    def BecomePrimaryEvent():
+      display_info = server_proxy.DeviceGetDisplayInfo()
+      for info in display_info:
+        if info['id'] == display_id:
+          return info['isPrimary']
+      return False
+
+    sync_utils.WaitFor(BecomePrimaryEvent, timeout)
+
+  @plugin.RPCFunction
+  def ListDisplayInfo(self, verbose: bool):
+    """Lists display info.
+
+    Args:
+      verbose: Show full information.
+
+    Returns:
+      A list of display info.
+    """
+    server_proxy: goofy_rpc.GoofyRPC = state.GetInstance()
+    display_info: List[Dict[str, Any]] = server_proxy.DeviceGetDisplayInfo()
+    if not verbose:
+      for index, info in enumerate(display_info):
+        display_info[index] = {
+            key: value
+            for key, value in info.items()
+            if key in _KEEP_LIST
+        }
+    return display_info
