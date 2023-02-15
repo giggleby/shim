afdfa18d999821596cd59d1ac7a59ff1fd30ec89
Wen-Ting Tseng <wdzeng@chromium.org>
2023-02-15 18:26:44 +0800

    ssh_integration_test: Add more tests
    This CL adds tests for Push, PushDirectory and Pull of SSHLink.
    
    This CL updates the way reading identity files to integrates with the
    partner_ssh_rsa identity file.
    
    BUG=b:267715472
    BUG=b:260620797
    TEST=make test
    TEST=Run integration test on a real DUT
    
    Change-Id: I68d1d76872c7f22c6b67ee648977500609bf14b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/4252998
    Tested-by: Wen-Ting Tseng <wdzeng@chromium.org>
    Reviewed-by: Chih-Yao Chuang <jasonchuang@google.com>
    Commit-Queue: Wen-Ting Tseng <wdzeng@chromium.org>

--- py/device/links/ssh_integration_test.py
+++ py/device/links/ssh_integration_test.py
@@ -3,62 +3,145 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""An integration test for SSHLink"""
+"""An integration test for SSHLink
+
+Manually run this unittest with following command::
+
+  ./bin/factory_env py/device/links/ssh_integration_test.py DUT_IP
+
+You may need to explicitly specify a key to authenticate::
+
+  ./bin/factory_env py/device/links/ssh_integration_test.py \
+  DUT_IP -i /path/to/key
+"""
 
 import argparse
 import logging
 import os
+import random
+import string
 import tempfile
+from typing import List, Union
 import unittest
 
+from cros.factory.device import device_types
 from cros.factory.device.links import local
 from cros.factory.device.links import ssh
-from cros.factory.test.env import paths
+from cros.factory.utils import file_utils
+from cros.factory.utils import type_utils
 
-dut_options = dict(
-    identity=os.path.join(paths.FACTORY_DIR, 'setup', 'sshkeys',
-                          'testing_rsa'))
 
-class SSHLinkUnittest(unittest.TestCase):
+dut_options = {}
+
+
+def _GenerateRandomString() -> str:
+  """Generate a random string with length 256."""
+  return ''.join(random.choices(string.ascii_letters, k=256))
+
+
+class SSHLinkIntegrationTest(unittest.TestCase):
+
+  ssh_link: ssh.SSHLink
+
+  @type_utils.Overrides
   def setUp(self):
-    if dut_options.get('identity'):
-      os.chmod(dut_options.get('identity'), 0o600)
-    self.ssh = ssh.SSHLink(**dut_options)
-    self.local = local.LocalLink()
+    self.ssh_link = ssh.SSHLink(**dut_options)
 
-  def _RunAndCompare(self, cmd):
-    # get output of SSHLink
-    ssh_output = tempfile.TemporaryFile('w+')
-    self.ssh.Shell(cmd, stdout=ssh_output)
-
-    # get output of LocalLink
-    local_output = tempfile.TemporaryFile('w+')
-    self.local.Shell(cmd, stdout=local_output)
-
-    ssh_output.seek(0)
-    local_output.seek(0)
-    self.assertEqual(ssh_output.readlines(), local_output.readlines())
-
-    local_output.seek(0)
-    logging.info(local_output.readlines())
-    ssh_output.close()
-    local_output.close()
+  def _GetOutput(self, link: device_types.DeviceLink,
+                 cmd: Union[str, List[str]]) -> str:
+    with tempfile.TemporaryFile('w+') as stdout:
+      self.assertEqual(link.Shell(cmd, stdout=stdout).wait(), 0)
+      stdout.seek(0)
+      output = stdout.read()
+    return output
 
   def testListEchoQuote(self):
     cmd = ['echo', '\'']
-    self._RunAndCompare(cmd)
+    local_link = local.LocalLink()
+    self.assertEqual(
+        self._GetOutput(self.ssh_link, cmd), self._GetOutput(local_link, cmd))
 
   def testShellEchoQuote(self):
     cmd = 'echo "\'"'
-    self._RunAndCompare(cmd)
+    local_link = local.LocalLink()
+    self.assertEqual(
+        self._GetOutput(self.ssh_link, cmd), self._GetOutput(local_link, cmd))
 
   def testShellSemiColon(self):
     cmd = 'echo 123; echo 456; echo 789'
-    self._RunAndCompare(cmd)
+    local_link = local.LocalLink()
+    self.assertEqual(
+        self._GetOutput(self.ssh_link, cmd), self._GetOutput(local_link, cmd))
 
   def testListSemiColon(self):
     cmd = ['echo', '123', ';', 'echo', '456', ';', 'echo', '789']
-    self._RunAndCompare(cmd)
+    local_link = local.LocalLink()
+    self.assertEqual(
+        self._GetOutput(self.ssh_link, cmd), self._GetOutput(local_link, cmd))
+
+  def testCommandFailed(self):
+    cmd = '>&2 echo -n error; false'
+    with tempfile.TemporaryFile('w+') as stdout, tempfile.TemporaryFile(
+        'w+') as stderr:
+      proc = self.ssh_link.Shell(cmd, stdout=stdout, stderr=stderr)
+      returncode = proc.wait()
+      stdout.seek(0)
+      stderr.seek(0)
+      self.assertEqual(stdout.read(), '')
+      self.assertEqual(stderr.read(), 'error')
+      self.assertEqual(returncode, 1)
+
+  def testPush(self):
+    """Tests that SSHLink can push a file from local to remote."""
+    remote_file = '/tmp/test_file'
+    test_file_content = _GenerateRandomString()
+
+    with file_utils.UnopenedTemporaryFile() as temp_file:
+      file_utils.WriteFile(temp_file, test_file_content)
+      self.ssh_link.Push(temp_file, remote_file)
+
+    cmd = f'cat "{remote_file}" && rm "{remote_file}"'
+    self.assertEqual(self._GetOutput(self.ssh_link, cmd), test_file_content)
+
+  def testPushDirectory(self):
+    """Tests that SSHLink can push a directory from local to remote."""
+    remote_dir = '/tmp/test_dir'
+    test_file_content = _GenerateRandomString()
+
+    with file_utils.TempDirectory() as temp_dir:
+      test_file = file_utils.CreateTemporaryFile(dir=temp_dir)
+      file_utils.WriteFile(test_file, test_file_content)
+      self.ssh_link.PushDirectory(temp_dir, remote_dir)
+
+    remote_file = os.path.join(remote_dir, os.path.basename(test_file))
+    cmd = f'[ -d "{remote_dir}" ] && cat "{remote_file}" && rm -r {remote_dir}'
+    self.assertEqual(self._GetOutput(self.ssh_link, cmd), test_file_content)
+
+  def testPull(self):
+    """Tests that SSHLink can pull a file from remote to local."""
+    test_file_content = _GenerateRandomString()
+    self.assertEqual(
+        self.ssh_link.Shell(
+            f'echo -n "{test_file_content}" > /tmp/content').wait(), 0)
+
+    with file_utils.UnopenedTemporaryFile() as temp_file:
+      self.ssh_link.Pull('/tmp/content', temp_file)
+      self.assertEqual(file_utils.ReadFile(temp_file), test_file_content)
+
+    # Delete test file on remote.
+    self.assertEqual(self.ssh_link.Shell('rm -f /tmp/content').wait(), 0)
+
+  def testPullAndThenRead(self):
+    """Tests that SSHLink can read a file on remote."""
+    test_file_content = _GenerateRandomString()
+    self.assertEqual(
+        self.ssh_link.Shell(
+            f'echo -n "{test_file_content}" > /tmp/content').wait(), 0)
+    self.assertEqual(self.ssh_link.Pull('/tmp/content'), test_file_content)
+
+    # Delete test file on remote.
+    self.assertEqual(self.ssh_link.Shell('rm -f /tmp/content').wait(), 0)
+
 
 if __name__ == '__main__':
   logging.basicConfig(level=logging.INFO)
@@ -73,5 +156,5 @@ if __name__ == '__main__':
 
   logging.info('dut_options: %s', dut_options)
 
-  suite = unittest.TestLoader().loadTestsFromTestCase(SSHLinkUnittest)
+  suite = unittest.TestLoader().loadTestsFromTestCase(SSHLinkIntegrationTest)
   unittest.TextTestRunner().run(suite)
