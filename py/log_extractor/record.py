980f942ee104bfb97cd35944e7db784b01f59718
Phoebe Wang <phoebewang@google.com>
2023-03-09 17:20:37 +0800

    record: add SystemLogRecord, VarLogMessageRecord and TestlogRecord
    This CL adds SystemLogRecord, VarLogMessageRecord and TestlogRecord to
    load and validate system log, /var/log/message and testlog. It also
    implements "__str__" to print the underlying data into reader-friendly
    format and implements GetTestRunName() to get the name of a running
    test.
    
    BUG=b:265757602
    TEST=make test
    
    Change-Id: Ia35291d86208215407393a4d15554a1e5d7d8cf2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/4328191
    Commit-Queue: Phoebe Wang <phoebewang@chromium.org>
    Reviewed-by: Stimim Chen <stimim@chromium.org>
    Reviewed-by: Wei-Shuo Su <stevesu@google.com>
    Tested-by: Phoebe Wang <phoebewang@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/4675117
    Auto-Submit: Phoebe Wang <phoebewang@chromium.org>

--- py/log_extractor/record.py
+++ py/log_extractor/record.py
@@ -3,9 +3,12 @@
 # found in the LICENSE file.
 
 import abc
+import datetime
 import json
-from typing import Dict
+import re
+from typing import Dict, Optional
 
+from cros.factory.testlog import testlog
 from cros.factory.utils.schema import FixedDict
 from cros.factory.utils.schema import Scalar
 
@@ -38,6 +41,11 @@ class IRecord(abc.ABC):
     """Returns the number of seconds passed since 1970/01/01 00:00:00."""
     raise NotImplementedError
 
+  def _GetFormattedUTCTime(self):
+    """Transforms time to a human-readable format."""
+    return datetime.datetime.utcfromtimestamp(
+        self.GetTime()).strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+
   def __lt__(self, other):
     return self.GetTime() < other.GetTime()
 
@@ -60,3 +68,107 @@ class FactoryRecord(IRecord):
 
   def GetTime(self) -> float:
     return self['time']
+
+  def GetTestRunName(self) -> Optional[str]:
+    """Gets the test run name.
+
+    The name should be in `<testName>-<testRunId>` format. This helps us
+    identify which test is running.
+
+    Returns:
+      None if the record itself cannot infer which test is running.
+    """
+    return None
+
+
+class SystemLogRecord(FactoryRecord):
+  _SCHEMA = FixedDict(
+      'System log record schema', items={
+          'filePath':
+              Scalar('Path to the raw log file.', str),
+          'lineNumber':
+              Scalar(
+                  'The line number of the raw log file where the record is '
+                  'generated.', int),
+          'logLevel':
+              Scalar('The log level of the record.', str),
+          'message':
+              Scalar('Message text.', str),
+          'time':
+              Scalar('Time in seconds since the epoch of the record.', float),
+      }, allow_undefined_keys=True)
+  _SYSLOG_TO_STR_TEMPLATE = '[{log_level}] {time} {file_path}:{line_num} {msg}'
+
+  def __str__(self):
+    return self._SYSLOG_TO_STR_TEMPLATE.format(
+        log_level=self['logLevel'], file_path=self['filePath'],
+        line_num=self['lineNumber'], time=self._GetFormattedUTCTime(),
+        msg=self['message'])
+
+
+class VarLogMessageRecord(SystemLogRecord):
+  # Example output of message:
+  #   goofy[1845]: Test generic:SMT.Update (123-456) starting
+  _GOOFY_TEST_RUN_REGEX = (
+      r'goofy\[\d+\]: Test (?P<testName>.+) \((?P<testRunId>.+)\)')
+
+  def GetTestRunName(self) -> Optional[str]:
+    match = re.search(self._GOOFY_TEST_RUN_REGEX, self['message'])
+    if match:
+      testName = match.group('testName').strip()
+      testRunId = match.group('testRunId').strip()
+      return f'{testName}-{testRunId}'
+    return None
+
+
+# TODO: Change the start and end time of the StationTestRun event.
+class TestlogRecord(FactoryRecord):
+
+  _STATION_TO_STR_TEMPLATE = '[{log_level}] {time} {msg}'
+
+  @classmethod
+  def FromJSON(cls, json_str: str, check_valid: bool = True):
+    data = testlog.EventBase.FromJSON(json_str, check_valid)
+
+    return cls(data)
+
+  def GetTestRunName(self) -> Optional[str]:
+    return f'{self["testName"]}-{self["testRunId"]}'
+
+  def _BuildStrFromStationMessage(self) -> str:
+    msg_list = []
+    if 'filePath' in self and 'lineNumber' in self:
+      msg_list.append(f"{self['filePath']}:{self['lineNumber']}")
+    msg_list.append(self['message'])
+    return ' '.join(msg_list)
+
+  def _BuildStrFromStationInit(self):
+    return (f"Goofy init count: {self['count']}, "
+            f"success: {self['success']!r}")
+
+  def _BuildStrFromStationStatus(self):
+    msg_list = []
+    # StationTestRun is a subclass of StationStatus.
+    if isinstance(self._data, testlog.StationTestRun):
+      msg_list.append(f"{self.GetTestRunName()} {self['status']}")
+    if 'filePath' in self:
+      msg_list.append(f"{self['filePath']}")
+    msg_str = ' '.join(msg_list)
+    if 'parameters' in self:
+      param_to_str = json.dumps(self['parameters'], indent=2, sort_keys=True)
+      msg_str += f'\nparameters:\n{param_to_str}'
+    return msg_str
+
+  def __str__(self):
+    """Transforms a Testlog record to a reader-friendly format."""
+    msg = ''
+    if isinstance(self._data, testlog.StationMessage):
+      msg = self._BuildStrFromStationMessage()
+    elif isinstance(self._data, testlog.StationInit):
+      msg = self._BuildStrFromStationInit()
+    else:
+      msg = self._BuildStrFromStationStatus()
+
+    log_level = self['logLevel'] if 'logLevel' in self else 'INFO'
+    return self._STATION_TO_STR_TEMPLATE.format(
+        log_level=log_level, time=self._GetFormattedUTCTime(), msg=msg)
--- py/log_extractor/record_unittest.py
+++ py/log_extractor/record_unittest.py
@@ -28,5 +28,70 @@ class FactoryRecordTest(unittest.TestCase):
     record2 = record_module.FactoryRecord.FromJSON('{"time": 2.34}')
     self.assertTrue(record1 < record2)
 
+
+class SystemLogRecordTest(unittest.TestCase):
+
+  def testToStr(self):
+    message_record = record_module.SystemLogRecord.FromJSON(
+        '{"filePath": "testlog.py", "lineNumber": 230, "logLevel": "WARNING", '
+        '"time": 1656340134.0011251, "message": "factory msg"}')
+    self.assertEqual(
+        str(message_record),
+        '[WARNING] 2022-06-27T14:28:54.001125Z testlog.py:230 factory msg')
+
+
+class VarLogMessageRecordTest(unittest.TestCase):
+
+  def testGetTestRunName(self):
+    testrun_starting = record_module.VarLogMessageRecord.FromJSON(
+        '{"message": "goofy[1845]: Test generic:SMT.Update '
+        '(f9c665ff-55d0) starting"}', check_valid=False)
+    self.assertEqual(testrun_starting.GetTestRunName(),
+                     'generic:SMT.Update-f9c665ff-55d0')
+
+
+class TestlogRecordTest(unittest.TestCase):
+
+  def testGetTestRunName(self):
+    testrun_record = record_module.TestlogRecord.FromJSON(
+        '{"type": "station.test_run", "testName": "generic_main:Idle",'
+        '"testRunId": "abc-123", "time": 1656340134.0011251}',
+        check_valid=False)
+    self.assertEqual(testrun_record.GetTestRunName(),
+                     'generic_main:Idle-abc-123')
+
+  def testToStr(self):
+    message_record = record_module.TestlogRecord.FromJSON(
+        '{"type": "station.message", "filePath": "testlog.py", '
+        '"lineNumber": 230, "logLevel": "ERROR", "time": 1656340134.0011251,'
+        '"message": "err message"}', check_valid=False)
+    self.assertEqual(
+        str(message_record),
+        '[ERROR] 2022-06-27T14:28:54.001125Z testlog.py:230 err message')
+
+    testrun_record = record_module.TestlogRecord.FromJSON(
+        '{"type": "station.test_run", "time": 1656340134.0011251,'
+        '"testName": "generic_main:Idle", "testRunId": "abc-123",'
+        '"status": "RUNNING"}', check_valid=False)
+    self.assertEqual(
+        str(testrun_record),
+        '[INFO] 2022-06-27T14:28:54.001125Z generic_main:Idle-abc-123 RUNNING')
+
+    status_record = record_module.TestlogRecord.FromJSON(
+        '{"type": "station.status", "time": 1656340134.0011251, '
+        '"filePath": "testlog.py", "parameters": {"status": {'
+        '"type": "measurement"}}}', check_valid=False)
+    self.assertEqual(
+        str(status_record), '[INFO] 2022-06-27T14:28:54.001125Z testlog.py\n'
+        'parameters:\n{\n  "status": {\n    "type": "measurement"\n  }\n}')
+
+    init_record = record_module.TestlogRecord.FromJSON(
+        '{"type": "station.init", "time": 1656340134.0011251, "count": 1,'
+        '"success": true}', check_valid=False)
+    self.assertEqual(
+        str(init_record),
+        '[INFO] 2022-06-27T14:28:54.001125Z Goofy init count: 1, success: True')
+
+
 if __name__ == '__main__':
   unittest.main()
