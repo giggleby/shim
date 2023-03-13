98206ec9836405e03a991b779764d432ec866b45
Stimim Chen <stimim@google.com>
2023-03-13 16:58:38 +0800

    umpire: enable unittests
    Enable umpire unittests, also:
    1. rename test_parameter.json to test_factory_drive.json, so
       factory_drives_unittest.py can pass.
    2. Mock os functions in utils_unittest.py, so it won't touch the real
       operating system.
    
    BUG=b:273189623
    TEST=make test
    
    Change-Id: I89693225f044951f682629d181fa25aa37222496
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/4333621
    Reviewed-by: Sung-Chi Li <lschyi@chromium.org>
    Tested-by: Stimim Chen <stimim@chromium.org>
    Commit-Queue: Sung-Chi Li <lschyi@chromium.org>
    Auto-Submit: Stimim Chen <stimim@chromium.org>
    Reviewed-by: Ruei-fong Lyu <rueifong@google.com>

--- py/umpire/server/utils_unittest.py
+++ py/umpire/server/utils_unittest.py
@@ -11,16 +11,30 @@ from cros.factory.umpire.server import utils
 
 
 class CreateLoopDeviceTest(unittest.TestCase):
-
   @mock.patch('os.mknod', return_value=True)
   @mock.patch('os.chown', return_value=True)
-  def testCreateLoopDevice(self, *unused_mocked_funcs):
-    self.assertTrue(utils.CreateLoopDevice("/dev/loop", 0, 256))
+  @mock.patch('os.path.exists', return_value=False)
+  @mock.patch('cros.factory.umpire.server.utils._GetLoopDeviceStat',
+              return_value=utils.DEFAULT_LOOP_DEVICE_STAT)
+  @mock.patch('os.makedev', return_value=True)
+  def testCreateLoopDeviceSuccess(self, mocked_makedev, *unused_mocked_funcs):
+    loop_path_prefix = '/dev/loop'
+    start = 0
+    end = 256
+    self.assertTrue(utils.CreateLoopDevice(loop_path_prefix, start, end))
+    self.assertEqual(mocked_makedev.call_args_list, [
+        mock.call(utils.DEFAULT_LOOP_DEVICE_STAT.major_number, i)
+        for i in range(start, end)
+    ])
 
+  @mock.patch('os.makedev', return_value=True)
   @mock.patch('os.mknod', return_value=True)
   @mock.patch('os.chown', side_effect=OSError(errno.ENOENT, 'No such file'))
+  @mock.patch('os.path.exists', return_value=False)
+  @mock.patch('cros.factory.umpire.server.utils._GetLoopDeviceStat',
+              return_value=utils.DEFAULT_LOOP_DEVICE_STAT)
   def testCreateLoopDeviceRaiseException(self, *unused_mocked_funcs):
-    self.assertFalse(utils.CreateLoopDevice("/dev/loop", 0, 256))
+    self.assertFalse(utils.CreateLoopDevice('/dev/loop', 0, 256))
 
 
 if __name__ == '__main__':
