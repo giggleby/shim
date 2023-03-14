a06fc518a4d8e5c28bdb7b2132aa05b6083b1d73
Phoebe Wang <phoebewang@google.com>
2023-03-14 16:54:17 +0800

    file_utils: extract testlog and /var/log/message by test run id
    This CL adds a function called ExtractAndWriteRecordByTestRun to extract
    the logs by test run. The extract starts when reading the STARTED events
    and stops when reading the COMPLETED event.
    The extracted logs will be placed under
    `{output_root}/{test_run_name}/{output_fname}`.
    
    BUG=b:265757602
    TEST=make test
    
    Change-Id: Ieb8fc231826c3ee83200a4732e2df15bae95400b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/4337298
    Tested-by: Phoebe Wang <phoebewang@chromium.org>
    Reviewed-by: Stimim Chen <stimim@chromium.org>
    Reviewed-by: Wei-Shuo Su <stevesu@google.com>
    Commit-Queue: Phoebe Wang <phoebewang@chromium.org>

--- py/log_extractor/file_utils.py
+++ py/log_extractor/file_utils.py
@@ -3,9 +3,13 @@
 # found in the LICENSE file.
 
 import logging
+import os
 from typing import Callable, Dict, Optional, Tuple
 
 from cros.factory.log_extractor.record import IRecord
+from cros.factory.log_extractor import test_run_handler
+from cros.factory.log_extractor.test_run_handler import TestRunStatus
+from cros.factory.utils import file_utils
 
 
 class LogExtractorFileReader:
@@ -51,6 +55,64 @@ class LogExtractorFileReader:
     raise StopIteration
 
 
+class LogExtractorStateMachine:
+  """A state machine which performs file I/O given different state."""
+
+  def __init__(self):
+    """Initializes the test_run_id to file descriptor map.
+
+    Since tests might run in parallel, we need to store all running tests.
+    """
+    self._f_map = {}
+
+  def BeginTestRun(self, test_run_id: str, output_path: str):
+    """Opens a file of path `output_path` and records its id `test_run_id`."""
+    if test_run_id in self._f_map:
+      logging.warning('Test %s has already started.', test_run_id)
+      return
+
+    fd = open(output_path, 'w', encoding='utf-8')  # pylint: disable=consider-using-with
+    self._f_map[test_run_id] = fd
+    logging.info('Test %s starts. Output path: %s', test_run_id, output_path)
+
+  def EndTestRun(self, test_run_id: str):
+    """Closes the file descriptor of id `test_run_id`."""
+    if test_run_id not in self._f_map:
+      logging.warning('Test %s ends without starting.', test_run_id)
+      return
+
+    logging.info('Test %s ends.', test_run_id)
+    self._f_map[test_run_id].close()
+    self._f_map.pop(test_run_id)
+
+  def WriteRecord(self, test_run_id: Optional[str], record: IRecord):
+    """Write a record to the current running test(s).
+
+    Args:
+      test_run_id: The test run id that generates the record. If set to None,
+        write to all running tests.
+      record: a record of type IRecord.
+    """
+    if test_run_id in self._f_map:
+      self._f_map[test_run_id].write(str(record) + '\n')
+    else:
+      # Cannot determine which test run the logs belongs to.
+      # Write to all running tests.
+      for f in self._f_map.values():
+        f.write(str(record) + '\n')
+
+  def IsTestRunning(self) -> bool:
+    """Check if there's any test running."""
+    return bool(self._f_map)
+
+
+def GetExtractedLogOutputPath(test_name: str, test_run_id: str, root: str,
+                              fname: str) -> str:
+  test_run_name = f'{test_name}-{test_run_id}'
+  test_run_dir = os.path.join(root, test_run_name)
+  file_utils.TryMakeDirs(test_run_dir)
+  return os.path.join(test_run_dir, fname)
+
 def ExtractAndWriteRecordByTestRun(reader, output_dir: str, output_fname: str):
   """Extracts records based on the test run event.
 
@@ -64,8 +126,27 @@ def ExtractAndWriteRecordByTestRun(reader, output_dir: str, output_fname: str):
     output_dir: The output directory.
     output_fname: The output filename.
   """
-  raise NotImplementedError
+  state_machine = LogExtractorStateMachine()
+  for record in reader:
+    status = test_run_handler.ParseStatus(record)
+    test_name, test_run_id = test_run_handler.TestRunNameHandler().Parse(record)
 
+    if status in (TestRunStatus.STARTED, TestRunStatus.COMPLETED):
+      if not test_name or not test_run_id:
+        logging.warning('Record %s does not contain test run name info.',
+                        str(record))
+        continue
+
+    if status == TestRunStatus.STARTED:
+      output_path = GetExtractedLogOutputPath(test_name, test_run_id,
+                                              output_dir, output_fname)
+      state_machine.BeginTestRun(test_run_id, output_path)
+
+    if state_machine.IsTestRunning():
+      state_machine.WriteRecord(test_run_id, record)
+
+    if status == TestRunStatus.COMPLETED:
+      state_machine.EndTestRun(test_run_id)
 
 def GetTestRunStartEndTime(reader) -> Dict[str, Tuple]:
   """Gets the start and end time of all the test run.
