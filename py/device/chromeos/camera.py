6f7affff71212e51b01560c59c45bfb5590b4ea1
Chih-Yao Chuang <jasonchuang@google.com>
2023-04-11 06:07:44 +0000

    Revert "camera: add cros_config as default method to search for device index"
    This reverts commit c99e7e2419d4e6b97deb49f8f2f0365433ffe29d.
    
    Reason for revert: Camera team wants to deprecate ids in cros_config,
    so the GetCameraIndexFromCrosConfig won't work in the future.
    
    Original change's description:
    > camera: add cros_config as default method to search for device index
    >
    > Originally, the device index of the camera is obtained by checking
    > the camera_characteristics.conf. However, the camera team is going
    > to move the facing setting to cros_config. We thus add cros_config
    > as a default method to read the facing setting. We also keep the
    > checking of camera_characteristics.conf as a fall back approach.
    >
    > BUG=b:186497201
    > TEST=test manually on DUT
    >
    > Change-Id: I65c9561aeff653ec0323c9f56c52a89a0f6e0da1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/2899830
    > Tested-by: Ting-Hsuan Wang <phoebewang@chromium.org>
    > Reviewed-by: Cheng Yueh <cyueh@chromium.org>
    > Commit-Queue: Ting-Hsuan Wang <phoebewang@chromium.org>
    
    BUG=b:277697780
    TEST=make test
    
    Change-Id: I629ea6d5f8f096e84e411d525e18f829661860f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/4596437
    Reviewed-by: Phoebe Wang <phoebewang@chromium.org>
    Auto-Submit: Cheng Yueh <cyueh@chromium.org>
    Tested-by: Cheng Yueh <cyueh@chromium.org>
    Commit-Queue: Phoebe Wang <phoebewang@chromium.org>

--- py/device/chromeos/camera.py
+++ py/device/chromeos/camera.py
@@ -17,6 +17,7 @@ from cros.factory.test.utils.camera_utils import CameraError
 from cros.factory.test.utils.camera_utils import GetValidCameraPaths
 from cros.factory.utils import type_utils
 
+
 CAMERA_CONFIG_PATH = '/etc/camera/camera_characteristics.conf'
 ALLOWED_FACING = type_utils.Enum(['front', 'rear', None])
 
@@ -33,15 +34,14 @@ class ChromeOSCamera(camera.Camera):
   _index_mapping = {}
 
   def GetDeviceIndex(self, facing):
-    """Search the video device index.
-
-    Since the video device index may change after device reboot/suspend resume,
-    we search the video device index using cros_config or from the camera
-    characteristics file.
+    """Search the video device index from the camera characteristics file.
 
     Args:
       facing: Direction the camera faces relative to device screen. Only allow
               'front', 'rear' or None. None is automatically searching one.
+
+    Since the video device index may change after device reboot/suspend resume,
+    we search the video device index from the camera characteristics file.
     """
     if facing not in ALLOWED_FACING:
       raise CameraError('The facing (%s) is not in ALLOWED_FACING (%s)' %
@@ -65,16 +65,7 @@ class ChromeOSCamera(camera.Camera):
     if num_camera == 0:
       raise CameraError('No camera detected')
 
-    camera_facing = self._device.CallOutput(
-        ['cros_config', '/camera/devices/0', 'facing'])
-
-    # If camera_facing is empty, it means that the system does not
-    # support device query using cros_config.
-    if camera_facing:
-      self.GetCameraIndexFromCrosConfig(index_to_vid_pid, num_camera)
-    else:
-      self.GetCameraIndexFromCameraConfig(index_to_vid_pid)
-
+    self.GetCameraIndexFromCameraConfig(index_to_vid_pid)
     if facing is None:
       if len(self._index_mapping) > 1:
         raise CameraError('Multiple cameras are found')
@@ -86,53 +77,8 @@ class ChromeOSCamera(camera.Camera):
       raise CameraError('No %s camera is found' % facing)
     return self._index_mapping[facing]
 
-  def GetCameraIndexFromCrosConfig(self, index_to_vid_pid, num_camera):
-    """ Search the camera index using cros_config.
-
-    Args:
-      index_to_vid_pid: store the ids of the vendor and the product.
-    """
-    vid_pid_to_cros_index = {}
-    for cros_index in range(num_camera):
-      id_index = 0
-
-      while True:
-        vid_pid = self._device.CallOutput([
-            'cros_config',
-            '/camera/devices/%d/ids' % cros_index,
-            '%d' % id_index
-        ])
-
-        if not vid_pid:
-          break
-
-        if vid_pid in vid_pid_to_cros_index:
-          raise CameraError(
-              'Multiple cameras have the same usb_vid_pid (%s)'
-              ' There are duplicated usb_vid_pid in the'
-              ' cros_config file. Please submit a CL to fix this.' % vid_pid)
-
-        vid_pid_to_cros_index[vid_pid] = cros_index
-        id_index += 1
-
-    for index, vid_pid in index_to_vid_pid.items():
-      if vid_pid in vid_pid_to_cros_index:
-        camera_facing = self._device.CallOutput([
-            'cros_config',
-            '/camera/devices/%d' % vid_pid_to_cros_index[vid_pid], 'facing'
-        ])
-        camera_facing = {
-            'front': 'front',
-            'back': 'rear'
-        }[camera_facing]
-        self._index_mapping[camera_facing] = index
-      else:
-        raise CameraError(
-            'No camera has the usb_vid_pid (%s)'
-            ' Please submit a CL to update the cros_config file.' % vid_pid)
-
   def GetCameraIndexFromCameraConfig(self, index_to_vid_pid):
-    """Fallback function when unable to query from cros_config.
+    """Search the video device index from the camera characteristics file.
 
     Args:
       index_to_vid_pid: store the ids of the vendor and the product.
