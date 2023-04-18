2cd61f6abdd1b0883f000bac04d8a38d0b5ea621
Phoebe Wang <phoebewang@google.com>
2023-04-18 16:51:27 +0800

    file_utils: modify the extraction algorithm
    Before the extraction starts when reading start event and ends when
    reading complete event. For test that contains shutdown, we use shutdown
    handler to change the event sequence from:
    STARTING (pre-shutdown) -> FAIL (pre-shutdown) -> (DUT reboots)
    -> STARTING (post-shutdown) -> PASS/FAIL (post-shutdown)
    to
    STARTING (pre-shutdown) -> RUNNING (pre-shutdown) -> (DUT reboots)
    -> RUNNING (post-shutdown) -> PASS/FAIL (post-shutdown)
    However, this does not handle the case where the test involves
    unintentional reboots or multiple intentional reboots.
    To solve this, we preprocess the factory log and count the number of
    start event beforehand and store it in `start_event_cnt_map`. When
    reading complete event, we check if the current number of start event
    matches the one in `start_event_cnt_map`. If yes, then stops the
    extraction.
    
    BUG=b:269997449
    TEST=Run the following tests and check the corresponding logs
         (1) Run a test and check the test pass/failure log.
         (2) Run a test and trigger reboot by command line.
         (3) Run a parallel test and trigger reboot by command line.
         (4) Run a reboot test
    
    Change-Id: I3f082742927ada5c5a54b39d67cc8b5ecfe92f09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/4458812
    Reviewed-by: Wei-Shuo Su <stevesu@google.com>
    Commit-Queue: Phoebe Wang <phoebewang@chromium.org>
    Tested-by: Phoebe Wang <phoebewang@chromium.org>

--- py/log_extractor/file_utils_unittest.py
+++ py/log_extractor/file_utils_unittest.py
@@ -11,6 +11,7 @@ import unittest
 
 import cros.factory.log_extractor.file_utils as log_extractor_file_utils
 import cros.factory.log_extractor.record as record_module
+from cros.factory.log_extractor import test_run_handler
 from cros.factory.utils import file_utils
 
 
@@ -68,6 +69,40 @@ class LogExtractorFileReaderTest(unittest.TestCase):
       self.assertEqual(None, reader.GetCurRecord())
 
 
+class GetStartEventCntTest(unittest.TestCase):
+
+  def testGenerateUsingFactoryLog(self):
+    FACTORY_TEST_LOG = textwrap.dedent("""\
+      {"type": "station.message", "filePath": "testlog.py", "lineNumber": 230, "logLevel": "INFO", "time": 1673599400.133407, "message": "err message"}
+      {"startTime": 1673599400.633407, "status": "STARTING", "testName": "generic:Test_1", "testRunId": "abc", "time": 1673599400.682136, "type": "station.test_run", "testType": "mock"}
+      {"type": "station.message", "filePath": "testlog.py", "lineNumber": 230, "logLevel": "ERROR", "time": 1673599410.122136, "message": "info message"}
+      {"endTime": 1673599411.0909407, "status": "FAIL", "testName": "generic:Test_1", "testRunId": "abc", "time": 1673599400.682136, "type": "station.test_run", "testType": "mock"}
+      {"startTime": 1673599497.0769532, "status": "STARTING", "testName": "generic:Test_2", "testRunId": "def", "time": 1673599497.132981, "type": "station.test_run"}
+      {"endTime": 1673599498.1234567, "status": "FAIL", "testName": "generic:Test_2", "testRunId": "def", "time": 1673599497.132981, "type": "station.test_run"}
+      {"type": "station.message", "filePath": "testlog.py", "lineNumber": 230, "logLevel": "INFO", "time": 1673599500.5086327, "message": "device rebooting"}
+      {"startTime": 1673599499.4567321, "status": "STARTING", "testName": "generic:Test_2", "testRunId": "def", "time": 1673599497.132981, "type": "station.test_run"}
+      {"endTime": 1673599500.5086327, "status": "PASS", "testName": "generic:Test_2", "testRunId": "def", "time": 1673599497.132981, "type": "station.test_run"}
+      {"type": "station.message", "filePath": "testlog.py", "lineNumber": 230, "logLevel": "INFO", "time": 1673599500.6086327, "message": "info message 2"}
+      {"startTime": 1673599501.6086327, "status": "STARTING", "testName": "generic:Test_2", "testRunId": "def", "time": 1673599502.8086327, "type": "station.test_run"}
+      {"endTime": 1673599503.5078237, "status": "PASS", "testName": "generic:Test_2", "testRunId": "def", "time": 1673599502.8086327, "type": "station.test_run"}
+    """)
+    TEST_RUN_ID_1 = 'abc'
+    TEST_RUN_ID_2 = 'def'
+
+    OUTPUT_NAME = 'factory_log'
+    with file_utils.TempDirectory() as temp_dir:
+      # Setup environment
+      input_path = os.path.join(temp_dir, OUTPUT_NAME)
+      file_utils.WriteFile(input_path, FACTORY_TEST_LOG)
+
+      reader = log_extractor_file_utils.LogExtractorFileReader(
+          input_path, record_module.TestlogRecord.FromJSON, False)
+      start_event_cnt = log_extractor_file_utils.GetStartEventCnt(reader)
+      self.assertEqual(start_event_cnt[TEST_RUN_ID_1], 1)
+      self.assertEqual(start_event_cnt[TEST_RUN_ID_2], 3)
+
+
+
 class ExtractAndWriteRecordByTestRunTest(unittest.TestCase):
 
   def ReadExtractedLog(self, test_name, test_run_id, root, output_name):
@@ -75,9 +110,10 @@ class ExtractAndWriteRecordByTestRunTest(unittest.TestCase):
         log_extractor_file_utils.GetExtractedLogOutputPath(
             test_name, test_run_id, root, output_name))
 
-  # TODO(phoebewang): Add reboot logs in /var/log/message.
   def testWriteVarLogMessage(self):
     VAR_LOG_MESSAGE_JSON = textwrap.dedent("""\
+      {"filePath": "messages", "lineNumber": 3845, "logLevel": "INFO", "message": "goofy[2051]: Test generic:SMT.Start (asdfghhj-1234) starting", "time": 1675736098.9592}
+      {"filePath": "messages", "lineNumber": 3846, "logLevel": "INFO", "message": "goofy[2051]: Test generic:SMT.Start (asdfghhj-1234) completed: FAILED", "time": 1675736099.147357}
       {"filePath": "messages", "lineNumber": 3840, "logLevel": "ERROR", "message": "chapsd[1288]: InitStage2 failed because SRK is not ready", "time": 1675736100.571854}
       {"filePath": "messages", "lineNumber": 3841, "logLevel": "WARNING", "message": "cryptohomed[2172]: GetTpmTokenSlotForPath: Path not found.", "time": 1675736100.572054}
       {"filePath": "messages", "lineNumber": 3842, "logLevel": "INFO", "message": "goofy[2051]: Test generic:SMT.ECWPPinHigh (042a5e4f-3c08) starting", "time": 1675736100.663978}
@@ -88,23 +124,45 @@ class ExtractAndWriteRecordByTestRunTest(unittest.TestCase):
       {"filePath": "messages", "lineNumber": 3847, "logLevel": "INFO", "message": "goofy[2051]: Test generic:SMT.Barrier (042a5e69-cd9a) completed: FAILED", "time": 1675736102.347357}
       {"filePath": "messages", "lineNumber": 3848, "logLevel": "INFO", "message": "kernel: [    9.412065] x86/PAT: memtype_reserve failed", "time": 1675736102.305098}
     """)
-    TEST_NAME_1 = 'generic:SMT.ECWPPinHigh'
-    TEST_RUN_ID_1 = '042a5e4f-3c08'
-    TEST_RUN_EXPECTED_OUTPUT_1 = textwrap.dedent("""\
+    TEST_NAME_1 = 'generic:SMT.Start'
+    TEST_RUN_ID_1 = 'asdfghhj-1234'
+
+    TEST_NAME_2 = 'generic:SMT.ECWPPinHigh'
+    TEST_RUN_ID_2 = '042a5e4f-3c08'
+    TEST_RUN_EXPECTED_OUTPUT_2 = textwrap.dedent("""\
       [INFO] 2023-02-07T02:15:00.663978Z messages:3842 goofy[2051]: Test generic:SMT.ECWPPinHigh (042a5e4f-3c08) starting
       [WARNING] 2023-02-07T02:15:01.305057Z messages:3843 kernel: [    9.412059] x86/PAT: flashrom:4034 conflicting memory types
       [INFO] 2023-02-07T02:15:01.851173Z messages:3844 goofy[2051]: Test generic:SMT.ECWPPinHigh (042a5e4f-3c08) completed: PASSED
     """)
 
-    TEST_NAME_2 = 'generic:SMT.Barrier'
-    TEST_RUN_ID_2 = '042a5e69-cd9a'
-    TEST_RUN_EXPECTED_OUTPUT_2 = textwrap.dedent("""\
+    TEST_NAME_3 = 'generic:SMT.Barrier'
+    TEST_RUN_ID_3 = '042a5e69-cd9a'
+    TEST_RUN_EXPECTED_OUTPUT_3 = textwrap.dedent("""\
       [INFO] 2023-02-07T02:15:01.959200Z messages:3845 goofy[2051]: Test generic:SMT.Barrier (042a5e69-cd9a) starting
       [INFO] 2023-02-07T02:15:02.147357Z messages:3846 goofy[2051]: Test generic:SMT.Barrier (042a5e69-cd9a) resuming
       [INFO] 2023-02-07T02:15:02.347357Z messages:3847 goofy[2051]: Test generic:SMT.Barrier (042a5e69-cd9a) completed: FAILED
     """)
 
+    EXPECTED_TEST_RUN_INFO_LIST = [
+        log_extractor_file_utils.TestRunInfo(
+            TEST_NAME_2, TEST_RUN_ID_2, test_run_handler.TestRunStatus.STARTED,
+            1675736100.663978),
+        log_extractor_file_utils.TestRunInfo(
+            TEST_NAME_2, TEST_RUN_ID_2,
+            test_run_handler.TestRunStatus.COMPLETED, 1675736101.851173),
+        log_extractor_file_utils.TestRunInfo(
+            TEST_NAME_3, TEST_RUN_ID_3, test_run_handler.TestRunStatus.STARTED,
+            1675736101.9592),
+        log_extractor_file_utils.TestRunInfo(
+            TEST_NAME_3, TEST_RUN_ID_3,
+            test_run_handler.TestRunStatus.COMPLETED, 1675736102.347357),
+    ]
+
     OUTPUT_NAME = 'message'
+    START_EVENT_CNT_MAP = {
+        TEST_RUN_ID_2: 1,
+        TEST_RUN_ID_3: 2,
+    }
     with file_utils.TempDirectory() as temp_dir:
       # Setup environment
       input_path = os.path.join(temp_dir, OUTPUT_NAME)
@@ -112,17 +170,27 @@ class ExtractAndWriteRecordByTestRunTest(unittest.TestCase):
 
       reader = log_extractor_file_utils.LogExtractorFileReader(
           input_path, record_module.SystemLogRecord.FromJSON, False)
-      log_extractor_file_utils.ExtractAndWriteRecordByTestRun(
-          reader, temp_dir, OUTPUT_NAME)
-      self.assertEqual(
-          TEST_RUN_EXPECTED_OUTPUT_1,
-          self.ReadExtractedLog(TEST_NAME_1, TEST_RUN_ID_1, temp_dir,
-                                OUTPUT_NAME))
+      test_run_info_list = (
+          log_extractor_file_utils.ExtractAndWriteRecordByTestRun(
+              reader, temp_dir, OUTPUT_NAME, START_EVENT_CNT_MAP))
+
+      # Should not exist.
+      self.assertFalse(
+          os.path.exists(
+              log_extractor_file_utils.GetExtractedLogOutputPath(
+                  TEST_NAME_1, TEST_RUN_ID_1, temp_dir, OUTPUT_NAME)))
 
       self.assertEqual(
           TEST_RUN_EXPECTED_OUTPUT_2,
           self.ReadExtractedLog(TEST_NAME_2, TEST_RUN_ID_2, temp_dir,
                                 OUTPUT_NAME))
+
+      self.assertEqual(
+          TEST_RUN_EXPECTED_OUTPUT_3,
+          self.ReadExtractedLog(TEST_NAME_3, TEST_RUN_ID_3, temp_dir,
+                                OUTPUT_NAME))
+
+      self.assertEqual(EXPECTED_TEST_RUN_INFO_LIST, test_run_info_list)
 
   def testWriteTestlog(self):
     FACTORY_TEST_LOG = textwrap.dedent("""\
@@ -130,12 +198,14 @@ class ExtractAndWriteRecordByTestRunTest(unittest.TestCase):
       {"startTime": 1673599400.633407, "status": "STARTING", "testName": "generic:Test_1", "testRunId": "abc", "time": 1673599400.682136, "type": "station.test_run", "testType": "mock"}
       {"type": "station.message", "filePath": "testlog.py", "lineNumber": 230, "logLevel": "ERROR", "time": 1673599410.122136, "message": "info message"}
       {"endTime": 1673599411.0909407, "status": "FAIL", "testName": "generic:Test_1", "testRunId": "abc", "time": 1673599400.682136, "type": "station.test_run", "testType": "mock"}
-      {"startTime": 1673599497.0769532, "status": "STARTING", "testName": "generic:Test_2", "testRunId": "def", "time": 1673599497.132981, "type": "station.test_run", "testType": "shutdown", "parameters": {"tag": {"data": [{"textValue": "pre-shutdown"}]}}}
-      {"endTime": 1673599498.1234567, "status": "FAIL", "testName": "generic:Test_2", "testRunId": "def", "time": 1673599497.132981, "type": "station.test_run", "testType": "shutdown", "parameters": {"tag": {"data": [{"textValue": "pre-shutdown"}]}}}
+      {"startTime": 1673599497.0769532, "status": "STARTING", "testName": "generic:Test_2", "testRunId": "def", "time": 1673599497.132981, "type": "station.test_run", "testType": "mock"}
+      {"endTime": 1673599498.1234567, "status": "FAIL", "testName": "generic:Test_2", "testRunId": "def", "time": 1673599497.132981, "type": "station.test_run", "testType": "mock"}
       {"type": "station.message", "filePath": "testlog.py", "lineNumber": 230, "logLevel": "INFO", "time": 1673599500.5086327, "message": "device rebooting"}
-      {"startTime": 1673599499.4567321, "status": "STARTING", "testName": "generic:Test_2", "testRunId": "def", "time": 1673599497.132981, "type": "station.test_run", "testType": "shutdown", "parameters": {"tag": {"data": [{"textValue": "post-shutdown"}]}}}
-      {"endTime": 1673599500.5086327, "status": "PASS", "testName": "generic:Test_2", "testRunId": "def", "time": 1673599497.132981, "type": "station.test_run", "testType": "shutdown", "parameters": {"tag": {"data": [{"textValue": "post-shutdown"}]}}}
-      {"type": "station.message", "filePath": "testlog.py", "lineNumber": 230, "logLevel": "INFO", "time": 1673599500.5086327, "message": "info message"}
+      {"startTime": 1673599499.4567321, "status": "STARTING", "testName": "generic:Test_2", "testRunId": "def", "time": 1673599497.132981, "type": "station.test_run", "testType": "mock"}
+      {"endTime": 1673599500.5086327, "status": "PASS", "testName": "generic:Test_2", "testRunId": "def", "time": 1673599497.132981, "type": "station.test_run", "testType": "mock"}
+      {"type": "station.message", "filePath": "testlog.py", "lineNumber": 230, "logLevel": "INFO", "time": 1673599500.6086327, "message": "info message 2"}
+      {"startTime": 1673599501.6086327, "status": "STARTING", "testName": "generic:Test_2", "testRunId": "def", "time": 1673599502.8086327, "type": "station.test_run", "testType": "mock"}
+      {"endTime": 1673599503.5078237, "status": "PASS", "testName": "generic:Test_2", "testRunId": "def", "time": 1673599502.8086327, "type": "station.test_run", "testType": "mock"}
     """)
     TEST_NAME_1 = 'generic:Test_1'
     TEST_RUN_ID_1 = 'abc'
@@ -148,14 +218,37 @@ class ExtractAndWriteRecordByTestRunTest(unittest.TestCase):
     TEST_NAME_2 = 'generic:Test_2'
     TEST_RUN_ID_2 = 'def'
     TEST_RUN_EXPECTED_OUTPUT_2 = textwrap.dedent("""\
-      [INFO] 2023-01-13T08:44:57.076953Z generic:Test_2-def STARTING (pre-shutdown)
-      [INFO] 2023-01-13T08:44:58.123457Z generic:Test_2-def FAIL (pre-shutdown)
+      [INFO] 2023-01-13T08:44:57.076953Z generic:Test_2-def STARTING
+      [INFO] 2023-01-13T08:44:58.123457Z generic:Test_2-def FAIL
       [INFO] 2023-01-13T08:45:00.508633Z testlog.py:230 device rebooting
-      [INFO] 2023-01-13T08:44:59.456732Z generic:Test_2-def STARTING (post-shutdown)
-      [INFO] 2023-01-13T08:45:00.508633Z generic:Test_2-def PASS (post-shutdown)
+      [INFO] 2023-01-13T08:44:59.456732Z generic:Test_2-def STARTING
+      [INFO] 2023-01-13T08:45:00.508633Z generic:Test_2-def PASS
+      [INFO] 2023-01-13T08:45:00.608633Z testlog.py:230 info message 2
+      [INFO] 2023-01-13T08:45:01.608633Z generic:Test_2-def STARTING
+      [INFO] 2023-01-13T08:45:03.507824Z generic:Test_2-def PASS
     """)
 
     OUTPUT_NAME = 'testlog'
+    START_EVENT_CNT_MAP = {
+        TEST_RUN_ID_1: 1,
+        TEST_RUN_ID_2: 3,
+    }
+
+    EXPECTED_TEST_RUN_INFO_LIST = [
+        log_extractor_file_utils.TestRunInfo(
+            TEST_NAME_1, TEST_RUN_ID_1, test_run_handler.TestRunStatus.STARTED,
+            1673599400.633407),
+        log_extractor_file_utils.TestRunInfo(
+            TEST_NAME_1, TEST_RUN_ID_1,
+            test_run_handler.TestRunStatus.COMPLETED, 1673599411.0909407),
+        log_extractor_file_utils.TestRunInfo(
+            TEST_NAME_2, TEST_RUN_ID_2, test_run_handler.TestRunStatus.STARTED,
+            1673599497.0769532),
+        log_extractor_file_utils.TestRunInfo(
+            TEST_NAME_2, TEST_RUN_ID_2,
+            test_run_handler.TestRunStatus.COMPLETED, 1673599503.5078237),
+    ]
+
     with file_utils.TempDirectory() as temp_dir:
       # Setup environment
       input_path = os.path.join(temp_dir, OUTPUT_NAME)
@@ -163,8 +256,10 @@ class ExtractAndWriteRecordByTestRunTest(unittest.TestCase):
 
       reader = log_extractor_file_utils.LogExtractorFileReader(
           input_path, record_module.TestlogRecord.FromJSON, False)
-      log_extractor_file_utils.ExtractAndWriteRecordByTestRun(
-          reader, temp_dir, OUTPUT_NAME)
+      test_run_info_list = (
+          log_extractor_file_utils.ExtractAndWriteRecordByTestRun(
+              reader, temp_dir, OUTPUT_NAME, START_EVENT_CNT_MAP))
+
       self.assertEqual(
           TEST_RUN_EXPECTED_OUTPUT_1,
           self.ReadExtractedLog(TEST_NAME_1, TEST_RUN_ID_1, temp_dir,
@@ -174,6 +269,8 @@ class ExtractAndWriteRecordByTestRunTest(unittest.TestCase):
           TEST_RUN_EXPECTED_OUTPUT_2,
           self.ReadExtractedLog(TEST_NAME_2, TEST_RUN_ID_2, temp_dir,
                                 OUTPUT_NAME))
+
+      self.assertEqual(test_run_info_list, EXPECTED_TEST_RUN_INFO_LIST)
 
   def testWriteTestlogParallelTest(self):
     PARALLEL_TEST_LOG = textwrap.dedent("""\
@@ -209,6 +306,26 @@ class ExtractAndWriteRecordByTestRunTest(unittest.TestCase):
     """)
 
     OUTPUT_NAME = 'parallel_testlog'
+    START_EVENT_CNT_MAP = {
+        TEST_RUN_ID_1: 1,
+        TEST_RUN_ID_2: 1,
+    }
+
+    EXPECTED_TEST_RUN_INFO_LIST = [
+        log_extractor_file_utils.TestRunInfo(
+            TEST_NAME_1, TEST_RUN_ID_1, test_run_handler.TestRunStatus.STARTED,
+            1673599400.633407),
+        log_extractor_file_utils.TestRunInfo(
+            TEST_NAME_2, TEST_RUN_ID_2, test_run_handler.TestRunStatus.STARTED,
+            1673599497.0769532),
+        log_extractor_file_utils.TestRunInfo(
+            TEST_NAME_2, TEST_RUN_ID_2,
+            test_run_handler.TestRunStatus.COMPLETED, 1673599601.1234567),
+        log_extractor_file_utils.TestRunInfo(
+            TEST_NAME_1, TEST_RUN_ID_1,
+            test_run_handler.TestRunStatus.COMPLETED, 1673599610.6086327),
+    ]
+
     with file_utils.TempDirectory() as temp_dir:
       # Setup environment
       input_path = os.path.join(temp_dir, OUTPUT_NAME)
@@ -216,8 +333,9 @@ class ExtractAndWriteRecordByTestRunTest(unittest.TestCase):
 
       reader = log_extractor_file_utils.LogExtractorFileReader(
           input_path, record_module.TestlogRecord.FromJSON, False)
-      log_extractor_file_utils.ExtractAndWriteRecordByTestRun(
-          reader, temp_dir, OUTPUT_NAME)
+      test_run_info_list = (
+          log_extractor_file_utils.ExtractAndWriteRecordByTestRun(
+              reader, temp_dir, OUTPUT_NAME, START_EVENT_CNT_MAP))
       self.assertEqual(
           TEST_RUN_EXPECTED_OUTPUT_1,
           self.ReadExtractedLog(TEST_NAME_1, TEST_RUN_ID_1, temp_dir,
@@ -227,6 +345,8 @@ class ExtractAndWriteRecordByTestRunTest(unittest.TestCase):
           TEST_RUN_EXPECTED_OUTPUT_2,
           self.ReadExtractedLog(TEST_NAME_2, TEST_RUN_ID_2, temp_dir,
                                 OUTPUT_NAME))
+
+      self.assertEqual(EXPECTED_TEST_RUN_INFO_LIST, test_run_info_list)
 
 
 if __name__ == '__main__':
