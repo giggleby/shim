619d7cf80649374f70ebff1d364c1adf213ec5cb
stevesu <stevesu@google.com>
2023-04-24 22:00:31 +0800

    probe: Add probe for feature management flags
    Feature management flags are part of the HWID by design so we need a
    probe function for it. The probe is designed in a two-stage fashion.
    If RLZ code is set on given DUT, the probe function will get the value
    stored in GSC, however if not, it will return values in regard to the
    values stored in device data, because we need to create temporary HWID
    for pytest defined in b/274033418.
    
    BUG=b:274033431
    TEST=make test
    run probe eval-function feature_management_flags on DUT
    
    Change-Id: I4567500d7679819c0022c31600dfddd577b19007
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/4466728
    Reviewed-by: Stimim Chen <stimim@chromium.org>
    Tested-by: Wei-Shuo Su <stevesu@google.com>
    Commit-Queue: Wei-Shuo Su <stevesu@google.com>

---
+++ py/probe/functions/feature_management_flags.py
@@ -0,0 +1,63 @@
+# Copyright 2023 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from cros.factory.external.chromeos_cli import gsctool as gsctool_module
+from cros.factory.probe.lib import probe_function
+from cros.factory.test import device_data
+
+
+class FeatureManagementFlagsFunction(probe_function.ProbeFunction):
+  """Probes the feature management flags."""
+
+  def __init__(self, **kwargs):
+    super().__init__(**kwargs)
+    self._gsctool = gsctool_module.GSCTool()
+
+  # TODO(stevesu): The current formal way of checking BoardID is to leverage
+  # `Gooftool.Core.IsCr50BoardIDSet()` however this complicates the whole
+  # issue as when probing we shouldn't care about the correctness of
+  # the RLZ code and we probably should not make the probe depends on the
+  # Gooftool as well. Requires a refactor to put below `IsBoardIDSet` function
+  # into gsctool and modify `Gooftool.Core.IsCr50BoardIDSet()` accordingly.
+  # Should be part of the work related with b/279136124.
+  def IsBoardIDSet(self):
+    try:
+      board_id = self._gsctool.GetBoardID()
+    except gsctool_module.GSCToolError as e:
+      raise RuntimeError(
+          f'Failed to get boardID with gsctool command: {e!r}') from None
+    if board_id.type == 0xffffffff:
+      return False
+    return True
+
+  def Probe(self):
+
+    chassis_branded = False
+    hw_compliance_version = 0
+
+    # In factory, in cases where board ID already set, treat the feature
+    # flags collected from GSC vendor command as source of truth.
+    # Otherwise, if either device data related with feature flags is None,
+    # return a valid default value pair (False, 0) for HWID to work.
+    if self.IsBoardIDSet():
+      feature_flags_gsc = self._gsctool.GetFeatureManagementFlags()
+      chassis_branded = feature_flags_gsc.is_chassis_branded
+      hw_compliance_version = feature_flags_gsc.hw_compliance_version
+    else:
+      chassis_branded_device_data = device_data.GetDeviceData(
+          device_data.KEY_FM_CHASSIS_BRANDED)
+      hw_compliance_version_device_data = device_data.GetDeviceData(
+          device_data.KEY_FM_HW_COMPLIANCE_VERSION)
+
+      if (chassis_branded_device_data is not None and
+          hw_compliance_version_device_data is not None):
+        chassis_branded = chassis_branded_device_data
+        hw_compliance_version = hw_compliance_version_device_data
+
+    results = [{
+        'is_chassis_branded': str(int(chassis_branded)),
+        'hw_compliance_version': str(hw_compliance_version)
+    }]
+
+    return results
