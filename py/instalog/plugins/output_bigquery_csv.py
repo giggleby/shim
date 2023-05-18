660642dd50815e6c11512b9d08a5c6551958fa98
chuntsen <chuntsen@chromium.org>
2023-05-18 06:52:52 +0000

    Instalog: Add CSV reader plugin and its BigQuery plugin
    To collect serial numbers and HWIDs on factory line for auditing, we add
    two output plugins.
    Add a plugin to read CSV files from Google Cloud Storage.
    Add a plugin to upload CSV events to BigQuery.
    
    BUG=b:283197840
    TEST=Manually test
    
    Change-Id: I552c1da0533c87cb49fae8e70ecee5e1a3735da0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/4546889
    Commit-Queue: Chun-Tsen Kuo <chuntsen@chromium.org>
    Reviewed-by: Sung-Chi Li <lschyi@chromium.org>
    Tested-by: Chun-Tsen Kuo <chuntsen@chromium.org>

---
+++ py/instalog/plugins/output_bigquery_csv.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+#
+# Copyright 2023 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""BigQuery upload output plugin.
+
+Subclasses OutputBigQuery to create table rows for CSV events.
+"""
+
+import json
+
+from google.cloud.bigquery.schema import SchemaField
+
+from cros.factory.instalog import plugin_base
+from cros.factory.instalog.plugins import output_bigquery
+
+
+class OutputBigQueryCSV(output_bigquery.OutputBigQuery):
+
+  def GetTableSchema(self):
+    """Returns a list of fields in the table schema."""
+    return [
+        SchemaField('serialNumber', 'string', 'REQUIRED', None, ()),
+        SchemaField('hwid', 'string', 'REQUIRED', None, ()),
+        SchemaField('time', 'timestamp', 'REQUIRED', None, ()),
+    ]
+
+  def ConvertEventToRow(self, event):
+    """Converts an event to its corresponding BigQuery table row JSON string."""
+    if not event.get('__csv__', False):
+      return None
+
+    row = {}
+
+    row['serialNumber'] = event.get('serialNumber')
+    row['hwid'] = event.get('hwid')
+    row['time'] = event.get('time')
+
+    return json.dumps(row, allow_nan=False)
+
+
+if __name__ == '__main__':
+  plugin_base.main()
---
+++ py/instalog/plugins/output_csv_reader.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+#
+# Copyright 2023 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Output CSV reader plugin.
+
+A plugin to parse CSV file to events.
+"""
+
+import csv
+import datetime
+import os
+
+from cros.factory.instalog import datatypes
+from cros.factory.instalog import plugin_base
+from cros.factory.utils.arg_utils import Arg
+from cros.factory.utils import gcs_utils
+from cros.factory.utils import time_utils
+
+
+_DEFAULT_INTERVAL = 60
+_DEFAULT_BATCH_SIZE = 100
+
+
+class OutputCSVReader(plugin_base.OutputPlugin):
+
+  ARGS = [
+      Arg('interval', (int, float),
+          'Frequency to call HWID decode service, in seconds.',
+          default=_DEFAULT_INTERVAL),
+      Arg('batch_size', int, 'How many HWID events to queue before decoding.',
+          default=_DEFAULT_BATCH_SIZE),
+      # TODO(chuntsen): Remove key_path argument since we don't use it anymore.
+      Arg(
+          'key_path', str,
+          'Path to Cloud Storage service account JSON key file.  If set to '
+          'None, the Google Cloud client will use the default service account '
+          'which is set to the environment variable '
+          'GOOGLE_APPLICATION_CREDENTIALS or Google Cloud services.',
+          default=None),
+      Arg(
+          'impersonated_account', str,
+          'A service account to impersonate.  The default credential should '
+          'have the permission to impersonate the service account.  '
+          '(roles/iam.serviceAccountTokenCreator)', default=None),
+  ]
+
+  def __init__(self, *args, **kwargs):
+    self.gcs = None
+    super().__init__(*args, **kwargs)
+
+  def SetUp(self):
+    self.gcs = gcs_utils.CloudStorage(
+        json_key_path=self.args.key_path, logger=self.logger,
+        impersonated_account=self.args.impersonated_account)
+
+  def Main(self):
+    """Main thread of the plugin."""
+    while not self.IsStopping():
+      if not self.DownloadAndProcessCSV():
+        self.Sleep(1)
+
+  def DownloadAndProcessCSV(self):
+    """Downloads CSV files from Storage and processes them."""
+    event_stream = self.NewStream()
+    if not event_stream:
+      return False
+
+    csv_events = []
+    for event in event_stream.iter(count=self.args.batch_size):
+      object_id = event.get('objectId', '')
+      if not object_id.endswith('.csv'):
+        continue
+      csv_path = os.path.join(self.GetDataDir(), 'temp.csv')
+      try:
+        self.gcs.DownloadFile(object_id, csv_path, overwrite=True)
+        csv_events.extend(self.ReadCSV(csv_path))
+      finally:
+        if os.path.exists(csv_path):
+          os.unlink(csv_path)
+
+    if self.Emit(csv_events):
+      self.info('Commit %d events', len(csv_events))
+      event_stream.Commit()
+      return True
+    event_stream.Abort()
+    return False
+
+  def ReadCSV(self, csv_path):
+    """Reads CSV files and converts to events."""
+    csv_events = []
+    with open(csv_path, 'r', encoding='utf-8') as f:
+      reader = csv.reader(f)
+      for row in reader:
+        if len(row) != 3:
+          return []
+        if row == ['serial_number', 'hwid', 'timestamp']:
+          continue
+        try:
+          timestamp = datetime.datetime.fromisoformat(row[2])
+        except ValueError:
+          return []
+        unixtime = time_utils.DatetimeToUnixtime(timestamp)
+        event = datatypes.Event({
+            '__csv__': True,
+            'serialNumber': row[0],
+            'hwid': row[1],
+            'time': unixtime
+        })
+        csv_events.append(event)
+    return csv_events
+
+
+if __name__ == '__main__':
+  plugin_base.main()
