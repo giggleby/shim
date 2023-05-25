d7ef34a51831647a4f7fbea3ea1b6ebe87c6bfa3
Phoebe Wang <phoebewang@google.com>
2023-05-25 16:54:48 +0800

    clear_inactive_gsc_slot: clears the inactive GSC slot
    This CL adds a pytest to clear the inactive GSC RW slot to prevent
    unexpected version rollback.
    
    BUG=b:269280908
    TEST=run the test on DUT & check non-active RW is empty in ti50 console
    
    Change-Id: Idbcd752d5c09ceadf5413b69fb91d0583a1ba2ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/4567904
    Tested-by: Phoebe Wang <phoebewang@chromium.org>
    Reviewed-by: Chih-Yao Chuang <jasonchuang@google.com>
    Commit-Queue: Phoebe Wang <phoebewang@chromium.org>
    (cherry picked from commit 544d9606b13becfbf78583f2cde4d7011ab2fe46)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/4624069
    Commit-Queue: LIU KUAN-FU <iamjeffliu@google.com>
    Reviewed-by: LIU KUAN-FU <iamjeffliu@google.com>
    Auto-Submit: Yu-An Wang <wyuang@google.com>
    Tested-by: Yu-An Wang <wyuang@google.com>

---
+++ py/test/pytests/clear_inactive_gsc_slot.py
@@ -0,0 +1,45 @@
+# Copyright 2023 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Clears the inactive GSC RW slot.
+
+Description
+-----------
+Clears the inactive GSC RW slot to prevent unexpected version rollback.
+
+Test Procedure
+--------------
+The test clears the inactive GSC slot by running `gsctool -a -c`. No user
+interaction is required.
+
+Dependency
+----------
+gsctool
+
+Examples
+--------
+To run the test, do::
+
+  {
+    "pytest_name": "clear_inactive_gsc_slot"
+  }
+
+"""
+
+import logging
+
+from cros.factory.device import device_utils
+from cros.factory.test import test_case
+
+from cros.factory.external.chromeos_cli import gsctool
+
+
+class ClearInactiveGscSlot(test_case.TestCase):
+
+  def setUp(self):
+    self.dut = device_utils.CreateDUTInterface()
+    self.gsctool = gsctool.GSCTool(dut=self.dut)
+
+  def runTest(self):
+    logging.info('Clearing inactive GSC slot ...')
+    self.gsctool.ClearInactiveGSCSlot()
