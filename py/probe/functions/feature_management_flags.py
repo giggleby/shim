bfc08f9e14511daebee60a8ccead5cff99fc10b8
stevesu <stevesu@google.com>
2023-06-12 14:51:06 +0800

    gsctool: Fix feature management flags lock status for Ti50
    APRO verification pytest has a step to provision board ID into
    Ti50 before normal `Cr50WriteFlashInfo`. This fails feature
    management flag provision as in the original implementation
    we're checking whether the board ID already set or not and
    this is a Cr50-only behavior. According to b/275356839#comment2,
    Ti50 is using `FactoryMode::is_enabled()` to check if we can
    write feature flags into GSC. This CL refactors the original
    implementation so that it can correctly provision feature flags
    for both Cr50/Ti50.
    
    BUG=b:286566945
    TEST=make test
    On Cr50 device with board ID already set, set feature device data to
    (True, 1) and run `probe eval-function feature_management_flags` and verify it to be (False, 0). For Ti50, also set feature device data to (True, 1) and toggle between factory modes. For factory mode enabled, we should get (True, 1) and when it's disabled we should get (False, 0).
    
    For end-to-end integration testing, it has to be done in factory
    with unprovisioned GSC chips.
    
    Change-Id: Ib8f7ee84f9b77dbcc7d4e45930b903fcc7547f61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/4604137
    Reviewed-by: Cheng Yueh <cyueh@chromium.org>
    Commit-Queue: Wei-Shuo Su <stevesu@google.com>
    Tested-by: Wei-Shuo Su <stevesu@google.com>

--- py/probe/functions/feature_management_flags.py
+++ py/probe/functions/feature_management_flags.py
@@ -14,33 +14,16 @@ class FeatureManagementFlagsFunction(probe_function.ProbeFunction):
     super().__init__(**kwargs)
     self._gsctool = gsctool_module.GSCTool()
 
-  # TODO(stevesu): The current formal way of checking BoardID is to leverage
-  # `Gooftool.Core.IsCr50BoardIDSet()` however this complicates the whole
-  # issue as when probing we shouldn't care about the correctness of
-  # the RLZ code and we probably should not make the probe depends on the
-  # Gooftool as well. Requires a refactor to put below `IsBoardIDSet` function
-  # into gsctool and modify `Gooftool.Core.IsCr50BoardIDSet()` accordingly.
-  # Should be part of the work related with b/279136124.
-  def IsBoardIDSet(self):
-    try:
-      board_id = self._gsctool.GetBoardID()
-    except gsctool_module.GSCToolError as e:
-      raise RuntimeError(
-          f'Failed to get boardID with gsctool command: {e!r}') from None
-    if board_id.type == 0xffffffff:
-      return False
-    return True
-
   def Probe(self):
 
     chassis_branded = False
     hw_compliance_version = 0
 
-    # In factory, in cases where board ID already set, treat the feature
-    # flags collected from GSC vendor command as source of truth.
+    # In factory, in cases where we cannot set feature management flags,
+    # we view flags collected from GSC vendor command as source of truth.
     # Otherwise, if either device data related with feature flags is None,
     # return a valid default value pair (False, 0) for HWID to work.
-    if self.IsBoardIDSet():
+    if self._gsctool.IsGSCFeatureManagementFlagsLocked():
       feature_flags_gsc = self._gsctool.GetFeatureManagementFlags()
       chassis_branded = feature_flags_gsc.is_chassis_branded
       hw_compliance_version = feature_flags_gsc.hw_compliance_version
