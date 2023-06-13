bc761f4c2618366309031652ae39e64b3579c700
Cheng Yueh <cyueh@chromium.org>
2023-06-13 14:10:17 +0800

    closure: Add allow list for warning
    There is no easy way to hide one type of warning directly in the closure
    compiler command so we parse it ourselves.
    
    Allow list is a good tool for build tool migration.
    
    BUG=b:286031735
    TEST=(1) `bin/factory_env py/goofy/js/goofy_js_unittest.py` passes
         (2)  sed -i 's/boolean /bool /g' py/goofy/js/goofy.js
         (3) `bin/factory_env py/goofy/js/goofy_js_unittest.py` fails
         (4) Add `re.compile(r'.*Bad type annotation.*')` in allow list
         (5) `bin/factory_env py/goofy/js/goofy_js_unittest.py` passes
         (6) To recover, `git checkout HEAD py/goofy/js/goofy.js`
    
    Change-Id: I8401f9806fc73125ed5b936621fe3ca06b1b7f0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/4604345
    Reviewed-by: Chih-Yao Chuang <jasonchuang@google.com>
    Commit-Queue: Cheng Yueh <cyueh@chromium.org>
    Tested-by: Cheng Yueh <cyueh@chromium.org>

--- py/goofy/js/goofy_js_unittest.py
+++ py/goofy/js/goofy_js_unittest.py
@@ -8,27 +8,98 @@
 """The unittest that use closure compiler to catch common error in goofy.js."""
 
 import os
+import re
 import subprocess
+from typing import Any, Dict, List
 import unittest
 
 from cros.factory.unittest_utils import label_utils
-from cros.factory.utils import process_utils
 
 
 SCRIPT_DIR = os.path.dirname(__file__)
+# The type of DESCRIPTION_ALLOW_LIST is Tuple[re.Pattern]. It's defined after
+# python3.8.
+DESCRIPTION_ALLOW_LIST = ()
+_closure_error_pattern = re.compile(
+    r'(?P<source>[^:]*):(?:(?P<line>.*):)?(?:(?P<column>.*):)? '
+    r'(?P<level>.*) - (?P<description>.*)')
+
+
+def FormatWarning(warning: Dict[str, Any]):
+  output_list = []
+
+  source = warning['source']
+  output_list.append(f'{source}:')
+
+  line = warning['line']
+  if line is not None:
+    output_list.append(f'{line}:')
+
+  column = warning['column']
+  if column is not None:
+    output_list.append(f'{column}:')
+
+  level = warning['level']
+  description = warning['description']
+  output_list.append(f' {level} - {description}\n')
+  return ''.join(output_list)
+
+
+def FormatWarnings(warnings):
+  return ''.join(FormatWarning(warning) for warning in warnings)
+
+
+def ParseClosureWarnings(stderr: str) -> List[Dict[str, Any]]:
+  """Parses the stderr from closure-compiler.
+
+  Output examples:
+    1. `goofy.js:1: WARNING - Bad type annotation. Unknown type bool`
+
+  Args:
+    stderr: The stderr from closure-compiler.
+
+  Returns:
+    A list of dictionary of the component of warnings.
+  """
+  warnings = []
+  for line in stderr.splitlines():
+    match = _closure_error_pattern.fullmatch(line)
+    if match:
+      warnings.append(match.groupdict())
+  return warnings
 
 
 # TODO (b/204838120)
 @label_utils.Informational
 class GoofyJSTest(unittest.TestCase):
+
   def runTest(self):
     static_dir = os.path.join(SCRIPT_DIR, '..', 'static')
-    output = process_utils.CheckOutput(
-        ['make', '-C', static_dir, 'check_js'], stderr=subprocess.STDOUT)
-    self.assertNotIn(
-        ' WARNING ', output,
-        "There's warning in closure compiler output, please fix them.\noutput:"
-        f"\n{output}")
+    result = subprocess.run(['make', '-C', static_dir, 'check_js'],
+                            stderr=subprocess.PIPE, encoding='utf8',
+                            check=False)
+    warnings = ParseClosureWarnings(result.stderr)
+
+    filtered_warnings = []
+    critical_warnings = []
+    for warning in warnings:
+      description = warning.get('description')
+      if (description and any(
+          pattern.fullmatch(description)
+          for pattern in DESCRIPTION_ALLOW_LIST)):
+        filtered_warnings.append(warning)
+        continue
+
+      critical_warnings.append(warning)
+
+    if filtered_warnings:
+      print('filtered warnings:')
+      print(FormatWarnings(filtered_warnings))
+
+    if critical_warnings or result.returncode != 0:
+      self.fail("There's warning in closure compiler output, please fix them.\n"
+                f'return_code:{result.returncode}\n'
+                f'critical warnings:\n{FormatWarnings(critical_warnings)}')
 
 
 if __name__ == '__main__':
