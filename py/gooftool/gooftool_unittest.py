dadff4c17279405e9eaf2a263db2e3cf099f4706
Phoebe Wang <phoebewang@google.com>
2023-07-12 14:01:24 +0800

    command: Add a new gooftool command `clear_unknown_vpd_entries`
    This CL
    (1) adds a new gooftool command `clear_unknown_vpd_entries`.
    (2) moves part of the code from `CheckVPDFields` to a new function
    called `GetInvalidVPDFields` so that `ClearUnknownVPDEntries` can reuse
    it.
    (3) collects all errors in `CheckVPDFields` and throws it at once as
    VPDError.
    (4) removes function Matchwhole and replaces it with re.fullmatch
    (5) replaces the error classes in `VerifyVPD` with `VPDError`
    
    BUG=b:286342384
    TEST=make test & run the command on DUT
    
    Change-Id: Ice8e985ee9a359d87a9febebe017d0baa1d88407
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/4678704
    Commit-Queue: Phoebe Wang <phoebewang@chromium.org>
    Reviewed-by: Wei-Shuo Su <stevesu@google.com>
    Tested-by: Phoebe Wang <phoebewang@chromium.org>

--- py/gooftool/gooftool_unittest.py
+++ py/gooftool/gooftool_unittest.py
@@ -733,7 +733,8 @@ class GooftoolTest(unittest.TestCase):
     # the error messages.
     dsm_string_regex = 'dsm_calib_(?:temp|r0)_[0-1]'
     self.assertRaisesRegex(
-        Error, f'Missing required RO VPD values: (?:{dsm_string_regex},){{3}}'
+        core.VPDError,
+        f'Missing required RO VPD values: (?:{dsm_string_regex},){{3}}'
         f'{dsm_string_regex}', self._gooftool.VerifyVPD)
 
   def testVerifyVPD_NoRegion(self):
@@ -742,7 +743,8 @@ class GooftoolTest(unittest.TestCase):
     self._SetupVPDMocks(ro=ro_vpd_value, rw=self._SIMPLE_VALID_RW_VPD_DATA)
 
     # Should fail, since region is missing.
-    self.assertRaisesRegex(Error, 'Missing required RO VPD values: region',
+    self.assertRaisesRegex(core.VPDError,
+                           'Missing required RO VPD values: region',
                            self._gooftool.VerifyVPD)
 
   def testVerifyVPD_InvalidRegion(self):
@@ -750,7 +752,7 @@ class GooftoolTest(unittest.TestCase):
     ro_vpd_value['region'] = 'nonexist'
     self._SetupVPDMocks(ro=ro_vpd_value, rw=self._SIMPLE_VALID_RW_VPD_DATA)
 
-    self.assertRaisesRegex(ValueError, 'Unknown region: "nonexist".',
+    self.assertRaisesRegex(core.VPDError, 'Unknown region: "nonexist".',
                            self._gooftool.VerifyVPD)
 
   def testVerifyVPD_InvalidMACKey(self):
@@ -758,7 +760,7 @@ class GooftoolTest(unittest.TestCase):
     ro_vpd_value['wifi_mac'] = '00:11:de:ad:be:ef'
     self._SetupVPDMocks(ro=ro_vpd_value, rw=self._SIMPLE_VALID_RW_VPD_DATA)
 
-    self.assertRaisesRegex(KeyError,
+    self.assertRaisesRegex(core.VPDError,
                            'Unexpected RO VPD: wifi_mac=00:11:de:ad:be:ef.',
                            self._gooftool.VerifyVPD)
 
@@ -769,7 +771,7 @@ class GooftoolTest(unittest.TestCase):
     rw_vpd_value['gbind_attribute'] = 'badvalue'
     self._SetupVPDMocks(ro=self._SIMPLE_VALID_RO_VPD_DATA, rw=rw_vpd_value)
 
-    self.assertRaisesRegex(ValueError, 'gbind_attribute is invalid:',
+    self.assertRaisesRegex(core.VPDError, 'gbind_attribute is invalid:',
                            self._gooftool.VerifyVPD)
 
   # TODO (b/212216855)
@@ -784,7 +786,7 @@ class GooftoolTest(unittest.TestCase):
         'zbTOX_9OQI_3EAAaCmNocm9tZWJvb2sQouDUgwQ=')
     self._SetupVPDMocks(ro=self._SIMPLE_VALID_RO_VPD_DATA, rw=rw_vpd_value)
 
-    self.assertRaisesRegex(ValueError, 'gbind_attribute is invalid: ',
+    self.assertRaisesRegex(core.VPDError, 'gbind_attribute is invalid: ',
                            self._gooftool.VerifyVPD)
 
   # TODO (b/212216855)
@@ -804,7 +806,8 @@ class GooftoolTest(unittest.TestCase):
     ro_vpd_value['initial_locale'] = 'en-US'
     self._SetupVPDMocks(ro=ro_vpd_value, rw=self._SIMPLE_VALID_RW_VPD_DATA)
 
-    self.assertRaisesRegex(KeyError, 'Unexpected RO VPD: initial_locale=en-US',
+    self.assertRaisesRegex(core.VPDError,
+                           'Unexpected RO VPD: initial_locale=en-US',
                            self._gooftool.VerifyVPD)
 
   def testVerifyReleaseChannel_CanaryChannel(self):
