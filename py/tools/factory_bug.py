bfdaabce3cd1177b52180bb89a43b4f4a12a2788
Phoebe Wang <phoebewang@google.com>
2023-08-04 17:03:40 +0800

    factory_bug: rename bios_log to ap_console
    The log is generated by coreboot and depthcharge and can be read from
    AP console. Renaming the file from bios_log to ap_console to better
    reflect its meaning.
    
    BUG=b:293978537
    TEST=Run `factory_bug`
    
    Change-Id: Ifae53bc8d22ad86ebed5560ffc1e94a9348c5ea0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/4755687
    Auto-Submit: Phoebe Wang <phoebewang@chromium.org>
    Tested-by: Phoebe Wang <phoebewang@chromium.org>
    Commit-Queue: Kerker Yang <kerker@chromium.org>
    Reviewed-by: Kerker Yang <kerker@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/4758769
    Commit-Queue: Cheng Yueh <cyueh@chromium.org>
    Reviewed-by: Cheng Yueh <cyueh@chromium.org>

--- py/tools/factory_bug.py
+++ py/tools/factory_bug.py
@@ -461,7 +461,7 @@ def SaveLogs(output_dir, archive_id=None, net=False, probe=False, dram=False,
           Run('audio_diagnostics', filename='audio_diagnostics',
               check_call=False, include_stderr=True),
           # Cannot zip an unseekable file, need to manually copy it instead.
-          Run(['cat', 'sys/firmware/log'], filename='bios_log',
+          Run(['cat', 'sys/firmware/log'], filename='ap_console',
               check_call=False),
           Run(['factory', 'tests', '--status'], filename='factory_test_status',
               check_call=False),
