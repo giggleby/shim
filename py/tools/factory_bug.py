35a37d8596dcc2faefe2848a7042cc009d445e5d
Phoebe Wang <phoebewang@google.com>
2023-08-04 16:38:21 +0800

    factory_bug: remove dmesg
    This CL removes dmesg from factory_bug since the content of dmesg should
    already be covered by /var/log/messages.
    
    BUG=b:293978537
    TEST=run `factory_bug` on DUT
    
    Change-Id: If449c16b6f9db7845b0d0efd3817fbf8b0a386bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/4755686
    Reviewed-by: Kerker Yang <kerker@chromium.org>
    Tested-by: Phoebe Wang <phoebewang@chromium.org>
    Auto-Submit: Phoebe Wang <phoebewang@chromium.org>
    Commit-Queue: Kerker Yang <kerker@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/4758768
    Commit-Queue: Cheng Yueh <cyueh@chromium.org>
    Reviewed-by: Cheng Yueh <cyueh@chromium.org>

--- py/tools/factory_bug.py
+++ py/tools/factory_bug.py
@@ -455,9 +455,8 @@ def SaveLogs(output_dir, archive_id=None, net=False, probe=False, dram=False,
     try:
       Run = RunCommandAndSaveOutputToFile
       files = [
-          Run('crossystem', filename='crossystem', include_stderr=True),
-          Run('dmesg', filename='dmesg'),
-          Run(['elogtool', 'list'], filename='firmware_eventlog',
+          Run(['crossystem'], filename='crossystem', include_stderr=True),
+          Run(['elogtool', 'list', '--utc'], filename='firmware_eventlog',
               check_call=False, include_stderr=True),
           Run('audio_diagnostics', filename='audio_diagnostics',
               check_call=False, include_stderr=True),
