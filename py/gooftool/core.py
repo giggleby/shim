9f9f3bafd78add6ff9e20bcc192a436b27055a4f
Chih-Yao Chuang <jasonchuang@google.com>
2023-08-11 15:01:05 +0800

    Refactor: Move vpd-validation-related function out from core.py
    Add vpd_utils.py for functions that verify/clear vpd.
    Move GetAudioVPDROData and GetDeviceNameForRegistrationCode and
    CheckVPDFields from VerifyVPD to improve the test coverage.
    
    BUG=b:292198852
    TEST=make test
    gooftool verify_vpd
    gooftool clear_factory_vpd_entries
    gooftool get_smart_amp_info
    
    Change-Id: I747e2ff9ef2bb7af2ed59a4337797feb3e5c4ab1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/4772957
    Auto-Submit: Chih-Yao Chuang <jasonchuang@google.com>
    Tested-by: Chih-Yao Chuang <jasonchuang@google.com>
    Reviewed-by: Phoebe Wang <phoebewang@chromium.org>
    Commit-Queue: Chih-Yao Chuang <jasonchuang@google.com>

--- py/gooftool/core.py
+++ py/gooftool/core.py
@@ -23,20 +23,18 @@ from cros.factory.gooftool.common import Util
 from cros.factory.gooftool import gbb
 from cros.factory.gooftool import management_engine
 from cros.factory.gooftool import vpd_data
+from cros.factory.gooftool import vpd_utils
 from cros.factory.gooftool import wipe
 from cros.factory.hwid.v3.database import Database
 from cros.factory.hwid.v3 import hwid_utils
 from cros.factory.test.l10n import regions
 from cros.factory.test.rules import phase
-from cros.factory.test.rules.privacy import FilterDict
 from cros.factory.test.rules import registration_codes
 from cros.factory.test.rules.registration_codes import RegistrationCode
 from cros.factory.test.utils import bluetooth_utils
 from cros.factory.test.utils import cbi_utils
 from cros.factory.test.utils import gsc_utils
 from cros.factory.test.utils import model_sku_utils
-from cros.factory.test.utils import smart_amp_utils
-from cros.factory.utils import config_utils
 from cros.factory.utils import file_utils
 from cros.factory.utils import json_utils
 from cros.factory.utils import pygpt
@@ -153,10 +151,6 @@ class CrosConfigError(Error):
     return CrosConfigError.message_template % (self.message, self.db_identity,
                                                self.cur_identity)
 
-
-class VPDError(Error):
-  pass
-
 class HWWPError(Error):
   pass
 
@@ -197,6 +191,7 @@ class Gooftool:
     self._util = Util()
     self._flashrom = flashrom
     self._vpd = vpd.VPDTool()
+    self.vpd_utils = vpd_utils.VPDUtils(self._project)
     self._unpack_gbb = gbb.UnpackGBB
     self._unpack_bmpblock = bmpblk.unpack_bmpblock
     self._named_temporary_file = tempfile.NamedTemporaryFile
@@ -666,126 +661,31 @@ class Gooftool:
     main_fw = self._ifdtool.LoadIntelMainFirmware()
     management_engine.VerifyMELocked(main_fw, self._util.shell)
 
+  # TODO(jasonchuang) Move it into vpd_utils.py
   def VerifyVPD(self):
     """Verify that VPD values are set properly."""
 
-    def GetAudioVPDROData():
-      """Return the required audio VPD RO data.
-
-      If a DUT comes with a smart amplifier, it must be calibrated in the
-      factory and the DSM-related VPD values must be set.
-      """
-      speaker_amp, sound_card_init_file, channel_names = self.GetSmartAmpInfo()
-      if speaker_amp:
-        logging.info('Amplifier %s found on DUT.', speaker_amp)
-      if not sound_card_init_file:
-        logging.info('No smart amplifier found! '
-                     'Skip checking DSM VPD value.')
-        return {}
-
-      num_channels = len(channel_names)
-      logging.info(
-          'The VPD RO should contain `dsm_calib_r0_N` and '
-          '`dsm_calib_temp_N` where N ranges from 0 ~ %d.', num_channels - 1)
-      dsm_vpd_ro_data = {}
-      for channel in range(num_channels):
-        dsm_vpd_ro_data[f'dsm_calib_r0_{int(channel)}'] = r'[0-9]*'
-        dsm_vpd_ro_data[f'dsm_calib_temp_{int(channel)}'] = r'[0-9]*'
-
-      return dsm_vpd_ro_data
-
-    def CheckVPDFields(section, data, required, optional, optional_re):
-      """Checks if all fields in data fall into given format.
-
-      Args:
-        section: a string for VPD section name, 'RO' or 'RW.
-        data: a mapping of (key, value) for VPD data.
-        required: a mapping of (key, format_RE) for required data.
-        optional: a mapping of (key, format_RE) for optional data.
-        optional_re: a mapping of (key_re, format_RE) for optional data.
-
-      Returns:
-        A list of verified keys.
-
-      Raises:
-        ValueError if some value does not match format_RE.
-        KeyError if some unexpected VPD key name is found.
-      """
-      known = required.copy()
-      known.update(optional)
-      unknown_keys, misformat_key_pattern = self.GetInvalidVPDFields(
-          data, known, optional_re)
-
-      errors = []
-      for k in unknown_keys:
-        errors.append(f'Unexpected {section} VPD: {k}={data[k]}.')
-
-      for k, pattern in misformat_key_pattern:
-        errors.append(f'Incorrect {section} VPD: {k}={data[k]} '
-                      f'(expected format: {pattern})')
-
-      missing_keys = set(required).difference(set(data.keys()))
-      if missing_keys:
-        errors.append(
-            f"Missing required {section} VPD values: {','.join(missing_keys)}")
-
-      if errors:
-        raise VPDError('\n'.join(errors))
-
-    def GetDeviceNameForRegistrationCode(project):
-
-      def _LoadConfigJsonFile(_config):
-        try:
-          return config_utils.LoadConfig(_config, validate_schema=False)
-        except Exception:
-          return None
-
-      config = 'custom_label_reg_code'
-      # Load config json file
-      reg_code_config = _LoadConfigJsonFile(config)
-      if reg_code_config is None:
-        # Fallback to the legacy name.
-        reg_code_config = _LoadConfigJsonFile('whitelabel_reg_code')
-        if reg_code_config:
-          logging.warning(
-              '"whitelabel_reg_code.json is deprecated, please rename it to %s',
-              config)
-
-      if reg_code_config is None:
-        return project
-
-      if project not in reg_code_config:
-        return project
-
-      # Get the custom-label-tag for custom label device
-      is_custom_label, custom_label_tag = self._cros_config.GetCustomLabelTag()
-      if (not is_custom_label or
-          custom_label_tag not in reg_code_config[project]):
-        return project
-      if reg_code_config[project][custom_label_tag]:
-        return custom_label_tag
-      return project
-
     required_vpd_ro_data = vpd_data.REQUIRED_RO_DATA.copy()
-    audio_vpd_ro_data = GetAudioVPDROData()
+    audio_vpd_ro_data = self.vpd_utils.GetAudioVPDROData()
     required_vpd_ro_data.update(audio_vpd_ro_data)
 
     # Check required data
     ro_vpd = self._vpd.GetAllData(partition=vpd.VPD_READONLY_PARTITION_NAME)
     rw_vpd = self._vpd.GetAllData(partition=vpd.VPD_READWRITE_PARTITION_NAME)
-    CheckVPDFields('RO', ro_vpd, required_vpd_ro_data, vpd_data.KNOWN_RO_DATA,
-                   vpd_data.KNOWN_RO_DATA_RE)
+    self.vpd_utils.CheckVPDFields('RO', ro_vpd, required_vpd_ro_data,
+                                  vpd_data.KNOWN_RO_DATA,
+                                  vpd_data.KNOWN_RO_DATA_RE)
 
-    CheckVPDFields(
-        'RW', rw_vpd, vpd_data.REQUIRED_RW_DATA, vpd_data.KNOWN_RW_DATA,
-        vpd_data.KNOWN_RW_DATA_RE)
+    self.vpd_utils.CheckVPDFields('RW', rw_vpd, vpd_data.REQUIRED_RW_DATA,
+                                  vpd_data.KNOWN_RW_DATA,
+                                  vpd_data.KNOWN_RW_DATA_RE)
 
     # Check known value contents.
     region = ro_vpd['region']
     if region not in regions.REGIONS:
-      raise VPDError(f'Unknown region: "{region}".')
+      raise vpd_utils.VPDError(f'Unknown region: "{region}".')
 
-    device_name = GetDeviceNameForRegistrationCode(self._project)
+    device_name = self.vpd_utils.GetDeviceNameForRegistrationCode(self._project)
 
     for type_prefix in ['UNIQUE', 'GROUP']:
       vpd_field_name = type_prefix[0].lower() + 'bind_attribute'
@@ -796,7 +696,8 @@ class Gooftool:
             rw_vpd[vpd_field_name], type=type_name, device=device_name,
             allow_dummy=(phase.GetPhase() < phase.PVT_DOGFOOD))
       except registration_codes.RegistrationCodeException as e:
-        raise VPDError(f'{vpd_field_name} is invalid: {e!r}') from None
+        raise vpd_utils.VPDError(
+            f'{vpd_field_name} is invalid: {e!r}') from None
 
   def VerifyReleaseChannel(self, enforced_channels=None):
     """Verify that release image channel is correct.
@@ -1163,88 +1064,10 @@ class Gooftool:
     return self._util.GetSystemInfo(filter_vpd=True)
 
   def ClearFactoryVPDEntries(self):
-    """Clears factory related VPD entries in the RW VPD.
-
-    All VPD entries with '.' in key name are considered as special.
-    We collect all special names and delete entries with known prefixes,
-    and fail if there are unknown entries left.
-
-    Returns:
-      A dict of the removed entries.
-    """
-    def _IsFactoryVPD(k):
-      # These names are defined in cros.factory.test.device_data
-      known_names = ['factory.', 'component.', 'serials.']
-      return any(name for name in known_names if k.startswith(name))
-
-    rw_vpd = self._vpd.GetAllData(partition=vpd.VPD_READWRITE_PARTITION_NAME)
-    dot_entries = {k: v for k, v in rw_vpd.items() if '.' in k}
-    logging.info('Current special RW VPDs: %r', FilterDict(dot_entries))
-    entries = {k: v for k, v in dot_entries.items() if _IsFactoryVPD(k)}
-    unknown_keys = set(dot_entries) - set(entries)
-    if unknown_keys:
-      raise VPDError(f'Found unexpected RW VPD(s): {unknown_keys!r}')
-
-    if entries:
-      self.ClearRWVPDEntries(entries.keys())
-    else:
-      logging.info('No factory-related RW VPDs are found. Skip clearing.')
-
-    return entries
+    return self.vpd_utils.ClearFactoryVPDEntries()
 
   def ClearUnknownVPDEntries(self):
-    rw_vpd = self._vpd.GetAllData(partition=vpd.VPD_READWRITE_PARTITION_NAME)
-    known_rw_vpd = dict(vpd_data.REQUIRED_RW_DATA, **vpd_data.KNOWN_RW_DATA)
-    unknown_keys, unused_misformat_key_pattern = self.GetInvalidVPDFields(
-        rw_vpd, known_rw_vpd, vpd_data.KNOWN_RW_DATA_RE)
-    logging.info('Current RW VPDs: %r', FilterDict(rw_vpd))
-    if unknown_keys:
-      self.ClearRWVPDEntries(unknown_keys)
-    else:
-      logging.info('No unknown RW VPDs are found. Skip clearing.')
-
-    return unknown_keys
-
-  def ClearRWVPDEntries(self, keys):
-    logging.info('Removing VPD entries with key %r', keys)
-    try:
-      self._vpd.UpdateData({k: None
-                            for k in keys},
-                           partition=vpd.VPD_READWRITE_PARTITION_NAME)
-    except Exception as e:
-      raise VPDError(f'Failed to remove VPD entries: {e!r}') from None
-
-  def GetInvalidVPDFields(self, data, known_vpd, known_vpd_re):
-    """Gets the invalid VPD fields from `data`.
-
-    Invalid VPDs are VPDs with unknown keys or unmatched value pattern.
-
-    Args:
-      data: a mapping of (key, value) for VPD data.
-      known_vpd: a mapping of (key, format_RE) for known data.
-      known_vpd_re: a mapping of (key_re, format_RE) for known data.
-
-    Returns:
-      A list of unknown keys and a list of (key, pattern) tuple where the
-      value of the key does not match the expected pattern.
-    """
-    unknown_keys = []
-    misformat_key_pattern = []
-    for k, v in data.items():
-      if k in known_vpd:
-        if not re.fullmatch(known_vpd[k], v):
-          misformat_key_pattern.append((k, known_vpd[k]))
-        continue
-
-      for rk, rv in known_vpd_re.items():
-        if re.fullmatch(rk, k):
-          if not re.fullmatch(rv, v):
-            misformat_key_pattern.append((k, rv))
-          break
-      else:
-        unknown_keys.append(k)
-
-    return unknown_keys, misformat_key_pattern
+    return self.vpd_utils.ClearUnknownVPDEntries()
 
   def GenerateStableDeviceSecret(self):
     """Generates a fresh stable device secret and stores it in RO VPD.
@@ -1526,8 +1349,3 @@ class Gooftool:
                      CrosConfigIdentity.non_inclusive_custom_label_tag_vpd_key)
 
     return db_identity, cur_identity
-
-  def GetSmartAmpInfo(self):
-    """Returns the smart amp info by parsing sound-card-init-conf."""
-
-    return smart_amp_utils.GetSmartAmpInfo()
