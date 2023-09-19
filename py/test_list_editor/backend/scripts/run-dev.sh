5aff5782d40be4eaa6eedf578f89841f891898ad
Louis Chiu <louischiu@google.com>
2023-09-19 08:32:48 +0000

    test_list_editor: Update backend server process
    Change the backend to use gunicorn server.
    
    The current flask program fells short when we are making consecutive
    requests from the frontend. It will receive
    "net::err_content_length_mismatch 200" response when we visit pages
    that fetches a large amount of data from the backend.
    
    In Flask setting, two consequtive request cannot be handled
    concurrently and hence is causing issues to the frontend.
    
    Changing the server to gunicorn allows us to serve the request in
    a concurrent manner. If in the future, we need more processing
    power, we can create more workers to handle it.
    
    BUG=b:270499613
    TEST=scripts/run-dev.sh and verify the process is running. Use curl
         to verify the service is able to receive requests.
    TEST=scripts/run-dev.sh and load the frontend from CL:4812919 chain.
         Visit localhost:5100/create/empty.test_list/ and see if error
         would show-up in the web console.
    
    Change-Id: I59cb5d59e3d87870c6aeb133d0d961b1729f662e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/4875420
    Auto-Submit: Louis Chiu <louischiu@google.com>
    Commit-Queue: Louis Chiu <louischiu@google.com>
    Reviewed-by: Cheng Yueh <cyueh@chromium.org>
    Commit-Queue: Cheng Yueh <cyueh@chromium.org>
    Tested-by: Louis Chiu <louischiu@google.com>

--- py/test_list_editor/backend/scripts/run-dev.sh
+++ py/test_list_editor/backend/scripts/run-dev.sh
@@ -15,5 +15,37 @@ source "$(get_venv_dir)"/bin/activate
 
 echo "Starting Flask Server";
 cd "${CROS_DIR}" || exit
-python3 -m flask --app cros.factory.test_list_editor.backend.main \
-  --debug run -p "${PORT_NUMBER}" --host 0.0.0.0
+
+# Use gunicorn to serve the backend program.
+
+# The program uses "gthread" worker class to look for a balance between
+# IO-bound task and CPU-bound task. This would help alleviate the stress when
+# React hooks are firing multiple requests to the service.
+#
+# The arguments would perform the following things:
+#
+# "-w 2": Sets process worker to 2. This is an arbitrary number.
+#         According to the gunicorn document, this should be fairly small
+#         number (< 12).
+# "-b 0.0.0.0:"${PORT_NUMBER}"": Binds the service to listen to the specified
+#                                url and port.
+# "--enable-stdio-inheritance": Allow python print output to stdout.
+# "--access-logfile -": Print the access log to stdout.
+# "--threads 2": Set the thread count in each worker to 2. This is an
+#                arbitrary number.
+# "--reload": Reload when there is change to the backend program. Useful
+#             functionality for debugging the service.
+#
+# For more detailed settings on how to tune the workers, please refer to
+# the gunicorn document.
+# https://docs.gunicorn.org/en/stable/settings.html#worker-processes
+
+python3 -m gunicorn \
+    -w 2 \
+    -b 0.0.0.0:"${PORT_NUMBER}" \
+    --worker-class gthread \
+    --enable-stdio-inheritance \
+    --access-logfile - \
+    --threads 2 \
+    --reload \
+    cros.factory.test_list_editor.backend.main:app
