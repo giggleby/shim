8edab799c3ff51c4683eea4f58a566197572f97b
Cheng Yueh <cyueh@chromium.org>
2023-10-18 18:24:52 +0800

    pytests: Remove unused and undocumented pytests
    Have no future plans and are unused:
    1. chameleon
    2. fastboot_flash
    3. offline_test
    
    Lost context and are unused:
    1, interrupt
    2. power_under_stress
    
    Have better test and are unused:
    1. usb
    
    BUG=b:305866437
    TEST=repo upload . --cbr --veri
    
    Change-Id: Id20db91f7d1c09f7030c03042beb16c7804c83a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/4950418
    Reviewed-by: Stimim Chen <stimim@chromium.org>
    Tested-by: Cheng Yueh <cyueh@chromium.org>
    Commit-Queue: Cheng Yueh <cyueh@chromium.org>

--- py/test/pytests/offline_test/shell/call_factory_par.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Copyright 2016 The ChromiumOS Authors
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-task_{%id%} () {
-  CROS_FACTORY_TEST_PATH={%pytest_name%} {%cmd%}
-  return "$?"
-}
--- py/test/pytests/offline_test/shell/wait_for.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Copyright 2016 The ChromiumOS Authors
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-task_{%id%} () {
-  info "wait for {%seconds%} seconds"
-  sleep {%seconds%}
-}
